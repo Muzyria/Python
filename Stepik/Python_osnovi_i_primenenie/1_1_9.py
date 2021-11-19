71e47e6b109661806b0d0ab54d17de71da784c9b
“Muzyria” <my email address used on my git repo>
2021-11-19 13:59:42 +0200

    d

--- Stepik/Python_osnovi_i_primenenie/1_1_9.py
+++
@@ -1,8 +0,0 @@
-# put your python code here
-n = int(input())
-a=0
-b=0
-while a < n:
-    b += int(input())
-    a +=1
-print(b)
--- Stepik/Python_osnovi_i_primenenie/1_2_9.py
+++
@@ -1,5 +0,0 @@
-mn = []
-for obj in objects:
-    if obj not in mn:
-        mn.append(obj)
-print(len(mn))
