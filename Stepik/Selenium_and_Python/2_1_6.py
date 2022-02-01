950123bbf216e2671ac6843a92fef1d349a531ab
“Muzyria” <my email address used on my git repo>
2022-02-01 11:17:42 +0200

    new

--- Stepik/Selenium_and_Python/2_1_6.py
+++ Stepik/Selenium_and_Python/2_1_6.py
@@ -6,7 +6,10 @@ try:
     browser = webdriver.Chrome()
     browser.get(link) #open link
 
-    
+    people_radio = browser.find_element_by_id("peopleRule")
+    people_checked = people_radio.get_attribute("checked")
+    print("value of people radio: ", people_checked)
+    assert people_checked is not None, "People radio is not selected by default"
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
