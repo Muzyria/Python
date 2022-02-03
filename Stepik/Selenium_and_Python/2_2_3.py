89439bad3d47bc52483df1bebfa629f7e1feb59a
“Muzyria” <my email address used on my git repo>
2022-02-03 09:43:37 +0200

    new

---
+++ Stepik/Selenium_and_Python/2_2_3.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+link = "http://suninjuly.github.io/selects1.html"
+browser.get(link)
+
+try:
+    x = browser.find_element_by_css_selector("#num1").text
+    y = browser.find_element_by_css_selector("#num2").text
+    summ = int(x) + int(y)
+
+    elements = browser.find_elements_by_css_selector("#dropdown option")
+    for element in elements:
+        select_num = element.get_attribute("value")
+        if select_num.isdigit() and int(select_num) == summ:
+            element.click()
+            break
+
+    browser.find_element_by_css_selector("form button[type=submit]").click()
+
+    time.sleep(3)
+
+    alert = browser.switch_to.alert
+    print(alert.text)
+    alert.accept()
+    time.sleep(5)
+    browser.close()
+
+finally:
+    browser.quit()
