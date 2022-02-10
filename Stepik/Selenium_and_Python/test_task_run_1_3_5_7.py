6bb687a37a06fcbf5b830177feeffd533173f70a
Muzyria <Fila090580@gmail.com>
2022-02-11 00:40:11 +0200

    new

---
+++ Stepik/Selenium_and_Python/test_task_run_1_3_5_7.py
@@ -0,0 +1,59 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage(object):
+    # номер 1
+    @pytest.mark.xfail
+    @pytest.mark.smoke
+    def test_guest_can_login(self, browser):
+        assert True
+
+    # номер 2
+    @pytest.mark.regression
+    def test_guest_can_add_book_from_catalog_to_basket(self, browser):
+        assert True
+
+
+class TestBasket(object):
+    # номер 3
+    @pytest.mark.skip(reason="not implemented yet")
+    @pytest.mark.smoke
+    def test_guest_can_go_to_payment_page(self, browser):
+        assert True
+
+    # номер 4
+    @pytest.mark.smoke
+    def test_guest_can_see_total_price(self, browser):
+        assert True
+
+@pytest.fixture(scope="class")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.mark.skip
+class TestBookPage():
+    # номер 5
+    @pytest.mark.smoke
+    def test_guest_can_add_book_to_basket(self, browser):
+        assert True
+
+    # номер 6
+    @pytest.mark.regression
+    def test_guest_can_see_book_price(self, browser):
+        assert True
+
+
+# номер 7
+@pytest.mark.beta_users
+@pytest.mark.smoke
+def test_guest_can_open_gadget_catalogue(browser):
+    assert True
