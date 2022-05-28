c16ddb1e7125622fb804790ae3e1a9bdefee5a72
“Muzyria” <my email address used on my git repo>
2022-05-28 12:36:52 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/4_3_3.py
+++ Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/4_3_3.py
@@ -6,3 +6,10 @@ elif x > y:
     print('NO')
 else:
     print("Don't know")
+
+'''
+speed_n, speed_k = int(input()), int(input())
+if speed_n < speed_k: print("YES")
+elif speed_n > speed_k: print("NO")
+else: print("Don't know")
+'''    
