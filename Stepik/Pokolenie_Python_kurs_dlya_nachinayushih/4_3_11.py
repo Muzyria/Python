f0ca02b97a2e0537a4fa9966c0dbb4bebbb17460
“Muzyria” <my email address used on my git repo>
2022-05-28 14:44:10 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/4_3_11.py
+++ Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/4_3_11.py
@@ -1,3 +1,4 @@
+'''
 a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
 if  b1 == a2:
     print(b1)
@@ -13,3 +14,10 @@ elif a2 <= a1 < b2 and b2 <= b1:
     print(a1, b2)
 else:
     print('пустое множество')
+'''
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+a = max(a1, a2) 
+b = min(b1, b2)
+if a < b: print(a,b)
+elif a == b: print(a)
+else: print("пустое множество")    
