50037904b3e2e541bfd920abd18acd583fb54021
“Muzyria” <my email address used on my git repo>
2022-05-30 21:41:22 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/6_2_8.py
+++ Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/6_2_8.py
@@ -4,3 +4,8 @@ from math import *
 n, a = float(input()), float(input())
 s = (n * a ** 2) / (4 * tan(pi / n))
 print(s)
+
+'''
+towns = [input() for _ in range(3)]
+print(min(towns, key=len), max(towns, key=len), sep='\n')
+'''
--- Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/7_2_8.py
+++ Stepik/Pokolenie_Python_kurs_dlya_nachinayushih/7_2_8.py
@@ -6,3 +6,11 @@ if n < m:
 else:
     for i in range(n, m - 1, -1):
         print(i)
+
+'''
+m, n, s = int(input()), int(input()), 1
+if m > n:
+    s = -1
+for i in range(m,n+s,s):
+    print(i)
+'''        
