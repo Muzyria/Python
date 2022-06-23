119d2387bb92a3e684a245e2aa8a5ce5fed98eb3
“Muzyria” <fila090580@gmail.com>
2022-06-23 15:28:49 +0300

    new

---
+++ Stepik/OOP_na_Python/2_1_9.py
@@ -0,0 +1,19 @@
+class Constructor:
+    def add_atribute(self, key, value):
+        setattr(self, key, value)
+
+    def display(self):
+        print(self.__dict__)    
+
+
+obj1 = Constructor()
+obj1.display() # печатает {}
+obj1.add_atribute('color', 'red')
+obj1.add_atribute('width', 20)
+obj1.display() # печатает {'color': 'red', 'width': 20}
+
+obj2 = Constructor()
+obj2.display() # печатает {}
+obj2.add_atribute('height', 100)
+obj2.display() # печатает {'height': 100}        
+
