2f5c3c05bdd9f0a5b7afd149388ac831c0ad76be
“Muzyria” <fila090580@gmail.com>
2022-06-25 00:59:47 +0300

    new

--- Stepik/OOP_na_Python/2_3_5.py
+++ Stepik/OOP_na_Python/2_3_5.py
@@ -1,19 +1,19 @@
+
 class CustomLabel:
     def __init__(self, text, **kwargs):
         self.text = text
-        print(kwargs)
-        #for key, value in kwargs.items():
-            #self.__dict__[key] = value
+        self.config(**kwargs) # Обращение к методу config класса CustomLabel.
 
     def config(self, **kwargs):
-        print(kwargs)
-        #self.__dict__.update(kwargs)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        
 
 
 label = CustomLabel(text="Hello", bd=20, bg='#ffaaaa')
 
-#print(label.__dict__) # {'text': 'Hello', 'bd': 20, 'bg': '#ffaaaa'}
+print(label.__dict__) # {'text': 'Hello', 'bd': 20, 'bg': '#ffaaaa'}
 
-#label.config(color='red', bd=100)
+label.config(color='red', bd=100)
 
-#print(label.__dict__) # {'text': 'Hello', 'bd': 100, 'bg': '#ffaaaa', 'color': 'red'}
+print(label.__dict__) # {'text': 'Hello', 'bd': 100, 'bg': '#ffaaaa', 'color': 'red'}
