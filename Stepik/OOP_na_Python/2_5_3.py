c59bf0ff76d3a0ffa9af0f027a69e48ff4f6ce3b
“Muzyria” <fila090580@gmail.com>
2022-06-25 08:15:32 +0300

    new

---
+++ Stepik/OOP_na_Python/2_5_3.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, age, branch):
+        self.__name = name
+        self.__age = age
+        self.__branch = branch
+
+    def __display_details(self):
+        print(f"Имя: {self.__name}\n"
+        f"Возраст: {self.__age}\n"
+        f"Направление: {self.__branch}")
+
+    def access_private_method(self):
+        self.__display_details()  
+
+obj = Student("Adam Smith", 25, "Information Technology")
+obj.access_private_method()
