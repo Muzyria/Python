88b4254b188e1518361c0c85d288d35482705183
“Muzyria” <fila090580@gmail.com>
2022-06-27 03:11:29 +0300

    new

--- Stepik/OOP_na_Python/2_11_3.py
+++ Stepik/OOP_na_Python/2_11_3.py
@@ -32,23 +32,29 @@ class Registration:
     @staticmethod
     def is_include_all_register(password):
         for char in ascii_uppercase:
-            if char not in password:
-                return False
-        for char in ascii_lowercase:
-            if char not in password:
-                return False 
-        return True  
+            if char in password:
+                
+                for char in ascii_lowercase:
+                    if char in password:
+                        return True 
+        return False  
 
     @staticmethod
     def is_include_only_latin(password):
         for char in ascii_letters:
-            if char not in password:
-                return False
-        return True 
+            if char in password:
+                return True
+        return False 
 
     @staticmethod
     def check_password_dictionary(password):
-        pass                  
+        list_pass = []
+        with open("easy_passwords.txt") as file:
+            for word in file:
+                list_pass.append(word[:-1])    
+        if password in list_pass:
+            return False
+        return True                          
 
     @password.setter
     def password(self, value):
@@ -62,4 +68,15 @@ class Registration:
             raise ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
         if not Registration.is_include_only_latin(value):
             raise ValueError('Пароль должен содержать только латинский алфавит')
+        if not Registration.check_password_dictionary(value):
+            raise ValueError('Ваш пароль содержится в списке самых легких')
         self.__password = value    
+
+
+r1 = Registration('qwerty@rambler.ru', 'QwerTy123') # здесь хороший логин
+print(r1.login, r1.password)  # qwerty@rambler.ru QwrRt124    
+
+# теперь пытаемся запись плохие пароли
+#r1.password = '123456'  # ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+#r1.password = 'LoW'  # raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+#1.password = 43  # raise TypeError("Пароль должен быть строкой")
