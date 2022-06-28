08c6b0f0df7d50eadcc9c12b4fc97a801db71eed
“Muzyria” <fila090580@gmail.com>
2022-06-29 01:07:44 +0300

    new

---
+++ Stepik/OOP_na_Python/3_6_2.py
@@ -0,0 +1,19 @@
+
+class City:
+    def __init__(self, name):
+        self.name = name.title()
+
+    def __str__(self):
+        return self.name 
+
+    def __bool__(self):
+        return False if self.name[-1] in 'aeiou' else True    
+
+
+
+p1 = City('new york')
+print(p1)  # печатает "New York"
+print(bool(p1))  # печатает "True"
+p2 = City('SaN frANCISco')
+print(p2)  # печатает "San Francisco"
+print(p2 == True)  # печатает "False"
