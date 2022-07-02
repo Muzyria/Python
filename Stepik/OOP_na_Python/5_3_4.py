70455744fc4781b584ce5843aa40edac86323e57
“Muzyria” <fila090580@gmail.com>
2022-07-02 17:02:22 +0300

    new

---
+++ Stepik/OOP_na_Python/5_3_4.py
@@ -0,0 +1,7 @@
+try:
+    a, b = int(input()), int(input())
+    print(f"Результат деления a на b: {a/b}")
+except ValueError:
+    print("Введите целое число")
+except ZeroDivisionError:
+    print("Делитель не должен быть равен нулю")      
---
+++ Stepik/OOP_na_Python/5_3_6.py
@@ -0,0 +1,9 @@
+
+def func(phrase):
+    func(phrase)
+    
+try:
+    func('Это рекурсия, детка!')    
+except RecursionError:
+    print("Кто-то должен остановить это безумие")
+
