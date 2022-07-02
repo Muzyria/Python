8a6760234a88b8cb0b3f2ab12e94bcace9c062e5
“Muzyria” <fila090580@gmail.com>
2022-07-02 17:45:56 +0300

    new

---
+++ Stepik/OOP_na_Python/5_4_2.py
@@ -0,0 +1,30 @@
+class Customer:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+
+    @staticmethod
+    def check_type(value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('Банк работает только с числами')
+
+    def withdraw(self, value):
+        self.check_type(value)
+        if self.balance >= value:
+            self.balance -= value
+        else:
+            raise ValueError('Сумма списания превышает баланс')
+
+    def deposit(self, value):
+        self.check_type(value)
+        self.balance += value
+
+
+
+bob = Customer('Bob Odenkirk')
+# bob.deposit('hello') # TypeError: Банк работает только с числами
+bob.deposit(200)
+print(bob.balance)  # 200
+#bob.withdraw(300)  # ValueError: Сумма списания превышает баланс
+bob.withdraw(150)
+print(bob.balance)  # 50            
