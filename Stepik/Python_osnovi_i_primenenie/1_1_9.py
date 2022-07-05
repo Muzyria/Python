799fb9bd6735ace3d3f414190771cbb50fffa94c
“Muzyria” <fila090580@gmail.com>
2022-07-05 19:54:48 +0300

    new

--- Stepik/Python_osnovi_i_primenenie/1_1_9.py
+++ Stepik/Python_osnovi_i_primenenie/1_1_9.py
@@ -1,5 +1,8 @@
-mn = []
-for obj in objects:
-    if obj not in mn:
-        mn.append(obj)
-print(len(mn))
+# put your python code here
+n = int(input())
+a=0
+b=0
+while a < n:
+    b += int(input())
+    a +=1
+print(b)
