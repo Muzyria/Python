0e1976d920376d829556fd8e55bcd91b6a632f24
“Muzyria” <fila090580@gmail.com>
2022-07-07 18:29:45 +0300

    new

---
+++ Stepik/Python_osnovi_i_primenenie/2_3_5.py
@@ -0,0 +1,22 @@
+import itertools
+
+
+def is_prime(num):
+    if num == 2: return True
+    if num % 2 == 0: return False
+    for _ in range(3, num // 2, 2):
+        if num % _ == 0:
+            return False
+    return True
+
+
+def primes():
+    num = 2
+    while True:
+        if is_prime(num):
+            yield num
+        num += 1
+
+
+print(list(itertools.takewhile(lambda x: x <= 31, primes())))
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
