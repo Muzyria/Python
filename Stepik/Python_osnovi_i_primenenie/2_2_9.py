a654f58eb9e939b3bc87d3af8fcfbab48ab3fb43
“Muzyria” <fila090580@gmail.com>
2022-07-07 03:12:45 +0300

    new

--- Stepik/Python_osnovi_i_primenenie/2_2_9.py
+++ Stepik/Python_osnovi_i_primenenie/2_2_9.py
@@ -1,6 +1,17 @@
-from simplecrypt import encrypt, decrypt
+from simplecrypt import decrypt
 
 
 with open("encrypted.bin", "rb") as inp:
     encrypted = inp.read()
+
+for password in open("passwords.txt", "r"):
+    try:
+        print(decrypt(password[:-1], encrypted).decode("utf8"))
+    except Exception:
+        pass
+    else:
+        print(password, 'is correct')
+
+
+
 
