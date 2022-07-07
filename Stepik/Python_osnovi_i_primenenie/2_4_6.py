8a6a184497d235e54329497cb797987f3f45483b
“Muzyria” <fila090580@gmail.com>
2022-07-07 22:19:45 +0300

    new

--- Stepik/Python_osnovi_i_primenenie/2_4_6.py
+++ Stepik/Python_osnovi_i_primenenie/2_4_6.py
@@ -3,7 +3,9 @@ import os
 with open("2_4_6.txt", "w") as file:
     for curr_dir, dirs, files in os.walk("main"):
         # print(curr_dir, dirs, files)
+        if set(filter(lambda x: x.endswith(".py"), files)):
+            # print(curr_dir)
+            file.write(curr_dir + "\n")
 
-        print(files)
 
 
