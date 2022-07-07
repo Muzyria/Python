bd3f506be02f4b8263cd0b07403cc371c10f5567
“Muzyria” <fila090580@gmail.com>
2022-07-07 19:51:54 +0300

    new

--- Stepik/Python_osnovi_i_primenenie/2_4_4.py
+++ Stepik/Python_osnovi_i_primenenie/2_4_4.py
@@ -1,4 +1,10 @@
-f = open('module2/tests/test_1.txt', "w")
-f.write("Hello")
+with open('dataset_24465_4 (3).txt', "r") as file, \
+        open('dataset_24465_4_reverse.txt.txt', "w") as file_2:
 
-f.close()
+    f1_line = file.read().splitlines()
+
+    for line in f1_line[::-1]:
+        file_2.write(line + '\n')
+
+
+
