92274a4866a483e702d98261ccdb859d2be5f9b5
“Muzyria” <fila090580@gmail.com>
2022-07-24 19:53:21 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/2_1_10.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/2_1_10.py
@@ -1,7 +1,17 @@
 
 def likes(names):
-    pass
+    l = len(names)
+    if l == 0: return f"Никто не оценил данную запись"
+    if l == 1: return f"{names[0]} оценил(а) данную запись"
+    if l == 2: return f"{names[0]} и {names[1]} оценили данную запись"
+    if l == 3: return f"{names[0]}, {names[1]} и {names[2]} оценили данную запись"
+    return f"{names[0]}, {names[1]} и {len(names) - 2} других оценили данную запись"
 
 
-print(likes(['Дима', 'Алиса']))
-print(likes(['Эндрю', 'Тоби', 'Том']))
+print(likes([]))
+print(likes(['Тимур']))
+print(likes(['Тимур', 'Артур']))
+print(likes(['Тимур', 'Артур', 'Руслан']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри', 'Дима']))
+print(likes(['Тимур', 'Артур', 'Руслан', 'Анри', 'Дима', 'Рома', 'Гвидо', 'Марк']))
