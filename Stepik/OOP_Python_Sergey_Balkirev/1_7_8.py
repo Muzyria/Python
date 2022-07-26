c3847a947ce503eef71d583fdd2dee308beb950f
“Muzyria” <fila090580@gmail.com>
2022-07-27 01:44:04 +0300

    new

---
+++ Stepik/OOP_Python_Sergey_Balkirev/1_7_8.py
@@ -0,0 +1,63 @@
+from string import ascii_lowercase, digits
+
+
+class TextInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if len(name) < 3 or len(name) > 50:
+            return False
+        for char in name:
+            if char not in cls.CHARS_CORRECT:
+                return False
+        return True
+
+
+class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if len(name) < 3 or len(name) > 50:
+            return False
+        for char in name:
+            if char not in cls.CHARS_CORRECT:
+                return False
+        return True
+
+
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+    def render_template(self):
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+
+
+# эти строчки не менять
+login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
+html = login.render_template()
