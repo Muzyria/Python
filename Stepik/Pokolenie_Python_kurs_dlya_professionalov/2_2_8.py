b18ff132837dd4b1542a6631e4e9b60b8b325886
“Muzyria” <fila090580@gmail.com>
2022-07-29 00:22:18 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/2_2_8.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/2_2_8.py
@@ -1,2 +1,35 @@
 
 n = int(input())
+lst_email = {}
+for _ in range(n):
+    email = input()
+    name_email, mail = email.split('@')
+    number = ''
+    name = ''
+    for i in name_email:
+        if i.isdigit():
+            number += i
+        else:
+            name += i
+    lst_email[name] = lst_email.get(name, []) + [number]
+# print(lst_email)
+
+m = int(input())
+for _ in range(m):
+    new_name = input()
+    if new_name not in lst_email:
+        lst_email[new_name] = lst_email.get(new_name, []) + ['']
+        print(f"{new_name}@beegeek.bzz")
+    else:
+        if '' in lst_email[new_name]:
+            for j in range(1, 100):
+                if str(j) not in lst_email[new_name]:
+                    lst_email[new_name] = lst_email.get(new_name, []) + [str(j)]
+                    print(f"{new_name}{j}@beegeek.bzz")
+                    break
+        else:
+            lst_email[new_name] = lst_email.get(new_name, []) + ['']
+            print(f"{new_name}@beegeek.bzz")
+
+
+print(lst_email)
