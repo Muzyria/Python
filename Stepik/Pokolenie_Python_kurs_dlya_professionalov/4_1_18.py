86e2e21f4d1a065ae531fb05cc0747e895eb6b6d
“Muzyria” <fila090580@gmail.com>
2022-08-11 12:48:59 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_1_18.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_1_18.py
@@ -1,5 +1,12 @@
-
 import sys
 
-
 data = [int(line.strip()) for line in sys.stdin]
+arif = [i for i in range(data[0], len(data) + 1)]
+geom = [int(data[0] * (data[1] / data[0]) ** (i - 1)) for i in range(1, len(data) + 1)]
+
+if data == arif:
+    print('Арифметическая прогрессия')
+elif data == geom:
+    print('Геометрическая прогрессия')
+else:
+    print('Не прогрессия')
