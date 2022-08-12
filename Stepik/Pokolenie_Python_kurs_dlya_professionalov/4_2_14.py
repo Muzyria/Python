d0182c5166b78eafbabf6b4b30d30f0672f71f16
“Muzyria” <fila090580@gmail.com>
2022-08-13 02:01:46 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_2_14.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_2_14.py
@@ -1,6 +1,10 @@
 import csv
 
-n = int(input())
-with open('salary_data.csv', encoding='utf-8') as file:
-    rows = csv.reader(file, delimiter=',', quotechar='"')
 
+with open('salary_data.csv', encoding='utf-8') as file:
+    result = {}
+    data = csv.DictReader(file, delimiter=';', quotechar='"')
+    for row in data:
+        result.setdefault(row['company_name'], []).append(int(row['salary']))
+    result = {k: (sum(v) / len(v)) for k, v in result.items()}
+    [print(i[0]) for i in (sorted(result.items(), key=lambda x: x[1]))]
