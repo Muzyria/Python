f52e690ad7c2c815c001a261908219cf384899cd
“Muzyria” <fila090580@gmail.com>
2022-08-14 16:02:22 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_2_18.py
@@ -0,0 +1,17 @@
+
+import csv
+
+
+with open('titanic.csv', encoding='utf-8') as file:
+    data = csv.DictReader(file, delimiter=';', quotechar='"')
+    result_male = []
+    result_female = []
+    for row in data:
+        # print(float(row['age']), type(row['age']))
+        if float(row['age']) < 18 and row['survived'] == '1':
+            if row['sex'] == 'male':
+                result_male.append(row['name'])
+            else:
+                result_female.append(row['name'])
+    [print(i) for i in result_male]
+    [print(i) for i in result_female]
