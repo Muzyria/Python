217c760ef44e00d75b5b0a279dbba8c6e5270cae
Muzyria <fila090580@gmail.com>
2022-08-25 00:07:59 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_12.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_12.py
@@ -1,7 +1,15 @@
 import json
 import csv
 
-with open('students.json', encoding='utf-8') as file:
+columns = ['name', 'phone']
+with open('students.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
+    result = []
     for row in data:
-        print(row)
+        if row['age'] >= 18 and row['progress'] >= 75:
+            result.append([row['name'], row['phone']])
+
+    with open('data_1.csv', 'w', encoding='utf-8', newline='') as file_w:
+        writer = csv.writer(file_w)
+        writer.writerow(['name', 'phone'])
+        writer.writerows(sorted(result, key=lambda x: x[0]))
