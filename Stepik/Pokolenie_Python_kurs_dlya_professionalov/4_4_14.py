5123ddf6268a27f41b834746d424980e9d075aa2
Muzyria <fila090580@gmail.com>
2022-08-29 01:30:43 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
@@ -4,16 +4,3 @@ from datetime import datetime
 
 with open('exam_results.csv', encoding='utf-8') as file:
     # data = csv.DictReader(file, delimiter=',', quotechar='"')
-    data = csv.reader(file)
-    next(data)
-    result = {}
-    for row in data:
-        name_full = (row[0], row[1])
-        if (row[0], row[1]) not in result:
-            result.setdefault((row[0], row[1]), (int(row[2]), datetime.fromisoformat(row[3]), row[4]))
-        else:
-            if result[(row[0], row[1])][0] < int(row[2]):
-                result[(row[0], row[1])] = (int(row[2]), datetime.fromisoformat(row[3]), row[4])
-            elif result[(row[0], row[1])][0] == int(row[2]):
-                result[(row[0], row[1])] = (int(row[2]), datetime.fromisoformat(row[3]), row[4])
-    print(result)
