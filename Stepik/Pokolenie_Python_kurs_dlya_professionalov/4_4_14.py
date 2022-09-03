b69eaa07a26fea2bf8d79033da03e5ce25675e64
Muzyria <fila090580@gmail.com>
2022-09-03 03:22:18 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
@@ -3,4 +3,4 @@ import csv
 from datetime import datetime
 
 with open('exam_results.csv', encoding='utf-8') as file:
-    # data = csv.DictReader(file, delimiter=',', quotechar='"')
+    data = csv.DictReader(file, delimiter=',', quotechar='"')
