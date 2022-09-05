451a3c9fb10732d5d50a45c8de839e380ab0ae79
“Muzyria” <fila090580@gmail.com>
2022-09-05 15:58:07 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
@@ -0,0 +1,8 @@
+import json
+import csv
+from datetime import datetime
+
+with open('exam_results.csv', encoding='utf-8') as file:
+    data = csv.DictReader(file, delimiter=';', quotechar='"')
+    for row in data:
+        print(row)
