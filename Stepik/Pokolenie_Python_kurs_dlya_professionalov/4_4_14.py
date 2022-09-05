5dc5e2eb0d4c1ad6337702f69db9555a24bcb8d7
“Muzyria” <fila090580@gmail.com>
2022-09-05 16:13:01 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_14.py
@@ -2,7 +2,10 @@ import json
 import csv
 from datetime import datetime
 
+students = []
+students_dict = {}
+pattern = "%Y-%m-%d %H:%M:%S"
 with open('exam_results.csv', encoding='utf-8') as file:
     data = csv.DictReader(file, delimiter=';', quotechar='"')
     for row in data:
-        print(row)
+        key = row['email']
