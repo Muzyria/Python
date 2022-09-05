376fce58adbe8a2870ae0143aefb367dcc3e6c12
“Muzyria” <fila090580@gmail.com>
2022-09-05 18:42:59 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_15.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_4_15.py
@@ -1,5 +1,16 @@
 import json
 
-with open('food_services.json', encoding='utf-8') as file:
-    data = json.load(file)
-    print(data)
+with open('food_services.json', encoding='UTF-8') as file:
+    json_data = json.load(file)
+    district, chain = {}, {}
+
+    for entry in json_data:
+        district[entry['District']] = district.get(entry['District'], 0) + 1
+        if entry['IsNetObject'] == 'да':
+            chain[entry['OperatingCompany']] = chain.get(entry['OperatingCompany'], 0) + 1
+
+    max_district = max(district.items(), key=lambda x: x[1])
+    max_chain = max(chain.items(), key=lambda x: x[1])
+
+    print(f'{max_district[0]}: {max_district[1]}')
+    print(f'{max_chain[0]}: {max_chain[1]}')
