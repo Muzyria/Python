cf57c451bfa4e2fd8da12fe37fe2d7fd3ce598ec
“Muzyria” <fila090580@gmail.com>
2022-09-06 16:53:14 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_5_14.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/4_5_14.py
@@ -3,3 +3,9 @@ from zipfile import ZipFile
 with ZipFile('workbook.zip') as zip_file:
     result = [file for file in zip_file.infolist() if file.file_size > 0]
     print(len(result))
+
+
+# from zipfile import ZipFile
+#
+# with ZipFile('workbook.zip') as z_file:
+#     print(sum(map(lambda x: not x.is_dir(), z_file.infolist())))
