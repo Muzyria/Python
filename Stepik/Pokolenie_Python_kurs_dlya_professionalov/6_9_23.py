ce169fc7e663084e8eab1c9894c3922a083b9dd9
“Muzyria” <fila090580@gmail.com>
2022-09-30 16:55:24 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/6_9_23.py
@@ -0,0 +1,31 @@
+from collections import Counter, ChainMap
+
+# y = 'ржаная булочка,ржаная булочка,говяжий бифштекс,сыр,сыр,салат,барбекю,помидор'
+y = 'сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр,сыр'
+data = sorted(set(y.split(',')))  #change to input()
+counter = Counter(y.split(','))
+
+bread = {'булочка с кунжутом': 15, 'обычная булочка': 10, 'ржаная булочка': 15}
+meat = {'куриный бифштекс': 50, 'говяжий бифштекс': 70, 'рыбный бифштекс': 40}
+sauce = {'сливочно-чесночный': 15, 'кетчуп': 10, 'горчица': 10, 'барбекю': 15, 'чили': 15}
+vegetables = {'лук': 10, 'салат': 15, 'помидор': 15, 'огурцы': 10}
+toppings = {'сыр': 25, 'яйцо': 15, 'бекон': 30}
+
+chain_map = ChainMap(bread, meat, sauce, vegetables, toppings)
+
+max_len = len(max(data, key=len))
+total = 0
+str_len = 0
+
+for elm in data:
+    total += chain_map[elm] * counter[elm]
+    str1 = f'{elm.ljust(max_len)} x {counter[elm]}'
+    str_len = len(str1)
+    print(str1)
+
+str2 = f'ИТОГ: {total}р'
+if len(str2) > str_len:
+    str_len = len(str2)
+
+print('-' * str_len)
+print(str2)
