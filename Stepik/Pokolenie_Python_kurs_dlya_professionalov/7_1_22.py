9c1805b3ed5810baf39b5b68b125d80842183165
Muzyria <fila090580@gmail.com>
2022-10-03 02:44:24 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_1_22.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_1_22.py
@@ -1,11 +1,9 @@
 def swapcase_vowels(string):
     vowels = 'aeiouy'
     swapped_string = ''
-
     for char in string:
-        if char.lewer() in vowels:
-            swapped_string += char.upper()
-        else:
-            swapped_string += char
+        if char in vowels:
+            char = char.upper()
+        swapped_string += char
 
     return swapped_string
