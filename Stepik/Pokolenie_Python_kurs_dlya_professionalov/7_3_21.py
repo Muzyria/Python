fa17be7152b8a0749013746f09b765a0d72085ab
Muzyria <fila090580@gmail.com>
2022-10-03 20:54:30 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_3_21.py
@@ -0,0 +1,15 @@
+
+def add_to_list_in_dict(data: dict, key, element):
+    try:
+        data[key].append(element)
+    except KeyError:
+        data[key] = [element]
+
+
+data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
+add_to_list_in_dict(data, 'b', 7)
+print(data)     # {'a': [1, 2, 3], 'b': [4, 5, 6, 7]}
+
+data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
+add_to_list_in_dict(data, 'c', 7)
+print(data)     # {'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7]}
