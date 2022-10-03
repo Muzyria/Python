eec82adbf29908133d37fcf36dfb3555995a45f3
Muzyria <fila090580@gmail.com>
2022-10-03 20:25:25 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_3_20.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_3_20.py
@@ -1,10 +1,9 @@
-import calendar
+from calendar import month_name
 
 
-name = list(calendar.month_name)
-
-
-
-
-
-
+try:
+    print(dict(enumerate(month_name[1:], 1))[int(input())])
+except KeyError:
+    print('Введено число из недопустимого диапазона')
+except:
+    print('Введено некорректное значение')
