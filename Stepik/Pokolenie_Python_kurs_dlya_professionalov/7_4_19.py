23b6fb8cef9d310c908ae548ecaf6601286b15bf
Muzyria <fila090580@gmail.com>
2022-10-04 20:02:05 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_4_19.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_4_19.py
@@ -6,27 +6,27 @@ def get_id(names: list, name: str):
     return len(names) + 1
 
 
-names = ['Timur', 'Anri', 'Dima']
-name = 'Arthur'
-print(get_id(names, name))
-
-names = ['Timur', 'Anri', 'Dima', 'Arthur']
-name = 'Ruslan1337'
-try:
-    print(get_id(names, name))
-except ValueError as e:
-    print(e)
-
-names = ['Timur', 'Anri', 'Dima', 'Arthur', 'Ruslan']
-name = ['E', 'd', 'u', 'a', 'r', 'd']
-try:
-    print(get_id(names, name))
-except TypeError as e:
-    print(e)
-
-names = ['Timur', 'Anri', 'Dima', 'Arthur']
-name = 'ruslan'
-try:
-    print(get_id(names, name))
-except ValueError as e:
-    print(e)
+# names = ['Timur', 'Anri', 'Dima']
+# name = 'Arthur'
+# print(get_id(names, name))
+#
+# names = ['Timur', 'Anri', 'Dima', 'Arthur']
+# name = 'Ruslan1337'
+# try:
+#     print(get_id(names, name))
+# except ValueError as e:
+#     print(e)
+#
+# names = ['Timur', 'Anri', 'Dima', 'Arthur', 'Ruslan']
+# name = ['E', 'd', 'u', 'a', 'r', 'd']
+# try:
+#     print(get_id(names, name))
+# except TypeError as e:
+#     print(e)
+#
+# names = ['Timur', 'Anri', 'Dima', 'Arthur']
+# name = 'ruslan'
+# try:
+#     print(get_id(names, name))
+# except ValueError as e:
+#     print(e)
