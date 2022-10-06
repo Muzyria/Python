1a3c225cc27de9756f649458a458b4d8d675517f
“Muzyria” <fila090580@gmail.com>
2022-10-06 12:25:44 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_4_20.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/7_4_20.py
@@ -1,1 +1,16 @@
+import json
+
+
+try:
+    file_name = input()
+    file = open(file_name)
+    try:
+        data = json.load(file)
+        print(data)
+    except json.decoder.JSONDecodeError:
+        print('Ошибка при десериализации')
+    finally:
+        file.close()
+except FileNotFoundError:
+    print('Файл не найден')
 
