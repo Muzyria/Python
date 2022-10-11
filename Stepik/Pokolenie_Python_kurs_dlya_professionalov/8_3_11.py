4b6b181f79634b053a690cc10d5e557e7c1ffc2d
“Muzyria” <fila090580@gmail.com>
2022-10-11 14:48:28 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/8_3_11.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/8_3_11.py
@@ -1,8 +1,10 @@
 def get_fast_pow(a: int, n: int):
     if n == 0:
         return 1
+    if n % 2 == 0:
+        return get_fast_pow(a * a, n / 2)
     else:
-        return get_fast_pow(a * a, n // 2)
+        return a * get_fast_pow(a * a, n // 2)
 
 
 print(get_fast_pow(2, 10))
