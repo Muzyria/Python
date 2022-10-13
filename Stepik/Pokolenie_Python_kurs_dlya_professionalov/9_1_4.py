0e23258dff89edacab1885074199cbb5b3105fa5
“Muzyria” <fila090580@gmail.com>
2022-10-13 16:58:10 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_1_4.py
@@ -0,0 +1,13 @@
+from string import ascii_lowercase
+
+for i in ascii_lowercase:
+    print(i)
+
+
+# def rec(a: str) -> None:
+#     if a != chr(ord('z') + 1):
+#         print(a)
+#         rec(chr(ord(a) + 1))
+#
+#
+# rec('a')
