1bbc48c2294827b05a4a0b885fc0c85005d9d48f
“Muzyria” <fila090580@gmail.com>
2022-10-13 16:33:59 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/8_4_7.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/8_4_7.py
@@ -1,29 +1,31 @@
-def get_all_values(nested_dicts, key, d=list()):
+
+
+def get_all_values(nested_dicts, key, d=set()):
     if key in nested_dicts:
-        # return nested_dicts[key]
-        d.append(nested_dicts[key])
+        d.add(str(nested_dicts[key]))
+        # return str(nested_dicts[key])
 
     for k, v in nested_dicts.items():
         if type(v) == dict:
-            value = get_all_values(v, key)
-            # if value is not None:
-            #     # print(value)
-            #     # return value
-            #     d.append(value)
+            get_all_values(v, key)
+
     return d
 
 
 my_dict = {'users': {'Arthur': {'grades': [4, 4, 3], 'top_grade': 4}, 'Timur': {'grades': [5, 5, 5], 'top_grade': 5}}}
 result = get_all_values(my_dict, 'top_grade')
 print(*sorted(result))
+# 4 5
 
 my_dict = {'Arthur': {'hobby': 'videogames', 'drink': 'cacao'}, 'Timur': {'hobby': 'math'}}
 result = get_all_values(my_dict, 'hobby')
 print(*sorted(result))
+# math videogames
 
 my_dict = {'Arthur': {'hobby': 'videogames', 'drink': 'cacao'}, 'Timur': {'hobby': 'math'}}
 result = get_all_values(my_dict, 'top_grade')
 print(len(sorted(result)))
+# 0
 
 # my_dict = {'users': {'Arthur': {'grades': [4, 4, 3], 'top_grade': 4}, 'Timur': {'grades': [5, 5, 5], 'top_grade': 5}}}
 # result = get_all_values(my_dict, 'top_grade')
@@ -32,7 +34,6 @@ print(len(sorted(result)))
 # my_dict = {'Arthur': {'hobby': 'videogames', 'drink': 'cacao'}, 'Timur': {'hobby': 'math'}}
 # result = get_all_values(my_dict, 'hobby')
 # print(*sorted(result))
-
 # 4 5
 # <class 'set'>
 # math videogames
