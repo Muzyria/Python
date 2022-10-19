8688f59b96a44c6896651c0cb7a2a1b9b05b6650
“Muzyria” <fila090580@gmail.com>
2022-10-19 17:01:47 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_4_12.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_4_12.py
@@ -1,8 +1,15 @@
-new_print = print
+# new_print = print
+#
+#
+# def print(*args, sep=" ", end=""):
+#     a = map(lambda x: x.upper() if isinstance(x, str) else x, args)
+#     new_print(*a, sep=sep.upper(), end=end.upper())
+
+my_func = print
 
 
-def print(*args, sep=" ", end=""):
-    a = map(lambda x: x.upper() if isinstance(x, str) else x, args)
-    new_print(*a, sep=sep.upper(), end=end.upper())
+def print(*args,  sep=' ', end='\n'):
+    lst = map(lambda x: x.upper() if type(x) is str else x, args)
+    my_func(*lst, sep=sep.upper(), end=end.upper())
 
 print('beegeek', [1, 2, 3], 4)
