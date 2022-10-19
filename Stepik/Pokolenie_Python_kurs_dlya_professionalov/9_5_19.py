f0997af37684519f124c5fc6e582d5be1626e47f
Muzyria <fila090580@gmail.com>
2022-10-19 23:29:13 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_5_19.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+d = {'ru': '%d.%m.%Y',
+     'us': '%m-%d-%Y',
+     'ca': '%Y-%m-%d',
+     'br': '%d/%m/%Y',
+     'fr': '%d.%m.%Y',
+     'pt': '%d-%m-%Y', }
+
+
+def date_formatter(country_code):
+    d_f = {'ru': '%d.%m.%Y',
+           'us': '%m-%d-%Y',
+           'ca': '%Y-%m-%d',
+           'br': '%d/%m/%Y',
+           'fr': '%d.%m.%Y',
+           'pt': '%d-%m-%Y', }
+
+    def func(x):
+        return date.strftime(x, d_f[country_code])
+
+    return func
+
+
+date_ru = date_formatter('ru')
+today = date(2022, 1, 25)
+print(date_ru(today))
