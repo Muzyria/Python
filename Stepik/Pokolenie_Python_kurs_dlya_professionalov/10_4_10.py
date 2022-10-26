9aa0f0182636098afcb922aa6b1963eca90d50e9
“Muzyria” <fila090580@gmail.com>
2022-10-26 13:56:31 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_10.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_10.py
@@ -6,7 +6,8 @@ class Fibonacci:
         return self
 
     def __next__(self):
-        pass
+        fib = lambda x: 1 if x in (1, 2) else fib(x - 1) + fib(x - 2)
+        return
 
 
 fibonacci = Fibonacci()
