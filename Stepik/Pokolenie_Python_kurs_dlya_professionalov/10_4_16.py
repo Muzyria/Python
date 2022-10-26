5dbbc41895cba3990d4a1b2eadac8170e2e2db71
Muzyria <fila090580@gmail.com>
2022-10-27 00:08:06 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_16.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_16.py
@@ -1,9 +1,26 @@
 class Alphabet:
-    def __init__(self, left, right, n):
-        pass
+    DI = {'en': 'abcdefghijklmnopqrstuvwxyz', 'ru': 'абвгдежзийклмнопрстуфхцчшщъыьэюя'}
+
+    def __init__(self, language):
+        self.alp = iter(Alphabet.DI[language])
+        self.language = language
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        pass
+        try:
+            return next(self.alp)
+        except StopIteration:
+            self.alp = iter(Alphabet.DI[self.language])
+            return next(self.alp)
+
+
+ru_alpha = Alphabet('ru')
+print(next(ru_alpha))
+print(next(ru_alpha))
+print(next(ru_alpha))
+
+en_alpha = Alphabet('en')
+letters = [next(en_alpha) for _ in range(58)]
+print(*letters)
