485869c2b468eb9c64bcc4e0ee02613de03683b1
“Muzyria” <fila090580@gmail.com>
2022-10-26 13:44:37 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_9.py
@@ -0,0 +1,22 @@
+class Square:
+    def __init__(self, n):
+        self.n = n
+        self.i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i != self.n:
+            self.i += 1
+            return self.i ** 2
+        else:
+            raise StopIteration
+
+
+squares = Square(2)
+print(next(squares))
+print(next(squares))
+
+squares = Square(10)
+print(list(squares))
