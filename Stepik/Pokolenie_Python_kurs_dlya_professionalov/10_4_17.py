e9c7a60a15bda3435732e95f03239edeef8ae6ca
Muzyria <fila090580@gmail.com>
2022-10-27 00:23:45 +0300

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_4_17.py
@@ -0,0 +1,28 @@
+class Xrange:
+    def __init__(self, start, end, step=1):
+        self.start = start - step
+        self.end = end
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start != self.end - self.step:
+            self.start += self.step
+            return self.start
+        else:
+            raise StopIteration
+
+
+evens = Xrange(0, 10, 2)
+print(*evens)
+
+xrange = Xrange(0, 3, 0.5)
+print(*xrange, sep='; ')
+
+xrange = Xrange(10, 1, -1)
+print(*xrange)
+
+xrange = Xrange(5, 10)
+print(*xrange)
