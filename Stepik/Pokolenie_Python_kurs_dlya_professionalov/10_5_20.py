56d1afaf226f7d3639eee3ad7fbae943d8806088
Muzyria <fila090580@gmail.com>
2022-10-28 22:27:32 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_20.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_20.py
@@ -1,6 +1,18 @@
 def card_deck(suit):
     card_values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "валет", "дама", "король", "туз"]
     card_mast = ["пик", "треф", "бубен", "червей"]
+    ind = 0
+    while True:
+        try:
+            for i in card_values:
+                if ind != card_mast.index(suit):
+                    yield f'{i} {card_mast[ind]}'
+                else:
+                    continue
+            ind += 1
+        except IndexError:
+            ind = 0
+
 
 
 
