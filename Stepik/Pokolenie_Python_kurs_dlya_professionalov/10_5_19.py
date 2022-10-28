5a7264abccb353fbfcb87f6cb170f6b883190fc3
Muzyria <fila090580@gmail.com>
2022-10-28 18:48:46 +0300

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_19.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_19.py
@@ -5,8 +5,11 @@ def dates(start, count=None):
     start = date.toordinal(start)
     if count is None:
         while True:
-            yield date.fromordinal(start)
-            start += 1
+            if start <= 3652059:
+                yield date.fromordinal(start)
+                start += 1
+            else:
+                return None
     else:
         for i in range(start, start + count):
             yield date.fromordinal(i)
@@ -19,3 +22,36 @@ print(next(generator))
 
 generator = dates(date(2022, 3, 8), 5)
 print(*generator)
+
+generator = dates(date(9999, 1, 7))
+
+for _ in range(348):
+    next(generator)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+try:
+    print(next(generator))
+except StopIteration:
+    print('Error')
+# 9999-12-21
+# 9999-12-22
+# 9999-12-23
+# 9999-12-24
+# 9999-12-25
+# 9999-12-26
+# 9999-12-27
+# 9999-12-28
+# 9999-12-29
+# 9999-12-30
+# 9999-12-31
+# Error
+
