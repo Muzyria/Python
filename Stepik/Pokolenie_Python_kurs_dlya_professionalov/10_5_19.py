c3fbf540ed72af1838aa7b174a1937e6a306473c
“Muzyria” <fila090580@gmail.com>
2022-10-31 13:19:13 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_19.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_5_19.py
@@ -55,3 +55,35 @@ except StopIteration:
 # 9999-12-31
 # Error
 
+generator = dates(date(9999, 1, 7))
+
+for _ in range(348):
+    next(generator)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+try:
+    print(next(generator))
+except StopIteration:
+    print('Error')
+# 9999-12-21
+# 9999-12-22
+# 9999-12-23
+# 9999-12-24
+# 9999-12-25
+# 9999-12-26
+# 9999-12-27
+# 9999-12-28
+# 9999-12-29
+# 9999-12-30
+# 9999-12-31
+# Error
+
