107c696b5d15a59d39bd51388da27f11254d2c77
Muzyria <fila090580@gmail.com>
2022-10-31 19:53:22 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_7_10.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_7_10.py
@@ -1,8 +1,7 @@
 def nonempty_lines(file):
     with open(file, 'r', encoding='utf-8') as file:
-        file_lines = (line for line in file)
-
-
+        file_lines = (line.strip() if len(line) <= 25 else '...' for line in file if line.strip() != '')
+        yield from (line for line in file_lines)
 
 
 lines = nonempty_lines('file1.txt')
@@ -10,4 +9,4 @@ print(next(lines))
 print(next(lines))
 print(next(lines))
 
-print(*nonempty_lines('file2.txt'))
+# print(*nonempty_lines('file2.txt'))
