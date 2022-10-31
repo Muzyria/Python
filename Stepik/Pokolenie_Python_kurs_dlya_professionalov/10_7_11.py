bed697f31db246c98e4c5dd73715eabc0e9b9ce0
Muzyria <fila090580@gmail.com>
2022-10-31 21:59:40 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_7_11.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_7_11.py
@@ -2,10 +2,8 @@
 with open('planets.txt', 'r', encoding='utf-8') as file:
     file_lines = (line for line in file)
     line_values = (line.rstrip().split(' = ') for line in file_lines)
-        # line_dicts = (dict() for data in line_values)
-    for _ in range(4):
-        for i in line_values:
-            d = dict(i[0] = i[1])
+    line_dicts = ({data[0]: data[1]} for data in line_values)
+
 
 
 # planets = txt_to_dict()
