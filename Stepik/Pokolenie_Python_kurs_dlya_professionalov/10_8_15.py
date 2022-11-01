77ea5dcbe0db9c7e43386c662411339c4eecd4c2
Muzyria <fila090580@gmail.com>
2022-11-01 21:12:20 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_8_15.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_8_15.py
@@ -2,10 +2,16 @@ from itertools import count
 
 
 def tabulate(func):
-    count1 = count()
+    yield from (func(i) for i in count(1))
 
 
 func = lambda x: x
 values = tabulate(func)
+print(next(values))
+print(next(values))
+
+func = lambda x: x + 10
+values = tabulate(func)
+print(next(values))
 print(next(values))
 print(next(values))
