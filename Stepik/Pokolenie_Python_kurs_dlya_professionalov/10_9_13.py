33eebd2448a0686719f555f68d4af93ab8f56f9a
“Muzyria” <fila090580@gmail.com>
2022-11-02 15:33:37 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_9_13.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_9_13.py
@@ -1,8 +1,10 @@
 from itertools import dropwhile
 
 
-def drop_this(iterable, obj):
-    yield from dropwhile(lambda x: x == obj, iterable)
+# def drop_this(iterable, obj):
+#     yield from dropwhile(lambda x: x == obj, iterable)
+
+drop_this = lambda it, obj: dropwhile(lambda n: n == obj, it)
 
 
 numbers = [0, 0, 0, 1, 2, 3]
