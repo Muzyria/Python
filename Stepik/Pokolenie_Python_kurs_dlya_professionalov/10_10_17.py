a5d6d438a0773b04a793dc2ff0a6828adf0a834a
“Muzyria” <fila090580@gmail.com>
2022-11-03 13:02:53 +0200

    new

---
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_10_17.py
@@ -0,0 +1,17 @@
+from itertools import zip_longest
+
+
+def grouper(iterable, n):
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+print(*grouper(numbers, 2))
+# (1, 2) (3, 4) (5, 6)
+
+iterator = iter([1, 2, 3, 4, 5, 6, 7])
+print(*grouper(iterator, 3))
+# (1, 2, 3) (4, 5, 6) (7, None, None)
+
+iterator = iter([1, 2, 3])
+print(*grouper(iterator, 5))
+# (1, 2, 3, None, None)
