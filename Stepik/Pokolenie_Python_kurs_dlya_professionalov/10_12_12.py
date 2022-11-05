f91ebddbccd666da68883809813760c4791620a0
Muzyria <fila090580@gmail.com>
2022-11-05 23:20:14 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_12_12.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_12_12.py
@@ -1,5 +1,9 @@
-from itertools import permutations, combinations
-
-
+from itertools import combinations
 wallet = [100, 100, 50, 50, 50, 50, 20, 20, 20, 10, 10, 10, 10, 10, 5, 5, 1, 1, 1, 1, 1]
 
+count = 0
+for i in range(1, len(wallet) + 1):
+    for j in set(combinations(wallet, i)):
+        if sum(j) == 100:
+            count += 1
+print(count)
