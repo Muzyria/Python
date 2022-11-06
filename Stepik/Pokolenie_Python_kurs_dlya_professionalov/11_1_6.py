e94a616e5a503aacd76caa7bef5d3e32dd9b54b1
Muzyria <fila090580@gmail.com>
2022-11-06 19:58:19 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_1_6.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_1_6.py
@@ -1,7 +1,4 @@
 import re
 
-
-s = 'Артур: +7-919-667-21-19, Анри: 7-hey-anri-anri, Тимур: 8-917-4864-1911'
-
-match = re.findall('7-\d\d\d-\d\d\d-\d\d-\d\d' or '8-\d\d\d-\d\d\d\d-\d\d\d\d', s)
-print(match)
+# reg = re.findall(r'7-\d{3}-\d{3}-\d{2}-\d{2}|8-\d{3}-\d{4}-\d{4}', input())
+print(*re.findall(r'7-\d{3}-\d{3}-\d{2}-\d{2}|8-\d{3}-\d{4}-\d{4}', input()), sep='\n')
