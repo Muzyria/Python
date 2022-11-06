e89c5bea51c2f674be515b5b8baadb5029a33fcb
Muzyria <fila090580@gmail.com>
2022-11-06 11:28:46 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_12_22.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/10_12_22.py
@@ -3,4 +3,8 @@ from itertools import product
 d = {0: "0", 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C',
      13: 'D', 14: 'E', 15: 'F'}
 
+n = 2
+m = 3
 
+for i in product(range(n), repeat=m):
+    print(i)
