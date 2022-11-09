ccd824c3d2a0109a680f98d2add00c735922f2a0
Muzyria <fila090580@gmail.com>
2022-11-09 21:07:04 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_7_16.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_7_16.py
@@ -1,6 +1,6 @@
-import re
+import re, sys
 
-for i in range(int(input())):
-    res = re.findall(r'<a href="(.*)">(.*)</a>', input())
+for i in sys.stdin:
+    res = re.findall(r'<a href="(.*)">(.*)</a>', i)
     if res:
         print(res[0][0], res[0][1], sep=', ')
