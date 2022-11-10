755348b1c37ef28ab1ff8ef179db4d5026eeca9e
“Muzyria” <fila090580@gmail.com>
2022-11-10 11:12:01 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_8_12.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/11_8_12.py
@@ -1,4 +1,14 @@
 import re
 
 
-print(re.sub(r'\b(\w)(\w)', r'\g<2>\g<1>', input()))
+def fun(a):
+    a1 = int(a.group(1))
+    a2 = a.group(2)
+    return a1*a2
+
+
+a = re.sub(r'(\d+)\((\w+)\)', fun, input())
+
+while '(' in a:
+    a = re.sub(r'(\d+)\((\w+)\)', fun, a)
+print(a)
