1ccf88bb68275b385e39ebe736021d0d0fb5918d
“Muzyria” <fila090580@gmail.com>
2022-11-10 15:08:04 +0200

    new

--- Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_9_11.py
+++ Stepik/Pokolenie_Python_kurs_dlya_professionalov/9_9_11.py
@@ -5,20 +5,7 @@ def send_email(name, email_address, text):
     return f'В письме для {name} на адрес {email_address} сказано следующее: {text}'
 
 
-t = 'Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....'
-e = "timyrik20@beegeek.ru"
-n = "Тимур"
+to_Timur = partial(send_email, 'Тимур', 'timyrik20@beegeek.ru')
 
-to_Timur = partial(send_email, t)
+send_an_invitation = partial(send_email, text='Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....')
 
-send_an_invitation = partial(to_Timur, n, e)
-
-
-print(send_an_invitation())
-
-try:
-    to_Timur('первое', 'второе')
-except:
-    print('ok')
-
-print(send_an_invitation("Тимур", "timyrik20@beegeek.ru"))
