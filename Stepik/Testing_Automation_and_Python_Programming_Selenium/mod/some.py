588ef677a74bc135fc410171f6e1b1dd004ce931
“Muzyria” <fila090580@gmail.com>
2022-11-14 13:34:08 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/mod/some.py
@@ -0,0 +1,6 @@
+def sum(a, b):
+    print(f'suma = {a + b}')
+
+
+def sub(a, b):
+    print(f'raznica = {a - b}')
--- Stepik/Testing_Automation_and_Python_Programming_Selenium/start.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/start.py
@@ -1,3 +1,11 @@
 from hello import some
+import math
+# from mod import math
+from mod import some
 
-some()
+
+# some()
+# print(math.pi)
+
+some.sum(10, 5)
+some.sub(10, 5)
