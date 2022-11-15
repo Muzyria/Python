7fa63fc58d58a573cd0db4009e374557cc60be7c
“Muzyria” <fila090580@gmail.com>
2022-11-15 13:05:48 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/2_24_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/2_24_1.py
@@ -1,11 +1,22 @@
-import sys
+# import sys
+#
+#
+# print('Ведите корректное математическое выражение (в фортмате 2 + 2)')
+# for line in sys.stdin:
+#     try:
+#         print(f' = {eval(line)}')
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя')
+#     except Exception:
+#         print('Не корректные данные')
 
 
-print('Ведите корректное математическое выражение (в фортмате 2 + 2)')
-for line in sys.stdin:
-    try:
-        print(f' = {eval(line)}')
-    except ZeroDivisionError:
-        print('На ноль делить нельзя')
-    except Exception:
-        print('Не корректные данные')
+try:
+    a = input('Введите 1-е число ')
+    z = input('Введите арифметический знак, один из: +, -, *, / ')
+    b = input('Введите 2-е число ')
+    print(eval(f'{a}{z}{b}'))
+except ZeroDivisionError:
+    print('На ноль делить нельзя')
+except Exception:
+    print('Не корректные данные')
