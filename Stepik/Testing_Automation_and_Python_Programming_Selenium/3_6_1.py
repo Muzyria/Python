481013550ab8bb157015374a2b70614cb53a407e
Muzyria <fila090580@gmail.com>
2022-11-17 00:06:33 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/3_6_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/3_6_1.py
@@ -8,7 +8,7 @@ class Car:
         self.mileage = mileage
         self.wheels = wheels
 
-    def description_car(self):
+    def description(self):
         print(f'модель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size},'
               f'\nцена - {self.price},\nпробег - {self.mileage},\nколичество колес - {self.wheels}')
         print()
@@ -18,14 +18,14 @@ class Track(Car):
     def __int__(self, model, year, engine_size, price, mileage, wheels=8):
         super.__init__(model, year, engine_size, price, mileage)
 
-    def description_car(self):
+    def description(self):
         print(f'модель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size},'
               f'\nцена - {self.price},\nпробег - {self.mileage},\nколичество колес - {self.wheels}')
         print()
 
 
 my_car = Car('Toyota', 2020, 2000, 15000, 10000)
-my_car.description_car()
+my_car.description()
 
 my_track = Car('Kamaz', 1990, 4000, 25000, 500000)
-my_track.description_car()
+my_track.description()
