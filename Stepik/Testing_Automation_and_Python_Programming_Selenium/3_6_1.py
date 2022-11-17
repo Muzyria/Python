e701507a5765bb4d457c6962a593e678f4dd4eed
“Muzyria” <fila090580@gmail.com>
2022-11-17 11:39:33 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/3_6_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/3_6_1.py
@@ -1,25 +1,26 @@
 
 class Car:
-    def __init__(self, model, year, engine_size, price, mileage, wheels=4):
+    def __init__(self, model, year, engine_size, price, mileage):
         self.model = model
         self.year = year
         self.engine_size = engine_size
         self.price = price
         self.mileage = mileage
-        self.wheels = wheels
+        self.wheels = 4
 
     def description(self):
-        print(f'модель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size},'
+        print(f'Характеристики автомобиля \nмодель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size}, '
               f'\nцена - {self.price},\nпробег - {self.mileage},\nколичество колес - {self.wheels}')
         print()
 
 
 class Track(Car):
-    def __int__(self, model, year, engine_size, price, mileage, wheels=8):
-        super.__init__(model, year, engine_size, price, mileage)
+    def __init__(self, model, year, engine_size, price, mileage):
+        super().__init__(model, year, engine_size, price, mileage)
+        self.wheels = 8
 
     def description(self):
-        print(f'модель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size},'
+        print(f'Характеристики грузовика\nмодель - {self.model},\nгод выпуска - {self.year},\nобъем двигателя - {self.engine_size},'
               f'\nцена - {self.price},\nпробег - {self.mileage},\nколичество колес - {self.wheels}')
         print()
 
@@ -27,5 +28,5 @@ class Track(Car):
 my_car = Car('Toyota', 2020, 2000, 15000, 10000)
 my_car.description()
 
-my_track = Car('Kamaz', 1990, 4000, 25000, 500000)
+my_track = Track('Kamaz', 1990, 4000, 25000, 500000)
 my_track.description()
