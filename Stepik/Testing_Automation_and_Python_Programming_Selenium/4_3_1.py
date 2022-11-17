7226b60f6ca9a71e05bfc137d7bd782b3d314989
“Muzyria” <fila090580@gmail.com>
2022-11-17 13:37:27 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome(executable_path='C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver.get('https://www.saucedemo.com')
