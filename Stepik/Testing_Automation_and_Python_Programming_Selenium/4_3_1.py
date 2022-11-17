0bbb7e3db6d5b1d16f7217e189001c05d0c903a5
“Muzyria” <fila090580@gmail.com>
2022-11-17 15:02:15 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
@@ -1,5 +1,16 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
 
-driver = webdriver.Chrome(executable_path='C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
-driver.get('https://www.saucedemo.com')
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+
+user_name = driver.find_element(By.ID, "user-name")
+user_name.send_keys("standart_user")
+
+time.sleep(3)
+driver.close()
