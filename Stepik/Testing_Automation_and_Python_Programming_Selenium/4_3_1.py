c233c8a4190ba73a69de5aefb056ea2b48d0bcf3
Muzyria <fila090580@gmail.com>
2022-11-20 11:29:29 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')  # win
-s = Service(r'/home/fila/Git_Muzyria/Stepik/Testing_Automation_and_Python_Programming_Selenium/chromedriver')  # linux
-driver = webdriver.Chrome(service=s)
+s = Service()  # linux
+driver = webdriver.Chrome()
 
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
