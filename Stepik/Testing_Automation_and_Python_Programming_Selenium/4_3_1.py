151d761488bc7654224bd538b0194fe78012009e
“Muzyria” <fila090580@gmail.com>
2022-11-21 11:26:47 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
@@ -3,17 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')  # win
-s = Service()  # linux
-driver = webdriver.Chrome()
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
 
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
 
-# user_name = driver.find_element(By.ID, "user-name")  # ID
-# user_name = driver.find_element(By.NAME, "user-name")  # NAME
-user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')  # XPATCH
-
+user_name = driver.find_element(By.ID, "user-name")
 user_name.send_keys("standart_user")
 
 time.sleep(3)
