75f0fe6702e777fb3a0b44bea6e1e096cde26a13
“Muzyria” <fila090580@gmail.com>
2022-11-22 16:05:04 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_3_1.py
@@ -12,13 +12,15 @@ driver.maximize_window()
 # user_name = driver.find_element(By.ID, "user-name")  # ID
 # user_name = driver.find_element(By.NAME, "user-name")  # NAME
 # user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')  # XPATH
-# user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  #ID XPATH
-user_name = driver.find_element(By.XPATH, '//input[@data-test="username"]')  #data-test XPATH
-user_name.send_keys("standart_user")
+# user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # ID XPATH
+user_name = driver.find_element(By.XPATH, '//input[@data-test="username"]')  # data-test XPATH
+user_name.send_keys("standard_user")
 
-password = driver.find_element(By.CSS_SELECTOR, '#password')  #data-test XPATH
+password = driver.find_element(By.CSS_SELECTOR, '#password')  # CSS
 password.send_keys("secret_sauce")
 
+button_login = driver.find_element(By.XPATH, "//input[@value='Login']")  # XPATH
+button_login.click()
 
 time.sleep(3)
 driver.close()
