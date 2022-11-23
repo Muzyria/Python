c3a36e3ac9e52639e3979ed8489de3899f7ca51e
“Muzyria” <fila090580@gmail.com>
2022-11-23 11:41:29 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_1.py
@@ -11,13 +11,13 @@ driver.get(base_url)
 driver.maximize_window()
 
 login_standard_user = "standard_user"
-pass_word = "secret_sauce"
+password_all = "secret_sauce"
 
 user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
 user_name.send_keys(login_standard_user)
 
 password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
-password.send_keys(pass_word)
+password.send_keys(password_all)
 
 button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
 button_login.click()
