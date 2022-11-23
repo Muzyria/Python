9815273819636816ddddaba9efe13a005cd390ad
“Muzyria” <fila090580@gmail.com>
2022-11-23 11:35:33 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/test_1.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+
+user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
+user_name.send_keys("standard_user")
+
+password = driver.find_element(By.XPATH, '//input[@id="password"]')  # CSS
+password.send_keys("secret_sauce")
+
+button_login = driver.find_element(By.XPATH, "//input[@value='Login']")  # XPATH
+button_login.click()
+
+time.sleep(3)
+driver.close()
