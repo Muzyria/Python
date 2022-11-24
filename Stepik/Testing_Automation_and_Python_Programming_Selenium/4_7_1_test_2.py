a93417fd0d3c42df79d3469c84b59f237e4648c8
“Muzyria” <fila090580@gmail.com>
2022-11-24 14:00:38 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_2.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_2.py
@@ -31,5 +31,7 @@ value_warring_text = warring_text.text
 assert value_warring_text == "Epic sadface: Username and password do not match any user in this service"
 print('Good test')
 
+driver.refresh()
+
 time.sleep(2)
 driver.close()
