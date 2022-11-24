2166b120e79e48a4684c6cc266199f850755f4c1
“Muzyria” <fila090580@gmail.com>
2022-11-24 14:03:24 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_3.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_7_1_test_3.py
@@ -10,29 +10,21 @@ base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
 
-
-login_standard_user = "standard_use"  # incorect login
+login_standard_user = "standard_user"
 password_all = "secret_sauce"
 
 user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
 user_name.send_keys(login_standard_user)
 print('Input Login')
 
-password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
-password.send_keys(password_all)
-print('Input Password')
+# password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
+# password.send_keys(password_all)
+# print('Input Password')
+#
+# button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
+# button_login.click()
+# print('Click Login Button')
 
-button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
-button_login.click()
-print('Click Login Button')
-
-warring_text = driver.find_element(By.XPATH, '//h3[@data-test="error"]')
-value_warring_text = warring_text.text
-
-assert value_warring_text == "Epic sadface: Username and password do not match any user in this service"
-print('Good test')
-
-driver.refresh()
 
 time.sleep(2)
 driver.close()
