9ca6ee4a0a31f90af8eb9bc061f1eaae5f68986a
“Muzyria” <fila090580@gmail.com>
2022-11-24 15:45:38 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_12_1_test_4.py
@@ -0,0 +1,36 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+password_all = "secret_sauce"
+
+user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
+user_name.send_keys(login_standard_user)
+print('Input Login')
+
+password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
+password.send_keys(password_all)
+print('Input Password')
+
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
+button_login.click()
+print('Click Login Button')
+
+now_date = datetime.datetime.utcnow().strftime('%Y.%m.%d.%H.%M.%S')
+name_screenshot = 'screenshot_' + now_date + '.png'
+driver.save_screenshot(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\screen\\'
+                       + name_screenshot)
+
+
+time.sleep(2)
+driver.close()
