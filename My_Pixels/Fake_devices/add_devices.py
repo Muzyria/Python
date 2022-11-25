bc292a3b3450a9ce27140745fd4c101dc85c2ea0
“Muzyria” <fila090580@gmail.com>
2022-11-25 16:54:14 +0200

    new

--- My_Pixels/Fake_devices/add_devices.py
+++ My_Pixels/Fake_devices/add_devices.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 
 s = Service(r'C:\Git_Muzyria\Python\Python\My_Pixels\driver\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
+driver.implicitly_wait(20)
 
 base_url = 'https://control.syncwise360.com/#login'
 driver.get(base_url)
@@ -41,16 +42,27 @@ result_salt.click()
 print('Click result')
 time.sleep(1)
 
-action = ActionChains(driver)
-add_new_car = driver.find_element(By.XPATH, '//*[@id="workarea"]/div[2]/section/div[3]/div[5]/table[3]/thead/tr/th[4]')
-action.move_to_element(add_new_car).perform()
-add_new_car.click()
-print('Click add new car')
-time.sleep(1)
 
 id_car_start = 'S101500002110180'  #  without two simbol
 for i in range(70, 91):
     new_id = f'{id_car_start}{i}'
+    print(new_id)
+
+    action = ActionChains(driver)
+    add_new_car = driver.find_element(By.XPATH,
+                                      '//*[@id="workarea"]/div[2]/section/div[3]/div[5]/table[3]/thead/tr/th[4]')
+    action.move_to_element(add_new_car).perform()
+    add_new_car.click()
+    print('Click add new car')
+    time.sleep(1)
+
+    select_devise_model = driver.find_element(By.XPATH, '//select[@name="id_deviceModel"]')
+    select_devise_model.click()
+
+    driver.refresh()
+    time.sleep(4)
+
+
 
 # driver.execute_script('window.scrollTo(0, 500)')
 # time.sleep(2)
