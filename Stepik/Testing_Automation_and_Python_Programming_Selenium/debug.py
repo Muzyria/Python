ad230effdd0fed9a5df9e35fbfab884f4d0e3dbc
Muzyria <fila090580@gmail.com>
2022-11-27 10:40:48 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/debug.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/debug.py
@@ -1,17 +1,2 @@
-import time
-
-# webdriver это и есть набор команд для управления браузером
-from selenium import webdriver
-
-# импортируем класс By, который позволяет выбрать способ поиска элемента
-from selenium.webdriver.common.by import By
-
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Chrome()
-
-# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
-
-# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://stepik.org/lesson/25969/step/12")
-time.sleep(5)
+import subprocess
+subprocess.call("test.sh")
