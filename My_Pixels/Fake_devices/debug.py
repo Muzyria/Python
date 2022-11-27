b49ba06ef95532704e98f40f6bbacd7a1b7c0b78
Muzyria <fila090580@gmail.com>
2022-11-27 11:13:14 +0200

    new

--- My_Pixels/Fake_devices/debug.py
+++ My_Pixels/Fake_devices/debug.py
@@ -2,4 +2,4 @@
 # subprocess.call("test.sh")
 
 import os
-os.system('adb version')
+os.system('adb shell am broadcast -a com.yama.fake.ADBCom --es id "S1123555ABABABBB55" --es lat "50.08593724592065" --es lng "36.21560508169411"')
