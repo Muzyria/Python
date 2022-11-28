327fa0e85403897d193eec01366996b504889ea1
“Muzyria” <fila090580@gmail.com>
2022-11-28 10:59:13 +0200

    new

--- My_Pixels/Fake_devices/debug.py
+++ My_Pixels/Fake_devices/debug.py
@@ -3,4 +3,9 @@
 # subprocess.call("test.sh")
 
 import os
-os.system('adb shell am broadcast -a com.yama.fake.ADBCom --es id "S1123555ABABABBB55" --es lat "50.08593724592065" --es lng "36.21560508169411"')
+# os.system('adb shell am broadcast -a com.yama.fake.ADBCom --es id "S1123555ABABABBB55" --es lat "50.08593724592065" --es lng "36.21560508169411"')
+os.system('adb shell am start -a android.intent.action.MAIN -n com.android.settings/.wifi.WifiSettings')
+os.system('adb shell input tap 800 100')
+os.system('adb shell input keyevent 4')
+os.system('')
+os.system('')
