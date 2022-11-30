024cbe34b0c0e0db5534bef91cb5f2fb58ac37a9
“Muzyria” <fila090580@gmail.com>
2022-11-30 14:48:00 +0200

    new

--- My_Pixels/Fake_devices/run.py
+++ My_Pixels/Fake_devices/run.py
@@ -8,18 +8,18 @@ test_49 = 'S10150000211018049'
 for i in ['S101500002110180' + str(i) for i in range(70, 91)]:
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
-    print(i, x, y)
+    print(i, x, y, 'На поле')
     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{i}" --es lat "{x}" --es lng "{y}"')
     time.sleep(1)
 
 
-for _ in range(3600):
+for count in range(3600):
     car_id = choice(['S101500002110180' + str(i) for i in range(70, 91)])
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
-    print(car_id, x, y)
+    print(car_id, x, y, ' -------------------------> ' + str(count))
     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{car_id}" --es lat "{x}" --es lng "{y}"')
-    time.sleep(1)
+    time.sleep(5)
 
 # lst_car = ['S101500002110180' + str(i) for i in range(70, 91)]
 
