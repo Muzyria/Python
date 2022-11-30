59a2494131bc2876b21f7141c2fb953488990734
“Muzyria” <fila090580@gmail.com>
2022-11-30 15:35:33 +0200

    new

--- My_Pixels/Fake_devices/run.py
+++ My_Pixels/Fake_devices/run.py
@@ -1,9 +1,11 @@
 import os
+from datetime import datetime
 from random import choice, randrange
 import time
 
 test_49 = 'S10150000211018049'
 
+start_time = datetime.now() # включаем счетчик
 # Высадка дисанта
 for i in ['S101500002110180' + str(i) for i in range(70, 91)]:
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
@@ -14,10 +16,11 @@ for i in ['S101500002110180' + str(i) for i in range(70, 91)]:
 
 count_iter = 3600
 for j in range(count_iter):
+    count_time = str(datetime.now() - start_time).split('.')[0]
     car_id = choice(['S101500002110180' + str(i) for i in range(70, 91)])
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
-    print(car_id, x, y, ' -------------------------> ' + str(count_iter - j))
+    print(car_id, x, y, ' -------------------------> ' + str(count_iter - j), ' прошло времени ' + count_time)
     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{car_id}" --es lat "{x}" --es lng "{y}"')
     time.sleep(5)
 
