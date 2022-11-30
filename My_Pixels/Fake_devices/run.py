1fb4e26288eeef32a42d27c1502434d7765f752d
“Muzyria” <fila090580@gmail.com>
2022-11-30 14:53:06 +0200

    new

--- My_Pixels/Fake_devices/run.py
+++ My_Pixels/Fake_devices/run.py
@@ -10,14 +10,14 @@ for i in ['S101500002110180' + str(i) for i in range(70, 91)]:
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
     print(i, x, y, 'На поле')
     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{i}" --es lat "{x}" --es lng "{y}"')
-    time.sleep(1)
+    time.sleep(0)
 
-
-for count in range(3600):
+count_iter = 3600
+for j in range(count_iter):
     car_id = choice(['S101500002110180' + str(i) for i in range(70, 91)])
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
-    print(car_id, x, y, ' -------------------------> ' + str(count))
+    print(car_id, x, y, ' -------------------------> ' + str(count_iter - j))
     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{car_id}" --es lat "{x}" --es lng "{y}"')
     time.sleep(5)
 
