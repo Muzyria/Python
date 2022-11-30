f9ace5b39697e08932f4d74851cd6e03bd314b1e
“Muzyria” <fila090580@gmail.com>
2022-11-30 16:30:11 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_15_1_test_7.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_15_1_test_7.py
@@ -16,25 +16,12 @@ password_all = "secret_sauce"
 user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
 user_name.send_keys(login_standard_user)
 print('Input Login')
-# time.sleep(3)
-# user_name.clear()
-
 password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
 password.send_keys(password_all)
 print('Input Password')
-
 button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
 button_login.click()
 print('Click Login Button')
-
-menu = driver.find_element(By.XPATH, '//button[@id="react-burger-menu-btn"]')
-menu.click()
-print('Click menu button')
-time.sleep(2)
-link_about = driver.find_element(By.XPATH, '//a[@id="about_sidebar_link"]')
-link_about.click()
-print('Click link button')
-
 
 time.sleep(2)
 driver.close()
