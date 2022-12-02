f3c6f72e4356d5cf254870980286b7597729a092
“Muzyria” <fila090580@gmail.com>
2022-12-02 15:45:49 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_23_1_test_12(9).py
@@ -0,0 +1,21 @@
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+base_url = 'https://html5css.ru/howto/howto_js_rangeslider.php'
+driver.get(base_url)
+driver.maximize_window()
+
+# action = ActionChains(driver)
+# double = driver.find_element(By.XPATH, '//button[@id="doubleClickBtn"]')
+# action.double_click(double).perform()
+
+
+time.sleep(3)
+driver.close()
