50d95e323aa45cff0c68f6bb04eb312fac2d2ca5
“Muzyria” <fila090580@gmail.com>
2022-12-02 11:32:27 +0200

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_20_1_test_10.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service(r'C:\Git_Muzyria\Python\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+base_url = 'https://demoqa.com/buttons'
+driver.get(base_url)
+driver.maximize_window()
+
+
+action = ActionChains(driver)
+double = driver.find_element(By.XPATH, '//button[@id="doubleClickBtn"]')
+action.double_click(double).perform()
+
+
+time.sleep(2)
+driver.close()
