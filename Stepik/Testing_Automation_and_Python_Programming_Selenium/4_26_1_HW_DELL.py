2850ff9a4ee295f2b7b82d8124b78f138da777b5
Muzyria <fila090580@gmail.com>
2022-12-08 18:51:37 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_26_1_HW_DELL.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_26_1_HW_DELL.py
@@ -4,11 +4,22 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # Приветствие и выбор товара
-print('Приветствую тебя в нашем интеренет магазине')
-print('Выбери один из следующих товаров и укажи его номер: 1 - Sauce Labs Backpack, 2 - Sauce Labs Bike Light,'
-      ' 3 - Sauce Labs Bolt T-Shirt, 4 - Sauce Labs Fleece Jacket, 5 - Sauce Labs Onesie,'
-      ' 6 - Test.allTheThings() T-Shirt (Red)')
-product = int(input())
+while True:
+    product = None
+    print('Приветствую тебя в нашем интеренет магазине')
+    print('Выбери один из следующих товаров и укажи его номер: 1 - Sauce Labs Backpack, 2 - Sauce Labs Bike Light,'
+        ' 3 - Sauce Labs Bolt T-Shirt, 4 - Sauce Labs Fleece Jacket, 5 - Sauce Labs Onesie,'
+        ' 6 - Test.allTheThings() T-Shirt (Red)')
+    try:
+        product = int(input())
+        if product not in range(1, 7):
+            continue
+        else:
+            break
+    except ValueError:
+        continue
+print(product)
+
 
 locators = {1: {'name_product': '//a[@id="item_4_title_link"]',
                 'price_product': '//*[@id="inventory_container"]/div/div[1]/div[2]/div[2]/div',
--- Stepik/Testing_Automation_and_Python_Programming_Selenium/debug.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/debug.py
@@ -1,13 +1,15 @@
-locators = {1: {'name_product': '//a[@id="item_4_title_link"]',
-                'price_product': '//*[@id="inventory_container"]/div/div[1]/div[2]/div[2]/div',
-                'button_product': '//button[@id="add-to-cart-sauce-labs-backpack"]'},
-            2: {},
-            3: {},
-            4: {},
-            5: {},
-            6: {}}
-
-n = int(input())
-print(locators[n]['name_product'])
-print(locators[n]['price_product'])
-print(locators[n]['button_product'])
+while True:
+    product = None
+    print('Приветствую тебя в нашем интеренет магазине')
+    print('Выбери один из следующих товаров и укажи его номер: 1 - Sauce Labs Backpack, 2 - Sauce Labs Bike Light,'
+        ' 3 - Sauce Labs Bolt T-Shirt, 4 - Sauce Labs Fleece Jacket, 5 - Sauce Labs Onesie,'
+        ' 6 - Test.allTheThings() T-Shirt (Red)')
+    try:
+        product = int(input())
+        if product not in range(1,7):
+            continue
+        else:
+            break
+    except ValueError:
+        continue
+print(product)
