8aae1ba6ab3dbed27339d3d2c27952ef34b9f163
Muzyria <fila090580@gmail.com>
2022-12-11 19:56:15 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/4_25_1_test_12_DELL.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/4_25_1_test_12_DELL.py
@@ -13,14 +13,17 @@ base_url = 'https://demoqa.com/dynamic-properties'
 driver.get(base_url)
 driver.maximize_window()
 
+print('start test')
 button_enabled_after = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,
                                                                                    '//button[@id="enableAfter"]')))
 button_enabled_after.click()
+print('click button')
 
+print('start test 2')
 button_visible_after = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,
                                                                                    '//button[@id="visibleAfter"]')))
 button_visible_after.click()
-
+print('click button 2')
 
 time.sleep(2)
 driver.close()
