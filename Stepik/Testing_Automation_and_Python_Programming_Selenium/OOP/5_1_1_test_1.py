b911db4f8c8c844a06d2e484bcf579359e1794d9
Muzyria <fila090580@gmail.com>
2022-12-11 20:56:49 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -22,18 +24,20 @@ class NoTest1:
         login_standard_user = "standard_user"
         password_all = "secret_sauce"
 
-        user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')  # id XPATH
+        user_name = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="user-name"]')))
         user_name.send_keys(login_standard_user)
         print('Input Login')
 
-        password = driver.find_element(By.XPATH, '//input[@id="password"]')  # id XPATH
+        password = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="password"]')))
         password.send_keys(password_all)
         print('Input Password')
 
-        button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")  # id XPATH
+        button_login = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
         button_login.click()
         print('Click Login Button')
+        time.sleep(2)
 
+# WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="enableAfter"]')))
 
 test = NoTest1()
 test.select_product()
