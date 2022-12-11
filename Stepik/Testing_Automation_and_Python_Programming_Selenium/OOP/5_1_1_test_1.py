6a33e08e08123f59132b8104482dcaf387852720
Muzyria <fila090580@gmail.com>
2022-12-11 20:32:56 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class Test1:
     def test_select_product(self):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
