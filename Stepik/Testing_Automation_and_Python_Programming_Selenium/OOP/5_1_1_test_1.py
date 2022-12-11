1a112620b518e520f41b3d4de98d90ed3a1ccfd3
Muzyria <fila090580@gmail.com>
2022-12-11 20:41:54 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_1_1_test_1.py
@@ -1,20 +1,22 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class Test1:
-    def test_select_product(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+class NoTest1:
+
+    def select_product(self):
+        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        s = Service(r'C:\Git_Muzyria\Python\Stepik\Testing_Automation_and_Python_Programming_Selenium\OOP\resource'
+                    r'\chromedriver.exe')
+        driver = webdriver.Chrome(service=s)
         base_url = 'https://demoqa.com/dynamic-properties'
         driver.get(base_url)
         driver.maximize_window()
-        time.sleep(2)
-        driver.close()
+        time.sleep(5)
 
 
-test = Test1()
-test.test_select_product()
+test = NoTest1()
+test.select_product()
 
