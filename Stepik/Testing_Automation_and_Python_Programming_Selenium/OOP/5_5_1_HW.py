5097b0e5cdc1535f8e0dec4a5132a14046e43af3
“Muzyria” <fila090580@gmail.com>
2022-12-14 12:50:28 +0200

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_5_1_HW.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/OOP/5_5_1_HW.py
@@ -14,42 +14,47 @@ class NoTest1:
         base_url = 'https://www.saucedemo.com/'
         driver.get(base_url)
         driver.maximize_window()
-        time.sleep(2)
+        time.sleep(1)
 
         print('Start test')
 
-        login_standard_user = "standard_user"
+        lst_login_user = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
         password_all = "secret_sauce"
 
-        user_name = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="user-name"]')))
-        user_name.send_keys(login_standard_user)
-        print('Input Login')
+        for login in lst_login_user:
+            try:
+                user_name = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="user-name"]')))
+                user_name.send_keys(login)
+                print('Input Login')
 
-        password = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="password"]')))
-        password.send_keys(password_all)
-        print('Input Password')
+                password = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="password"]')))
+                password.send_keys(password_all)
+                print('Input Password')
 
-        button_login = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
-        button_login.click()
-        print('Click Login Button')
-        time.sleep(2)
+                button_login = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
+                button_login.click()
+                print('Click Login Button')
+                time.sleep(2)
 
-        success_test = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//span[@class="title"]')))
-        value_success_test = success_test.text
-        print(value_success_test)
-        assert value_success_test == 'PRODUCTS'
-        print('Test Succcess !!!')
 
-        button_menu = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="react-burger-menu-btn"]')))
-        button_menu.click()
-        print('Click Button Menu')
-        time.sleep(1)
 
-        items_logout = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//a[@id="logout_sidebar_link"]')))
-        items_logout.click()
-        print('Click Item Logout')
-        time.sleep(2)
+                success_test = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//span[@class="title"]')))
+                value_success_test = success_test.text
+                print(value_success_test)
+                assert value_success_test == 'PRODUCTS'
+                print('Test Succcess !!!')
 
+                button_menu = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="react-burger-menu-btn"]')))
+                button_menu.click()
+                print('Click Button Menu')
+                time.sleep(1)
+
+                items_logout = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//a[@id="logout_sidebar_link"]')))
+                items_logout.click()
+                print('Click Item Logout')
+                time.sleep(2)
+            except Exception:
+                print('Error ---------------------------------')
 
 test = NoTest1()
 test.select_product()
