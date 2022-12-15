c6e7fe3246157c2b52d25b2d9faca90363f7fd1e
Muzyria <fila090580@gmail.com>
2022-12-15 22:55:24 +0300

    new

---
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/pytestLessons/test/test_mail_1.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture()
+def set_up():
+    print('Вход в систему')
+
+
+def test_sending_mail_1(set_up):
+    print('Письмо отправлено')
+
+
+def test_sending_mail_2(set_up):
+    print('Письмо отправлено')
+
+
