589c6421393f82aa1449d87de22d256015108826
Muzyria <fila090580@gmail.com>
2022-12-15 23:27:28 +0300

    new

--- Stepik/Testing_Automation_and_Python_Programming_Selenium/pytestLessons/test/conftest.py
+++ Stepik/Testing_Automation_and_Python_Programming_Selenium/pytestLessons/test/conftest.py
@@ -6,3 +6,9 @@ def set_up():
     print('Вход в систему')
     yield
     print('Выход из системы')
+
+@pytest.fixture(scope='module')
+def some():
+    print('Начало')
+    yield
+    print('Конец')
