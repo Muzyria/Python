409d10e30054f013238e07be12c3e69e97813972
“Muzyria” <fila090580@gmail.com>
2023-01-05 14:11:06 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_3_10.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_3_10.py
@@ -25,3 +25,21 @@ print(*fig1.__dict__)
 # del fig1.color
 #
 # [print(i, end=' ') for i in fig1.__dict__]
+
+
+"""
+class Figure:
+    type_fig = 'ellipse'
+    color = 'red'
+    
+fig1 = Figure()
+
+fig1.start_pt = (10, 5)
+fig1.end_pt = (100, 20)
+fig1.color = 'blue'
+
+delattr(fig1, 'color')
+
+print(*[i for i in fig1.__dict__.keys()])
+"""
+
