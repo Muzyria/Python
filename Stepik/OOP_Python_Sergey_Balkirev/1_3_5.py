0ff1d1fce47465f65669f7909e311b68ce02aa69
“Muzyria” <fila090580@gmail.com>
2023-01-05 12:33:17 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_3_5.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_3_5.py
@@ -7,3 +7,14 @@ class Goods:
 
 setattr(Goods, 'inflation', 100)
 setattr(Goods, 'price', 2048)
+
+"""
+class Goods:
+    title = "Мороженое"
+    weight = 154
+    tp = "Еда"
+    price = 1024
+
+Goods.price = 2048
+Goods.inflation = 100
+"""
