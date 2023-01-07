653cceeea3f20e0b972ef2c24ac18506608e3d75
Muzyria <fila090580@gmail.com>
2023-01-07 23:27:42 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_5_5.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_5_5.py
@@ -22,4 +22,34 @@ for obj in elements:
         obj.ep = (0, 0)
         obj.sp = (0, 0)
 
+"""
+import random
 
+
+class Line:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+class Rect:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+class Ellipse:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+elements = [random.choice([Line, Rect, Ellipse])
+            (random.randint(1, 9), random.randint(1, 9), random.randint(1, 9), random.randint(1, 9))
+            for i in range(217)]
+
+# for obj in elements:
+#     if isinstance(obj, Line):
+#         obj.sp = (0, 0)
+#         obj.ep = (0, 0)
+elements = [Line(0, 0, 0, 0) if isinstance(i, Line) else i for i in elements]
+
+"""
--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,9 +1,28 @@
-class Point:
-    def __init__(self, x, y, color='black'):
-        self.x = x
-        self.y = y
-        self.color = color
+import random
 
 
-points = [ i for i in range(1, 2001, 2)]
-print(len(points))
+class Line:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+class Rect:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+class Ellipse:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+elements = [random.choice([Line, Rect, Ellipse])
+            (random.randint(1, 9), random.randint(1, 9), random.randint(1, 9), random.randint(1, 9))
+            for i in range(217)]
+
+# for obj in elements:
+#     if isinstance(obj, Line):
+#         obj.sp = (0, 0)
+#         obj.ep = (0, 0)
+elements = [Line(0, 0, 0, 0) if isinstance(i, Line) else i for i in elements]
