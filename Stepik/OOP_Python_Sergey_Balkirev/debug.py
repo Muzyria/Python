4470838ac44dc6e7396cc21b3ecf64e95d53cd77
Muzyria <fila090580@gmail.com>
2023-01-09 17:43:31 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,16 +1,3 @@
-class TriangleChecker:
-    def __init__(self, a, b, c):
-            self.a = a
-            self.b = b
-            self.c = c
-
-    def is_triangle(self):
-        if type(self.a) not in (int, float) or type(self.b) not in (int, float) or type(self.c) not in (int, float):
-            return 1
-        if self.a <= 0 or self.b <= 0 or self.c <= 0:
-            return 1
-        if self.a + self.b <= self.c or self.b + self.c <= self.a or self.a + self.c <= self.b:
-            return 2
-        return 3
-
-        # tr = TriangleChecker(a, b, c)
+class Graph:
+    def __init__(self, data, is_show=True):
+        self.data = data
