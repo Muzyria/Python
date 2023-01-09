b1d4630296874f5a1aa71a234884b864f361c79c
Muzyria <fila090580@gmail.com>
2023-01-09 20:36:29 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_5_7.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_5_7.py
@@ -1,6 +1,6 @@
 class Graph:
-    def __init__(self, data=[], is_show=True):
-        self.data = data 
+    def __init__(self, data, is_show=True):
+        self.data = data[:]
         self.is_show = is_show
 
     def set_data(self, data):
@@ -33,7 +33,50 @@ data_graph = list(map(int, input().split()))
 
 gr = Graph(data_graph)
 gr.show_bar()
-gr.set_show(fl_show = False)
+gr.set_show(fl_show=False)
+gr.show_table()
+
+"""
+class Graph:
+    def __init__(self, data, is_show=True):
+        self.data = data[:]
+        self.is_show = is_show
+
+    def set_data(self, data):
+        self.data.append(data)
+
+    def show_table(self):
+        if self.is_show:
+            print(" ".join(map(str, self.data)))
+        else:
+            print("Отображение данных закрыто")
+
+    def show_graph(self):
+        if self.is_show:
+            print(f'Графическое отображение данных: {" ".join(map(str, self.data))}')
+        else:
+            print("Отображение данных закрыто")
+
+    def show_bar(self):
+        if self.is_show:
+            print(f'Столбчатая диаграмма: {" ".join(map(str, self.data))}')
+        else:
+            print("Отображение данных закрыто")
+
+    def set_show(self, fl_show):
+        self.is_show = fl_show
+
+
+
+# считывание списка из входного потока (эту строку не менять)
+data_graph = list(map(int, input().split()))
+
+# здесь создаются объекты классов и вызываются нужные методы
+gr = Graph(data_graph)
+
+gr.show_bar()
+gr.set_show(fl_show=False)
 gr.show_table()
 
 
+"""
--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,3 +1,37 @@
 class Graph:
     def __init__(self, data, is_show=True):
         self.data = data
+        self.is_show = is_show
+
+    def set_data(self, data):
+        self.data.append(data)
+
+    def show_table(self):
+        if self.is_show:
+            print(" ".join(map(str, self.data)))
+        else:
+            print("Отображение данных закрыто")
+
+    def show_graph(self):
+        if self.is_show:
+            print(f'Графическое отображение данных: {" ".join(map(str, self.data))}')
+        else:
+            print("Отображение данных закрыто")
+
+    def show_bar(self):
+        if self.is_show:
+            print(f'Столбчатая диаграмма: {" ".join(map(str, self.data))}')
+        else:
+            print("Отображение данных закрыто")
+
+    def set_show(self, fl_show):
+        self.is_show = fl_show
+
+
+data_graph = list(map(int, input().split()))
+
+gp = Graph(data_graph)
+
+gp.show_bar()
+gp.set_show(fl_show=False)
+gp.show_table()
