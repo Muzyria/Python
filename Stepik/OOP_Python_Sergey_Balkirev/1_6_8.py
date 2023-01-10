6c62ce73ac2fa86bdb7eab260dd99da7aa326407
Muzyria <fila090580@gmail.com>
2023-01-10 23:44:10 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_6_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_6_8.py
@@ -11,6 +11,18 @@ class SingletonFive:
     def __init__(self, name):
         self.name = name
 
+"""
+class SingletonFive:
+    __instances = []
+    def __new__(cls, *args, **kwargs):
+        if len(cls.__instances) < 5:
+            cls.__instances.append(super().__new__(cls))
+        return cls.__instances[-1]
+    def __init__(self, name):
+        self.name = name
+objs = [SingletonFive(str(n)) for n in range(10)] # эту строчку не менять
+"""
+
             
 
 
