4d8f0b902bb4463482bd5f4bdbbf8bfe5f0e63c3
Muzyria <fila090580@gmail.com>
2023-01-16 22:32:25 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/1_7_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/1_7_8.py
@@ -61,3 +61,64 @@ class FormLogin:
 # эти строчки не менять
 login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
 html = login.render_template()
+
+"""
+from string import ascii_lowercase, digits
+
+
+# здесь объявляйте классы TextInput и PasswordInput
+class TextInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:    
+            raise ValueError("некорректное поле name")    
+
+    def get_html(self):
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and all(char in TextInput.CHARS_CORRECT for char in name):
+            return True
+        return False
+
+
+class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+    def __init__(self, name, size=10):
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:    
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and all(char in PasswordInput.CHARS_CORRECT for char in name):
+            return True
+        return False
+
+
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+    def render_template(self):
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+
+
+# эти строчки не менять
+login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
+html = login.render_template()
+"""
--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -3,16 +3,29 @@ from string import ascii_lowercase, digits
 
 # здесь объявляйте классы TextInput и PasswordInput
 class TextInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
 
     def __init__(self, name, size=10):
-        self.name = name
-        self.size = size
+        if self.check_name(name):
+            self.name = name
+            self.size = size
+        else:
+            raise ValueError("некорректное поле name")
 
     def get_html(self):
         return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
 
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and all(char in TextInput.CHARS_CORRECT for char in name):
+            return True
+        return False
+
 
 class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
 
     def __init__(self, name, size=10):
         self.name = name
@@ -20,6 +33,12 @@ class PasswordInput:
 
     def get_html(self):
         return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and all(char in PasswordInput.CHARS_CORRECT for char in name):
+            return True
+        return False
 
 
 class FormLogin:
