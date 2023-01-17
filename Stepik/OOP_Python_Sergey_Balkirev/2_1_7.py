af561703a4d7f6ae4b67804ec99324b882d13185
Muzyria <fila090580@gmail.com>
2023-01-17 21:48:05 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/2_1_7.py
+++ Stepik/OOP_Python_Sergey_Balkirev/2_1_7.py
@@ -33,3 +33,32 @@ print(
     book.get_author(),
     book.get_price()
     )
+
+"""
+class Book:
+    def __init__(self, author, title, price):
+        self.__title = None
+        self.__author = None
+        self.__price = None
+        self.set_author(author) 
+        self.set_title(title)
+        self.set_price(price) 
+        
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
+"""
--- Stepik/OOP_Python_Sergey_Balkirev/2_1_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/2_1_8.py
@@ -21,3 +21,25 @@ class Line:
 
 line = Line(1, 2, 3, 4)
 line.draw()
+
+"""
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+        self.set_coords(x1, y1, x2, y2)
+
+    def set_coords(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_coords(self):
+        return self.__x1, self.__y1, self.__x2, self.__y2
+        
+    def draw(self):
+        print(*self.get_coords())
+"""
