e5eda9f1a39d6d989b0b8520bfd41e55f9a2eb29
Muzyria <fila090580@gmail.com>
2023-01-18 23:46:06 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/2_2_6.py
+++ Stepik/OOP_Python_Sergey_Balkirev/2_2_6.py
@@ -41,3 +41,41 @@ w.height = 50
 q = WindowDlg("qwerty", -10, 20)
 q.show()
 
+"""
+class WindowDlg:
+    def __init__(self, title, width, height):
+        self.__title = title
+        self.__width = None
+        self.__height = None
+        self.width = width
+        self.height = height
+
+    def show(self):
+        print(f'{self.__title}: {self.__width}, {self.__height}')
+
+    @classmethod
+    def check_val(cls, value):
+        return type(value) == int and value in range(10001)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if self.check_val(value):
+            if self.__width:
+                self.show()
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if self.check_val(value):
+            if self.__height:
+                self.show()
+            self.__height = value
+"""
--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,23 +1,45 @@
-class Car:
-    def __init__(self):
-        self.__model = None
+class WindowDlg:
+    def __init__(self, title, width, height):
+        self.__title = title
+        self.__width = None
+        self.__height = None
+        self.width = width
+        self.height = height
 
-    @property
-    def model(self):
-        return self.__model
-
-    @model.setter
-    def model(self, x):
-        if self.check_model(x):
-            self.__model = x
+    def show(self):
+        print(f'{self.__title}: {self.__width}, {self.__height}')
 
     @classmethod
-    def check_model(cls, x):
-        if type(x) == str and len(x) in range(2, 101):
-            return True
-        return False
+    def check_val(cls, value):
+        return type(value) == int and value in range(10001)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if self.check_val(value):
+            if self.__width:
+                self.show()
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if self.check_val(value):
+            if self.__height:
+                self.show()
+            self.__height = value
 
 
-car = Car()
-car.model = "Toyota"
-print(car.model)
+w = WindowDlg("qwerty", -10, 20)
+w.show()
+w.width = 50
+w.height = 50
+
+q = WindowDlg("qwerty", -10, 20)
+q.show()
