fa24d05b8cd2fd7e876a4a78e65c02caeed2d9c7
“Muzyria” <fila090580@gmail.com>
2023-01-18 14:32:34 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,29 +1,36 @@
-class Point:
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
-
-    def get_coords(self):
-        return self.__x, self.__y
-
-class Rectangle:
-    def __init__(self, *args):
-        if len(args) == 4:
-            self.__sp = args[0], args[1]
-            self.__ep = args[2], args[3]
-        else:
-            self.__sp = args[0]
-            self.__ep = args[1]
-
-    def set_coords(self, sp, ep):
-        self.__sp = sp
-        self.__ep = ep
-
-    def get_coords(self):
-        return self.__sp, self.__ep
-
-    def draw(self):
-        print(f"Прямоугольник с координатами: {self.__sp} {self.__ep}")
+from random import randint, choice
+from string import ascii_lowercase, digits, ascii_uppercase
 
 
-rect = Rectangle((0, 0), (20, 34))
+class EmailValidator:
+    EMAIL_CHARS = ascii_lowercase + ascii_uppercase + digits + "_.@"
+    EMAIL_RANDOM_CHARS = ascii_lowercase + ascii_uppercase + digits + "_"
+
+    def __new__(cls, *args, **kwargs):
+        return None
+
+    @classmethod
+    def get_random_email(cls):
+        return "".join([choice(cls.EMAIL_RANDOM_CHARS) for _ in range(randint(4, 20))]) + "@gmail.com"
+
+    @classmethod
+    def check_email(cls, email):
+        if all(char in cls.EMAIL_CHARS for char in email):
+            if len(email) <= 100:
+                if ".." not in email:
+                    if len(email.split("@")[1]) <= 50:
+                        if "." in email.split("@")[1]:
+                            if email.split("@")[1].count(".") == 1:
+                                if cls.__is_email_str(email):
+                                    return True
+        return False
+
+    @staticmethod
+    def __is_email_str(email):
+        return type(email) == str
+
+
+res = EmailValidator.check_email("sc_lib@list.ru") # True
+print(res)
+res = EmailValidator.check_email("sc_lib@list_ru") # False
+print(res)
