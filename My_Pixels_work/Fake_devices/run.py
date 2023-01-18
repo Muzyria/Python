4c21263038656494dbd25d5149b4323f31e6dc2e
“Muzyria” <fila090580@gmail.com>
2023-01-18 12:29:32 +0200

    new

--- My_Pixels_work/Fake_devices/run.py
+++ My_Pixels_work/Fake_devices/run.py
@@ -6,7 +6,7 @@ import time
 # test_49 = 'S10150000211018049'
 time_value = 8  # Величина паузы между командами
 
-for i in ['S10150000211018' + str(i) for i in range(201, 209)]:
+for i in ['S10150000211018' + str(i) for i in range(201, 250)]:
 
     # Return area
     x = '32.67211882231258'
@@ -30,6 +30,7 @@ for i in ['S10150000211018' + str(i) for i in range(201, 209)]:
     time.sleep(time_value)
 
 
+
 # # Высадка дисанта
 # for i in ['S101500002110180' + str(i) for i in range(70, 91)]:
 #     x = '32.6' + str(randrange(6739266744535, 7320189906043))
@@ -38,11 +39,22 @@ for i in ['S10150000211018' + str(i) for i in range(201, 209)]:
 #     os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{i}" --es lat "{x}" --es lng "{y}"')
 #     time.sleep(1)
 
+# count_iter = 3600
+# start_time = datetime.now()  # включаем счетчик
+# for j in range(count_iter):
+#     count_time = str(datetime.now() - start_time).split('.')[0]
+#     for i in ['S10150000211018' + str(i) for i in range(201, 205)]:
+#         x = '32.67075509305431'
+#         y = '-116.95889191333002'
+#         print(i, 'На HOLE 1 +')
+#         os.system(f'adb shell am broadcast -a com.yama.fake.ADBCom --es id "{i}" --es lat "{x}" --es lng "{y}"')
+#         time.sleep(time_value)
+
 count_iter = 3600
 start_time = datetime.now()  # включаем счетчик
 for j in range(count_iter):
     count_time = str(datetime.now() - start_time).split('.')[0]
-    car_id = choice(['S10150000211018' + str(i) for i in range(201, 209)])
+    car_id = choice(['S10150000211018' + str(i) for i in range(201, 250)])
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
     print(car_id, x, y, ' -------------------------> ' + str(count_iter - j), ' прошло времени ' + count_time,
--- Stepik/OOP_Python_Sergey_Balkirev/2_1_4.py
+++ Stepik/OOP_Python_Sergey_Balkirev/2_1_4.py
@@ -17,7 +17,7 @@ class Clock:
         return type(tm) == int and 0 <= tm <= 100000
 
 
-clock = Clock(4530)
+clock = Clock(-4530)
 print(clock.get_time())  # 4530
 
 clock = Clock(4530)
@@ -28,3 +28,25 @@ clock.set_time(-1)
 clock.set_time('2')
 clock.set_time(0.1)
 print(clock.get_time())  # 7
+
+
+"""
+class Clock:
+    def __init__(self, tm):
+        self.__time = 0
+        self.set_time(tm)
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.__time
+
+    @classmethod
+    def __check_time(cls, tm):
+        if type(tm) == int and 0 <= tm <= 100000:
+            return True
+        return False
+
+"""
