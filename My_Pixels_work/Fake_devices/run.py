e4d8612e9b775221272ad8759e3767c23b59d2b6
“Muzyria” <fila090580@gmail.com>
2023-01-20 17:04:50 +0200

    new

--- My_Pixels_work/Fake_devices/run.py
+++ My_Pixels_work/Fake_devices/run.py
@@ -4,14 +4,14 @@ from random import choice, randrange
 import time
 
 # test_49 = 'S10150000211018049'
-time_value = 7  # Величина паузы между командами
+time_value = 6  # Величина паузы между командами
 
 geofens_for_4_car = {'S10150000211018201': ("32.67029213323701", "-116.95823751379653"),
                      'S10150000211018202': ("32.67030568038896", "-116.95804439474746"),
                      'S10150000211018203': ("32.66996700097419", "-116.95840917517349"),
                      'S10150000211018204': ("32.66999861110722", "-116.95814631869003")}
 
-for i in ['S10150000211018' + str(i) for i in range(201, 250)]:
+for i in ['S10150000211018' + str(i) for i in range(201, 296)]:
 
     # Return area
     x = '32.67211882231258'
@@ -49,7 +49,7 @@ count_iter = 3600
 start_time = datetime.now()  # включаем счетчик
 for j in range(count_iter):
     count_time = str(datetime.now() - start_time).split('.')[0]
-    car_id = choice(['S10150000211018' + str(i) for i in range(201, 250)])
+    car_id = choice(['S10150000211018' + str(i) for i in range(201, 296)])
     x = '32.6' + str(randrange(6739266744535, 7320189906043))
     y = '-116.9' + str(randrange(5185681343078, 6095486640930))
     print(car_id, x, y, ' -------------------------> ' + str(count_iter - j), ' прошло времени ' + count_time,
--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,66 +1,36 @@
-class StringValue:
+class Bag:
+    def __init__(self, max_weight: int):
+        self.max_weight = max_weight
+        self.__things = []
 
-    def __init__(self, min_length=2, max_length=50):
-        self.min_length = min_length
-        self.max_length = max_length
+    @property
+    def things(self):
+        return self.__things
 
-    def valid_string(self, string: str):
-        return isinstance(string, str) and self.min_length <= len(string) <= self.max_length
+    def add_thing(self, thing: object):
+        if self.get_total_weight() + thing.weight < self.max_weight:
+            self.things.append(thing)
 
-    def __set_name__(self, owner, name):
-        self.name = f'_{name}'
+    def remove_thing(self, indx: int):
+        self.things.pop(indx)
 
-    def __set__(self, instance, value):
-        if self.valid_string(value):
-            setattr(instance, self.name, value)
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
-
-
-class PriceValue:
-    def __init__(self, max_value=10000):
-        self.max_value = max_value
-
-    def valid_number(self, value: (int, float)):
-        return isinstance(value, (int, float)) and 0 <= value <= self.max_value
-
-    def __set_name__(self, owner, name):
-        self.name = f'_{name}'
-
-    def __set__(self, instance, value):
-        if self.valid_number(value):
-            setattr(instance, self.name, value)
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
+    def get_total_weight(self):
+        return sum([thing.weight for thing in self.things])
 
 
-class Product:
-    name = StringValue()
-    price = PriceValue()
-
-    def __init__(self, name, price):
+class Thing:
+    def __init__(self, name: str, weight: float):
         self.name = name
-        self.price = price
-
-
-class SuperShop:
-
-    def __init__(self, name):
-        self.name = name
-        self.goods = []
-
-    def add_product(self, product: object):
-        self.goods.append(product)
-
-    def remove_product(self, product: object):
-        self.goods.remove(product)
+        self.weight = weight
 
 
 if __name__ == '__main__':
-    shop = SuperShop("У Балакирева")
-    shop.add_product(Product("Курс по Python", 0))
-    shop.add_product(Product("Курс по Python ООП", 2000))
-    for p in shop.goods:
-        print(f"{p.name}: {p.price}")
+    bag = Bag(1000)
+    bag.add_thing(Thing("Книга по Python", 100))
+    bag.add_thing(Thing("Котелок", 500))
+    bag.add_thing(Thing("Спички", 20))
+    bag.add_thing(Thing("Бумага", 100))
+    w = bag.get_total_weight()
+    assert bag.get_total_weight() == 720
+    for t in bag.things:
+        print(f"{t.name}: {t.weight}")
