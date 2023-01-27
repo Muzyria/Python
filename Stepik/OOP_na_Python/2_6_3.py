f260cad570af8f215e6c5dcab64c39fa75620564
“Muzyria” <fila090580@gmail.com>
2023-01-27 15:09:59 +0200

    new

--- Stepik/OOP_na_Python/2_6_3.py
+++ Stepik/OOP_na_Python/2_6_3.py
@@ -21,5 +21,33 @@ print(k.email)  # prince@wait.you
 k.email = [1, 2, 3] # ErrorMail:[1, 2, 3]
 k.email = 'prince@still@.wait'  # ErrorMail:prince@still@.wait
 k.email = 'prince@still.wait'
-print(k.email)  # prince@still.wait   
+print(k.email)  # prince@still.wait
+
+'''
+import re
+class UserMail:
+    def __init__(self, login, email):
+        self.__email = None
+        self.login = login
+        self.set_email(email)
+
+    def get_email(self):
+        return self.__email
+
+    def set_email(self, email):
+        if isinstance(email, str) and re.findall(r'^\w+@\w+\.\w+$', email):
+            self.__email = email
+        else:
+            print(f"ErrorMail:{email}")
+
+    email = property(fget=get_email, fset=set_email)
+
+
+k = UserMail('belosnezhka', 'prince@wait.you')
+print(k.email)  # prince@wait.you
+k.email = [1, 2, 3] # ErrorMail:[1, 2, 3]
+k.email = 'prince@still@.wait'  # ErrorMail:prince@still@.wait
+k.email = 'prince@still.wait'
+print(k.email)  # prince@still.wait
+'''
         
--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -15,11 +15,3 @@ class UserMail:
             print(f"ErrorMail:{email}")
 
     email = property(fget=get_email, fset=set_email)
-
-
-k = UserMail('belosnezhka', 'prince@wait.you')
-print(k.email)  # prince@wait.you
-k.email = [1, 2, 3] # ErrorMail:[1, 2, 3]
-k.email = 'prince@still@.wait'  # ErrorMail:prince@still@.wait
-k.email = 'prince@still.wait'
-print(k.email)  # prince@still.wait
