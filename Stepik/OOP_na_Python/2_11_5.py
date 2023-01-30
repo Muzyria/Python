3f81cc6a7a122ef2d8002488570606f1cfa32a8d
“Muzyria” <fila090580@gmail.com>
2023-01-30 12:23:37 +0200

    new

--- Stepik/OOP_na_Python/2_11_5.py
+++ Stepik/OOP_na_Python/2_11_5.py
@@ -67,3 +67,61 @@ Trash.add(f2)
 Trash.add(passwords)
 Trash.clear()
 f1.read() # ErrorReadFileTrashed(puppies.jpg)
+
+"""
+class File:
+    def __init__(self, name):
+        self.name = name
+        self.in_trash = False
+        self.is_deleted = False
+
+    def restore_from_trash(self):
+        print(f"Файл {self.name} восстановлен из корзины")
+        self.in_trash = False
+
+    def remove(self):
+        print(f"Файл {self.name} был удален")
+        self.is_deleted = True
+
+    def read(self):
+        if self.is_deleted:
+            print(f"ErrorReadFileDeleted({self.name})")
+        elif self.in_trash:
+            print(f"ErrorReadFileTrashed({self.name})")
+        else:
+            print(f"Прочитали все содержимое файла {self.name}")
+
+    def write(self, content):
+        if self.is_deleted:
+            print(f"ErrorWriteFileDeleted({self.name})")
+        elif self.in_trash:
+            print(f"ErrorWriteFileTrashed({self.name})")
+        else:
+            print(f"Записали значение {content} в файл {self.name}")
+
+
+class Trash:
+    content = []
+
+    @staticmethod
+    def add(file):
+        if not isinstance(file, File):
+            print('В корзину добавлять можно только файл')
+        else:
+            Trash.content.append(file)
+            file.in_trash = True
+
+    @staticmethod
+    def clear():
+        print('Очищаем корзину')
+        [File.remove(i) for i in Trash.content]
+        Trash.content = []
+        print('Корзина пуста')
+
+    @staticmethod
+    def restore():
+        print('Восстанавливаем файлы из корзины')
+        [File.restore_from_trash(i) for i in Trash.content]
+        Trash.content = []
+        print('Корзина пуста')
+"""
--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,65 +1,76 @@
-from string import ascii_letters
+class File:
+    def __init__(self, name):
+        self.name = name
+        self.in_trash = False
+        self.is_deleted = False
 
-class Registration:
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
+    def restore_from_trash(self):
+        print(f"Файл {self.name} восстановлен из корзины")
+        self.in_trash = False
 
-    @property
-    def login(self):
-        return self.__login
+    def remove(self):
+        print(f"Файл {self.name} был удален")
+        self.is_deleted = True
 
-    @login.setter
-    def login(self, value):
-        if value.count("@") != 1:
-            raise ValueError("Логин должен содержать один символ '@'")
-        elif "." not in value[value.index("@"):]:
-            raise ValueError("Логин должен содержать символ '.'")
+    def read(self):
+        if self.is_deleted:
+            print(f"ErrorReadFileDeleted({self.name})")
+        elif self.in_trash:
+            print(f"ErrorReadFileTrashed({self.name})")
         else:
-            self.__login = value
+            print(f"Прочитали все содержимое файла {self.name}")
 
-    @property
-    def password(self):
-        return self.__password
-
-    @password.setter
-    def password(self, value):
-        if not type(value) == str:
-            raise TypeError("Пароль должен быть строкой")
-        elif not 4 < len(value) < 12:
-            raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
-        elif not self.is_include_digit(value):
-            raise ValueError('Пароль должен содержать хотя бы одну цифру')
-        elif not self.is_include_all_register(value):
-            raise ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
-        elif not self.is_include_only_latin(value):
-            raise ValueError('Пароль должен содержать только латинский алфавит')
-        elif self.check_password_dictionary(value):
-            raise ValueError('Ваш пароль содержится в списке самых легких')
+    def write(self, content):
+        if self.is_deleted:
+            print(f"ErrorWriteFileDeleted({self.name})")
+        elif self.in_trash:
+            print(f"ErrorWriteFileTrashed({self.name})")
         else:
-            self.__password = value
+            print(f"Записали значение {content} в файл {self.name}")
+
+
+class Trash:
+    content = []
 
     @staticmethod
-    def is_include_digit(value):
-        return any(i.isdigit() for i in value)
+    def add(file):
+        if not isinstance(file, File):
+            print('В корзину добавлять можно только файл')
+        else:
+            Trash.content.append(file)
+            file.in_trash = True
 
     @staticmethod
-    def is_include_all_register(value):
-        return any(i.islower() for i in value) and any(i.isupper() for i in value)
+    def clear():
+        print('Очищаем корзину')
+        [File.remove(i) for i in Trash.content]
+        Trash.content = []
+        print('Корзина пуста')
 
     @staticmethod
-    def is_include_only_latin(value):
-        return all(i in ascii_letters for i in value if i.isalpha())
+    def restore():
+        print('Восстанавливаем файлы из корзины')
+        [File.restore_from_trash(i) for i in Trash.content]
+        Trash.content = []
+        print('Корзина пуста')
 
-    @staticmethod
-    def check_password_dictionary(value):
-        with open('easy_passwords.txt', 'r', encoding='utf-8') as f:
-            return value in f.read().split()
+f1 = File('puppies.jpg')
+f2 = File('cat.jpg')
+passwords = File('pass.txt')
 
+f1.read() # Прочитали все содержимое файла puppies.jpg
+Trash.add(f1)
+f1.read() # ErrorReadFileTrashed(puppies.jpg)
 
-r1 = Registration('qwerty@rambler.ru', 'QwrRt124') # здесь хороший логин
-print(r1.login, r1.password)  # qwerty@rambler.ru QwrRt124
+Trash.add(f2)
+Trash.add(passwords)
+Trash.clear() # после этой команды вывод должен быть таким
+'''
+Очищаем корзину
+Файл puppies.jpg был удален
+Файл cat.jpg был удален
+Файл pass.txt был удален
+Корзина пуста
+'''
 
-r1.password = '123456'  # ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
-# r1.password = 'LoW'  # raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
-# r1.password = 43  # raise TypeError("Пароль должен быть строкой")
+f1.read() # ErrorReadFileTrashed(puppies.jpg)
