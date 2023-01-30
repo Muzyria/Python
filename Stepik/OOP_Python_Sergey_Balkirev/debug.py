b41d1d35fc1561b2dc8a9d9f37be0a2f51f60f38
“Muzyria” <fila090580@gmail.com>
2023-01-30 14:27:09 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,45 +1,7 @@
-class WindowDlg:
-    def __init__(self, title, width, height):
-        self.__title = title
-        self.__width = None
-        self.__height = None
-        self.width = width
-        self.height = height
-
-    def show(self):
-        print(f'{self.__title}: {self.__width}, {self.__height}')
-
-    @classmethod
-    def check_val(cls, value):
-        return type(value) == int and value in range(10001)
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if self.check_val(value):
-            if self.__width:
-                self.show()
-            self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if self.check_val(value):
-            if self.__height:
-                self.show()
-            self.__height = value
 
 
-w = WindowDlg("qwerty", -10, 20)
-w.show()
-w.width = 50
-w.height = 50
+d = {}
 
-q = WindowDlg("qwerty", -10, 20)
-q.show()
+d["qq"] = 56
+d["qq"] -= 6
+print(d["qq"])
--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Product:
     def __init__(self, name, price):
         self.name = name
@@ -30,12 +33,83 @@ class User:
         self.__balance -= value
         return True
 
+class Cart:
+    def __init__(self, user: User):
+        self.user = user
+        self.goods = {}
+        self.__total = 0
+
+    def add(self, product: Product, count=1):
+        self.goods[product.name] = [product.price, count]
+        self.__total += product.price * count
+
+    def remove(self, product: Product, count=1):
+        if self.goods[product.name][1] < count:
+            count = self.goods[product.name][1]
+        self.__total -= self.goods[product.name][0] * count
+        self.goods[product.name][1] -= count
+
+    @property
+    def total(self):
+        return self.__total
+
+    def order(self):
+        if User.payment(self.user, self.__total):
+            print('Заказ оплачен')
+        else:
+            print('Проблема с оплатой')
+
+
+    def print_check(self):
+        print('---Your check---')
+        # for k, v in self.goods.items():
+        #     print(f"{k} {v[0]} {v[1]}")
+        [print(f"{k} {v[0]} {v[1]} {v[0] * v[1]}") for k, v in sorted(self.goods.items())]
+        print(f'---Total: {self.total}---')
+
+
+
 
 billy = User('billy@rambler.ru')
-print(billy) # Пользователь billy@rambler.ru, баланс - 0
-billy.deposit(100)
-billy.deposit(300)
-print(billy) # Пользователь billy@rambler.ru, баланс - 400
-billy.payment(500) # Не хватает средств на балансе. Пополните счет
-billy.payment(150)
-print(billy) # Пользователь billy@rambler.ru, баланс - 250
+
+lemon = Product('lemon', 20)
+carrot = Product('carrot', 30)
+
+cart_billy = Cart(billy)
+print(cart_billy.user) # Пользователь billy@rambler.ru, баланс - 0
+cart_billy.add(lemon, 2)
+cart_billy.add(carrot)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 2 40
+---Total: 70---'''
+cart_billy.add(lemon, 3)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.remove(lemon, 6)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+---Total: 30---'''
+print(cart_billy.total) # 30
+cart_billy.add(lemon, 5)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.order()
+''' Печатает текст ниже
+Не хватает средств на балансе. Пополните счет
+Проблема с оплатой'''
+cart_billy.user.deposit(150)
+cart_billy.order() # Заказ оплачен
+print(cart_billy.user.balance) # 20
