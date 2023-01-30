e318a437e35137474c16ffbfe6014b34e2940e1c
“Muzyria” <fila090580@gmail.com>
2023-01-30 17:18:56 +0200

    new

--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,23 +1,11 @@
-class Person:
-    def __init__(self, name, surname, gender="male"):
-        self.name = name
-        self.surname = surname
-        if gender in ("male", "female"):
-            self.gender = gender
-        else:
-            print('Не знаю, что вы имели ввиду? Пусть это будет мальчик!')
-            self.gender = "male"
+class Vector:
+    def __init__(self, *args):
+        self.values = [i for i in args if type(i) == int]
 
     def __str__(self):
-        if self.gender == 'male':
-            return f"Гражданин {self.surname} {self.name}"
-        else:
-            return f"Гражданка {self.surname} {self.name}"
+        return f"Вектор{tuple(sorted(self.values))}" if self.values else "Пустой вектор"
 
-
-p1 = Person('Chuck', 'Norris')
-print(p1) # печатает "Гражданин Norris Chuck"
-p2 = Person('Mila', 'Kunis', 'female')
-print(p2) # печатает "Гражданка Kunis Mila"
-p3 = Person('Оби-Ван', 'Кеноби', True)# печатает "Не знаю, что вы имели ввиду? Пусть это будет мальчик!"
-print(p3) # печатает "Гражданин Кеноби Оби-Ван"
+v1 = Vector(1,2,3)
+print(v1) # печатает "Вектор(1, 2, 3)"
+v2 = Vector()
+print(v2) # печатает "Пустой вектор"
