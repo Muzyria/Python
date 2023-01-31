8348a44dc3f81ed26dc132cb9b1f8432030c552f
“Muzyria” <fila090580@gmail.com>
2023-01-31 16:21:29 +0200

    new

--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,20 +1,9 @@
-class Quadrilateral:
-    def __init__(self, *args):
-        self.width = args[0]
-        self.height = args[1] if len(args) > 1 else args[0]
+class Addition:
+    def __call__(self, *args, **kwargs):
+        print(f'Сумма переданных значений = {sum(i for i in args if type(i) in (int, float))}')
 
-    def __str__(self):
-        if self.__bool__():
-            return f'Куб размером {self.width}х{self.height}'
-        return f'Прямоугольник размером {self.width}х{self.height}'
+add = Addition()
 
-    def __bool__(self):
-        return self.width == self.height
-
-
-q1 = Quadrilateral(10)
-print(q1)  # печатает "Куб размером 10х10"
-print(bool(q1))  # печатает "True"
-q2 = Quadrilateral(3, 5)
-print(q2)  # печатает "Прямоугольник размером 3х5"
-print(q2 == True)  # печатает "False"
+add(10, 20) # печатает "Сумма переданных значений = 30"
+add(1, 2, 3.4) # печатает "Сумма переданных значений = 6.4"
+add(1, 2, 'hello', [1, 2], 3) # печатает "Сумма переданных значений = 6"
