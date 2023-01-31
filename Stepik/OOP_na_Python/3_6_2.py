8065608c6bba8965cf80c9efd8f9b4a1ebea1beb
“Muzyria” <fila090580@gmail.com>
2023-01-31 14:24:51 +0200

    new

--- Stepik/OOP_na_Python/3_6_2.py
+++ Stepik/OOP_na_Python/3_6_2.py
@@ -17,3 +17,15 @@ print(bool(p1))  # печатает "True"
 p2 = City('SaN frANCISco')
 print(p2)  # печатает "San Francisco"
 print(p2 == True)  # печатает "False"
+
+"""
+class City:
+    def __init__(self, name):
+        self.name = name.title()
+
+    def __str__(self):
+        return self.name
+
+    def __bool__(self):
+        return not self.name.endswith(('a', 'e', 'i', 'o', 'u'))
+"""
