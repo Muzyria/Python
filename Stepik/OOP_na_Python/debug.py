320fbf4976bc3d8a149dcb7280f84fd33cf46cb8
“Muzyria” <fila090580@gmail.com>
2023-02-01 17:50:18 +0200

    new

--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,5 +1,19 @@
-s = ''.join([bin(int(i))[2:].zfill(8) for i in input().split('.')])
+n1, n2, n3, n4 = [int(i) for i in input().split('.')]
+m1, m2, m3, m4 = [int(i) for i in input().split('.')]
 
-print(all(i in range(256) for i in (n1, n2, n3, n4)) and f'{n1}.{n2}.{n3}.{n4}' not in ('0.0.0.0', '255.255.255.255'))
 
-print("1" not in s[s.index("0"):])
+def check_ip(*args):
+    # print('.'.join(list(map(str, args))))
+    return all(i in range(256) for i in args) and '.'.join(list(map(str, args))) not in ('0.0.0.0', '255.255.255.255')
+
+def check_mask(*args):
+    if all(i in range(256) for i in args):
+        s = ''.join([bin(int(i))[2:].zfill(8) for i in args])
+
+        return "1" not in s[s.index("0"):] if "0" in s else True
+    else:
+        return False
+
+
+print(check_ip(n1, n2, n3, n4))
+print(check_ip(m1, m2, m3, m4))
