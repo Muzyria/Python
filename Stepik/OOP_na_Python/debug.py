60a62a9f36b7af3fd27c6560ca8578c4da5784b1
“Muzyria” <fila090580@gmail.com>
2023-02-02 17:03:14 +0200

    new

--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,35 +1,50 @@
-class Vehicle:
-    def __init__(self, name, mileage, capacity):
-        self.name = name
+class Transport:
+    def __init__(self, brand, max_speed, kind=None):
+        self.brand = brand
+        self.max_speed = max_speed
+        self.kind = kind
+
+    def __str__(self):
+        return f"Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч"
+
+
+class Car(Transport):
+    def __init__(self, brand, max_speed, mileage, gasoline_residue):
+        super().__init__(brand, max_speed, kind="Car")
         self.mileage = mileage
-        self.capacity = capacity
+        self.gasoline = gasoline_residue
 
-    def fare(self):
-        return self.capacity * 100
+    @property
+    def gasoline(self):
+        return f"Осталось бензина на {self.__gasoline_residue} км"
 
-    def display(self):
-        print(f'Total {self.name} fare is: {self.fare()}')
-
-class Bus(Vehicle):
-    def __init__(self, name, mileage):
-        super().__init__(name, mileage, capacity=50)
-
-    def fare(self):
-        return super().fare() * 1.1
-
-class Taxi(Vehicle):
-    def __init__(self, name, mileage):
-        super().__init__(name, mileage, capacity=4)
-
-    def fare(self):
-        return super().fare() * 1.35
+    @gasoline.setter
+    def gasoline(self, value):
+        self.__gasoline_residue = 5
 
 
-sc = Vehicle('Scooter', 100, 2)
-sc.display() #печатает: Total Scooter fare is: 200
 
-merc = Bus("Mercedes", 120000)
-merc.display() #печатает: Total Mercedes fare is: 5500.0
+class Boat(Transport):
+    pass
 
-polo = Taxi("Volkswagen Polo", 15000)
-polo.display() #печатает: Total Volkswagen Polo fare is: 540.0
+
+class Plane(Transport):
+    pass
+
+
+transport = Transport('Telega', 10)
+print(transport)  # Тип транспорта None марки Telega может развить скорость 10 км/ч
+bike = Transport('shkolnik', 20, 'bike')
+print(bike)  # Тип транспорта bike марки shkolnik может развить скорость 20 км/ч
+
+first_plane = Plane('Virgin Atlantic', 700, 450)
+print(first_plane)  # Самолет марки Virgin Atlantic вмещает в себя 450 людей
+first_car = Car('BMW', 230, 75000, 300)
+print(first_car)  # Тип транспорта Car марки BMW может развить скорость 230 км/ч
+print(first_car.gasoline)  # Осталось бензина на 300 км
+first_car.gasoline = 20  # Печатает 'Объем топлива увеличен на 20 л и составляет 320 л'
+print(first_car.gasoline)  # Осталось бензина на 320 км
+second_car = Car('Audi', 230, 70000, 130)
+second_car.gasoline = [None]  # Печатает 'Ошибка заправки автомобиля'
+first_boat = Boat('Yamaha', 40, 'Petr')
+print(first_boat)  # Этой лодкой марки Yamaha владеет Petr
---
+++ Stepik/OOP_na_Python/debug_2.py
@@ -0,0 +1,28 @@
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents
+
+    @property
+    def dollars(self):
+        return self.total_cents // 100
+
+    @dollars.setter
+    def dollars(self, val):
+        if type(val) == int and val >= 0:
+            self.total_cents = self.cents + val * 100
+        else:
+            print("Error dollars")
+
+    @property
+    def cents(self):
+        return self.total_cents % 100
+
+    @cents.setter
+    def cents(self, val):
+        if type(val) == int and 0 <= val < 100:
+            self.total_cents = self.total_cents - self.cents + val
+        else:
+            print("Error cents")
+
+    def __str__(self):
+        return f"Ваше состояние составляет {self.dollars} долларов {self.cents} центов"
