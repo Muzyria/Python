79b3e416076df2b155ee540da2e727f962d74ca3
Muzyria <fila090580@gmail.com>
2023-02-02 21:45:27 +0200

    new

--- Stepik/OOP_na_Python/debug.py
+++ Stepik/OOP_na_Python/debug.py
@@ -1,63 +1,34 @@
-class Transport:
-    def __init__(self, brand, max_speed, kind=None):
-        self.brand = brand
-        self.max_speed = max_speed
-        self.kind = kind
-
-    def __str__(self):
-        return f"Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч"
-
-
-class Car(Transport):
-    def __init__(self, brand, max_speed, mileage, gasoline_residue):
-        super().__init__(brand, max_speed, kind="Car")
-        self.mileage = mileage
-        self.__gasoline_residue = gasoline_residue
-
-    @property
-    def gasoline(self):
-        return f"Осталось бензина на {self.__gasoline_residue} км"
-
-    @gasoline.setter
-    def gasoline(self, value):
-        if isinstance(value, int):
-            self.__gasoline_residue += value
-            print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
+class Initialization:
+    def __init__(self, capacity: int, food: list):
+        if isinstance(capacity, int):
+            self.capacity = capacity
+            self.food = food
         else:
-            print(f'Ошибка заправки автомобиля')
+            print("Количество людей должно быть целым числом")
 
 
-class Boat(Transport):
-    def __init__(self, brand, max_speed, owners_name):
-        super().__init__(brand, max_speed, kind="Boat")
-        self.owners_name = owners_name
-
-    def __str__(self):
-        return f"Этой лодкой марки {self.brand} владеет {self.owners_name}"
-
-
-class Plane(Transport):
-    def __init__(self, brand, max_speed, capacity):
-        super().__init__(brand, max_speed, kind="Plane")
-        self.capacity = capacity
+class Vegetarian(Initialization):
+    def __init__(self, capacity, food):
+        super().__init__(capacity, food)
 
     def __str__(self):
-        return f"Самолет марки {self.brand} вмещает в себя {self.capacity} людей"
+        return f"{self.capacity} людей предпочитают не есть мясо! Они предпочитают {self.food}"
 
 
-transport = Transport('Telega', 10)
-print(transport)  # Тип транспорта None марки Telega может развить скорость 10 км/ч
-bike = Transport('shkolnik', 20, 'bike')
-print(bike)  # Тип транспорта bike марки shkolnik может развить скорость 20 км/ч
+class MeatEater(Initialization):
+    def __init__(self, capacity, food):
+        super().__init__(capacity, food)
 
-first_plane = Plane('Virgin Atlantic', 700, 450)
-print(first_plane)  # Самолет марки Virgin Atlantic вмещает в себя 450 людей
-first_car = Car('BMW', 230, 75000, 300)
-print(first_car)  # Тип транспорта Car марки BMW может развить скорость 230 км/ч
-print(first_car.gasoline)  # Осталось бензина на 300 км
-first_car.gasoline = 20  # Печатает 'Объем топлива увеличен на 20 л и составляет 320 л'
-print(first_car.gasoline)  # Осталось бензина на 320 км
-second_car = Car('Audi', 230, 70000, 130)
-second_car.gasoline = [None]  # Печатает 'Ошибка заправки автомобиля'
-first_boat = Boat('Yamaha', 40, 'Petr')
-print(first_boat)  # Этой лодкой марки Yamaha владеет Petr
+    def __str__(self):
+        return f"{self.capacity} мясоедов в Москве! Помимо мяса они едят еще и {self.food}"
+
+
+class SweetTooth(Initialization):
+    def __init__(self, capacity, food):
+        super().__init__(capacity, food)
+
+    def __str__(self):
+        return f"Сладкоежек в Москве {self.capacity}. Их самая любимая еда: {self.food}"
+
+    def __eq__(self, other):
+        pass
