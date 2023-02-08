23a4d62f6a7ad5c5b17cf8f0ac56c011c5f4b915
“Muzyria” <fila090580@gmail.com>
2023-02-08 16:15:00 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/debug.py
+++ Stepik/OOP_Python_Sergey_Balkirev/debug.py
@@ -1,3 +1,33 @@
-attrs = {'id': int, 'name': str, 'weight': (int, float), 'price': (int, float)}
+class Circle:
+    def __init__(self, x, y, radius):
+        self.__x = self.__y = self.__radius = None
+        self.x = x
+        self.y = y
+        self.radius = radius
 
-print(set(str(type(5))).issubset(set(str(attrs['id']))))
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = 1000
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        self.__radius = value
+
+c = Circle(1,2, 5)
+print(c.__dict__)
