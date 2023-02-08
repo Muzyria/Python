68f0c9950beb3862d42c2ad1da60670d52fd80b5
“Muzyria” <fila090580@gmail.com>
2023-02-08 17:17:01 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/3_1_11.py
+++ Stepik/OOP_Python_Sergey_Balkirev/3_1_11.py
@@ -1,4 +1,3 @@
-
 import time
 
 
@@ -10,3 +9,50 @@ class GeyserClassic:
         self.slot_2 = None
         self.slot_3 = None
 
+    def add_filter(self, slot_num, filter):
+        if slot_num == 1 and self.slot_1 is None:
+            self.slot_1 = filter
+        if slot_num == 2 and self.slot_2 is None:
+            self.slot_2 = filter
+        if slot_num == 3 and self.slot_3 is None:
+            self.slot_3 = filter
+
+
+
+class Mechanical:
+    def __init__(self, date: float):
+        self.__date = date
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        print("__setter")
+        pass
+
+class Aragon:
+    def __init__(self, date: float):
+        self.__date = date
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        pass
+
+
+class Calcium:
+    def __init__(self, date: float):
+        self.__date = date
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        pass
