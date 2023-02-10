d63ebbde6ed5ea532bc954db79657da48f2ee70a
“Muzyria” <fila090580@gmail.com>
2023-02-10 11:22:10 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/3_2_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/3_2_8.py
@@ -1,2 +1,12 @@
 class HandlerGET:
-    pass
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, request: dict, *args, **kwargs):
+        m = request.get('method', 'GET')
+        if m == "GET":
+            return self.get(self.func, request)
+        return None
+
+    def get(self, func, request, *args, **kwargs):
+        return f'GET: {func(request)}'
