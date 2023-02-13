7e3882e9d889ed6724fe20c3c7e1c66e72c21cab
“Muzyria” <fila090580@gmail.com>
2023-02-13 13:48:28 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/3_6_7.py
+++ Stepik/OOP_Python_Sergey_Balkirev/3_6_7.py
@@ -1,36 +1,29 @@
-import sys
-import re
-
 
 class ShopItem:
-    def __init__(self, name: str, weight: (int, float), price: (int, float)):
+    def __init__(self, name, weight, price):
         self.name = name
         self.weight = weight
         self.price = price
 
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
     def __hash__(self):
         return hash((self.name.lower(), self.weight, self.price))
 
-    def __eq__(self, other: object):
-        return hash(self) == hash(other)
 
+lst_in = ['Системный блок: 1500 75890.56',
+          'Монитор Samsung: 2000 34000',
+          'Клавиатура: 200.44 545',
+          'Монитор Samsung: 2000 34000']
 
-def get_int_or_float_number(string: str) -> (int, float):
-    return int(string) if string.isdigit() else float(string)
+shop_items = {}
+for i in lst_in:
+    temp_list = i.split()
+    temp_item = ShopItem(temp_list[0], temp_list[1], temp_list[2])
+    if temp_item in shop_items:
+        shop_items[temp_item][1] += 1
+    else:
+        shop_items[temp_item] = [temp_item, 1]
 
-
-if __name__ == '__main__':
-    # lst_in = list(map(str.strip, sys.stdin.readlines()))
-    lst_in = ['Системный блок: 1500 75890.56', 'Монитор Samsung rerr: 2000 34000', 'Клавиатура: 200.44 545',
-              'Монитор Samsung: 2000 34000']
-    reg = r'[A-Za-zА-Яа-я\s]+:|\s*[\d\.]*\s*'
-    gen_lst = ([elm.strip(': ') for elm in re.findall(reg, s) if elm not in ('', ' ')] for s in lst_in)
-    lst_obj = [ShopItem(elm[0], get_int_or_float_number(elm[1]), get_int_or_float_number(elm[2])) for elm in gen_lst]
-    shop_items = {}
-    for obj in lst_obj:
-        if obj not in shop_items:
-            shop_items[obj] = [obj, 1]
-        else:
-            shop_items[obj][1] += 1
-    print(shop_items)
     
