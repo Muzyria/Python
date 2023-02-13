b6c02ec4e5bc4f1c3360bea29e78f420c67b1090
Muzyria <fila090580@gmail.com>
2023-02-13 20:57:37 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/4_2_6.py
+++ Stepik/OOP_Python_Sergey_Balkirev/4_2_6.py
@@ -1,27 +1,71 @@
 class Protists:
+    def __init__(self, name: str, weight: float, old: int):
+        self.name = name
+        self.weight = weight
+        self.old = old
+
+
+class Plants(Protists):
     pass
 
-class Plants:
+
+class Animals(Protists):
     pass
 
-class Animals:
+
+class Mosses(Plants):
     pass
 
-class Mosses:
+
+class Flowering(Plants):
     pass
 
-class Flowering:
+
+class Worms(Animals):
     pass
 
-class Worms:
+
+class Mammals(Animals):
     pass
 
-class Mammals:
+
+class Human(Mammals):
     pass
 
-class Human:
+
+class Monkeys(Mammals):
     pass
 
-class Monkeys:
-    pass
 
+class Monkey(Monkeys):
+    def __init__(self, name: str, weight: float, old: int):
+        super().__init__(name, weight, old)
+
+
+class Person(Human):
+    def __init__(self, name: str, weight: float, old: int):
+        super().__init__(name, weight, old)
+
+
+class Flower(Flowering):
+    def __init__(self, name: str, weight: float, old: int):
+        super().__init__(name, weight, old)
+
+
+class Worm(Worms):
+    def __init__(self, name: str, weight: float, old: int):
+        super().__init__(name, weight, old)
+
+
+lst_objs = [Monkey("мартышка", 30.4, 7), Monkey("шимпанзе", 24.6, 8),
+            Person("Балакирев", 88, 34), Person("Верховный жрец", 67.5, 45),
+            Flower("Тюльпан", 0.2, 1), Flower("Роза", 0.1, 2),
+            Worm("червь", 0.01, 1), Worm("червь 2", 0.02, 1)]
+
+lst_animals = [i for i in lst_objs if isinstance(i, Animals)]
+lst_plants = [i for i in lst_objs if isinstance(i, Plants)]
+lst_mammals = [i for i in lst_objs if isinstance(i, Mammals)]
+
+print(lst_animals)
+print(lst_plants)
+print(lst_mammals)
