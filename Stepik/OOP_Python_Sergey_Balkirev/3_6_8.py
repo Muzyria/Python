264dd7fac2a4b0dc92d310420b66f0bd5147a5cb
“Muzyria” <fila090580@gmail.com>
2023-02-13 13:43:36 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/3_6_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/3_6_8.py
@@ -1,46 +1,54 @@
+from typing import NoReturn
 import sys
 
 
 class DataBase:
-    def __init__(self, path: str):
+
+
+    def __init__(self, path: str) -> NoReturn:
         self.path = path
         self.dict_db = {}
 
-    def write(self, record: object):
-        self.dict_db[record] = self.dict_db.get(record, []) + [record]
+    def write(self, record: 'Record') -> NoReturn:
+        self.dict_db[record] = []
+        self.dict_db[record].append(record)
 
-    def read(self, pk: int):
-        for value in self.dict_db.values():
-            for obj in value:
-                if pk == obj.pk:
-                    return obj
+    def read(self, pk: int) -> 'Record':
+        for k in self.dict_db.values():
+            if len(k) == 1 and k[0].pk == pk:
+                return k[0]
+            else:
+                for v in k:
+                    if v.pk == pk:
+                        return v
 
 
 class Record:
-    UNIQUE_RECORD_IDENTIFIER = 0
+    PK = 0
 
-    def __init__(self, fio: str, descr: str, old: int):
-        Record.UNIQUE_RECORD_IDENTIFIER += 1
+    def __init__(self, fio: str, descr: str, old: int) -> NoReturn:
         self.fio = fio
         self.descr = descr
         self.old = old
-        self.pk = Record.UNIQUE_RECORD_IDENTIFIER
+        self.pk = self.PK
 
-    def __hash__(self):
+    def __new__(cls, *args) -> NoReturn:
+        cls.PK += 1
+        return super().__new__(cls)
+
+    def __hash__(self) -> hash:
         return hash((self.fio.lower(), self.old))
 
-    def __eq__(self, other: object):
-        if not isinstance(other, Record):
-            raise TypeError('Объекты сравнения должны иметь тип Record')
+    def __eq__(self, other: 'Record') -> bool:
         return hash(self) == hash(other)
 
 
-if __name__ == '__main__':
-    lst_in = list(map(str.strip, sys.stdin.readlines()))
-    db = DataBase('path')
-    for string in lst_in:
-        fio, descr, old = string.split('; ')
-        db.write(Record(fio, descr, old))
-    print(db.dict_db)
-    print(db.read(4).fio)
-    
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+db = DataBase('path')
+for i in lst_in:
+    text = i.split('; ')
+    record = Record(text[0].strip(), text[1].strip(), int(text[2].strip()))
+    if record in db.dict_db:
+        db.dict_db[record].append(record)
+    else:
+        db.write(record)
