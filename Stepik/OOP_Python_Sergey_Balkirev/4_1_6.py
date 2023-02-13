e0f05818cd3f18f2299e3d2fb6badcb4addbf195
“Muzyria” <fila090580@gmail.com>
2023-02-13 12:24:11 +0200

    new

---
+++ Stepik/OOP_Python_Sergey_Balkirev/4_1_6.py
@@ -0,0 +1,35 @@
+class Thing:
+    ID = 0
+
+    def __init__(self, name: str, price: float):
+        Thing.ID += 1
+        self.id = Thing.ID
+        self.name = name
+        self.price = price
+        self.weight = None
+        self.dims = None
+        self.memory = None
+        self.frm = None
+
+    def get_data(self):
+        return self.__dict__
+
+class Table(Thing):
+    def __init__(self, name: str, price: float, weight, dims):
+        super().__init__(name, price)
+        self.weight = weight
+        self.dims = dims
+
+class ElBook(Thing):
+    def __init__(self, name: str, price: float, memory: int, frm):
+        super().__init__(name, price)
+        self.memory = memory
+        self.frm = frm
+
+
+table = Table("Круглый", 1024, 812.55, (700, 750, 700))
+book = ElBook("Python ООП", 2000, 2048, 'pdf')
+print(*table.get_data())
+print(*book.get_data())
+
+print(table.__dict__)
