84f9591a7123f19e61c602c0c7e4129538a167b8
“Muzyria” <fila090580@gmail.com>
2023-02-13 17:04:06 +0200

    new

---
+++ Stepik/OOP_Python_Sergey_Balkirev/4_2_6.py
@@ -0,0 +1,27 @@
+class Protists:
+    pass
+
+class Plants:
+    pass
+
+class Animals:
+    pass
+
+class Mosses:
+    pass
+
+class Flowering:
+    pass
+
+class Worms:
+    pass
+
+class Mammals:
+    pass
+
+class Human:
+    pass
+
+class Monkeys:
+    pass
+
