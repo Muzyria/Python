4829fb3e1e2ef93251b5df7180138096a90697c6
Muzyria <fila090580@gmail.com>
2023-02-15 00:15:09 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/4_3_7.py
+++ Stepik/OOP_Python_Sergey_Balkirev/4_3_7.py
@@ -9,4 +9,26 @@ class Router:
     def add_callback(cls, path, func):
         cls.app[path] = func
 
-# здесь объявляйте декоратор Callback
+
+class Callback:
+    def __init__(self, path: str, router_cls: object):
+        self.path = path
+        self.router_cls = router_cls
+
+    def __call__(self, func):
+        self.router_cls.add_callback(self.path, func)
+
+
+@Callback('/', Router)
+def index():
+    print('index')
+    return '<h1>Главная</h1>'
+
+
+if __name__ == '__main__':
+
+    route = Router.get('/')
+    if route:
+        ret = route()
+        print(ret)
+        
