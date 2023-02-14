6bbe9c8813452099c78392e9a7b6b9c9c79eb406
“Muzyria” <fila090580@gmail.com>
2023-02-14 12:40:22 +0200

    new

--- Stepik/OOP_Python_Sergey_Balkirev/4_2_8.py
+++ Stepik/OOP_Python_Sergey_Balkirev/4_2_8.py
@@ -1,2 +1,29 @@
 class  VideoItem:
+    def __init__(self, title: str, descr: str, path):
+        self.title = title
+        self.descr =descr
+        self.path = path
+        self.rating = VideoRating()
+
+class VideoRating:
     def __init__(self):
+        self.__rating = 0
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if value not in range(6):
+            raise ValueError('неверное присваиваемое значение')
+        self.__rating = value
+
+
+v = VideoItem('Курс по Python ООП', 'Подробный курс по Python ООР', 'D:/videos/python_oop.mp4')
+print(v.rating.rating) # 0
+v.rating.rating = 5
+print(v.rating.rating) # 5
+title = v.title
+descr = v.descr
+# v.rating.rating = 6  # ValueError
