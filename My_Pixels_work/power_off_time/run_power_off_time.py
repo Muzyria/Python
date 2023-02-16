1ed40b78309f0c4744133199c1817bfa1de5b84c
“Muzyria” <fila090580@gmail.com>
2023-02-16 13:48:44 +0200

    new

---
+++ My_Pixels_work/power_off_time/run_power_off_time.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import time
+from datetime import datetime, timedelta
+
+def set_time():
+    now = datetime.now()  # получаем текущее время
+    one_minute_later = now + timedelta(minutes=1)  # добавляем 1 минуту к текущему времени
+    hour = one_minute_later.time().hour  # получаем часы через 1 минуту
+    minute = one_minute_later.time().minute  # получаем минуты через 1 минуту
+    print(f'Будет установалено power_off_time={hour}:{minute}')
+    return f'{hour}{minute}'
+
+def check_devices_active():
+    # Запускаем команду adb devices для получения списка устройств
+    output = subprocess.check_output(['adb', 'devices'])
+    # Проверяем, есть ли подключенные устройства в выводе
+    if b'device' in output:
+        print('Устройство Android подключено и активно.')
+        return True
+
+    else:
+        print('Устройство Android не найдено или неактивно.')
+        return False
+
+while True:
+    if check_devices_active():
+        os.system(f'adb shell settings put system power_off_time {set_time()}')
+        time.sleep(120)
+    else:
+        time.sleep(60)
