b4758fc7af2897cd21686ab56ba2c9947b03a32c
“Muzyria” <fila090580@gmail.com>
2023-02-20 15:59:30 +0200

    new

--- My_Pixels_work/SyncWice_get_post/SyncWise_get_geofence.py
+++ My_Pixels_work/SyncWice_get_post/SyncWise_get_geofence.py
@@ -31,9 +31,32 @@ class Test_new:
 
 
     def ntest_get_geofence_list(self):
-        base_url = "https://accounts.syncwise360.com/proxy_dna/CourseGeofenceList/?id_company=236&active=1"  # базовая url
-        result_get = requests.get(base_url, )
+        url = 'https://accounts.syncwise360.com/proxy_dna/CourseGeofenceList/'
+        params = {'id_company': '236', 'active': '1'}
+        headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
+            'Connection': 'keep-alive',
+            'Cookie': f'user_id=305; secretKey={self.secretKey}; apiKey=igolfsaltcreek',
+            'Referer': 'https://accounts.syncwise360.com/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
 
+        response = requests.get(url, params=params, headers=headers)
 
+        if response.status_code == 200:
+            data = response.json()
+            # обрабатываем полученные данные
+        else:
+            print(f'Request failed with status code {response.status_code}')
+
+        print(self.secretKey)
 new = Test_new()
-print(new.__dict__, "++++++++++++++++++++")
+new.ntest_get_geofence_list()
