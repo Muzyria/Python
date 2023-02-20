81f774e0323b355094d1a03f84660d8eb64c03d6
“Muzyria” <fila090580@gmail.com>
2023-02-20 11:03:06 +0200

    new

--- My_Pixels_work/SyncWice_get_post/SyncWise_get_geofence.py
+++ My_Pixels_work/SyncWice_get_post/SyncWise_get_geofence.py
@@ -3,28 +3,22 @@ import requests
 
 class Test_new:
     def ntest_post_authorisaion(self):
-
         base_url = "https://api-dna.igolf.com/rest/action/"  # базовая url
         post_resource = "UserAccountLogin/uUqnXUKU86kghJk/1.0/2.0/HmacSHA256/2Wx7AIBZ4ctGrThUZVTWgvyq-qVGYz2NVDW9SbHQgyQ/221229151003GMT+02:00/JSON"  # Ресурс метода пост
         post_url = base_url + post_resource
         print(post_url)
 
-        json_new = {
+        json_post = {
                     "username": "igolfsaltcreek",
                     "password": "92108340"
                     }
 
-        result_post = requests.post(post_url, json=json_new)
-        # print(result_post.text)
+        result_post = requests.post(post_url, json=json_post)
 
         assert 200 == result_post.status_code
         print("Успешно!!!")
 
-        print(result_post.json())
-
         check_post = result_post.json()
-        # check_info_post = check_post.get('secretKey')
-        # print(check_info_post)
         [print(k, v) for k, v in check_post.items()]
 
 
