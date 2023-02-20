fee32625add19f646ecc6d05339c403a1e72b92b
“Muzyria” <fila090580@gmail.com>
2023-02-20 18:08:29 +0200

    new

--- My_Pixels_work/SyncWice_get_post/debug.py
+++ My_Pixels_work/SyncWice_get_post/debug.py
@@ -1,9 +1,9 @@
 import subprocess
 
-collection_path = 'C:\Git_Muzyria\Python\Python\My_Pixels_work\SyncWice_get_post\SyncWise_postman_collection.json'
+collection_path = r'C:\Git_Muzyria\Python\Python\My_Pixels_work\SyncWice_get_post\SyncWise_postman_collection.json'
 environment_path = '/path/to/environment.json'
 
-command = f'newman run {collection_path} -e {environment_path}'
+command = f'newman run {collection_path}'
 
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
