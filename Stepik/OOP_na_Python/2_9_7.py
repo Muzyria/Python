b73f0d91510d19f9d4ab2cfc40d7ae7eccf1cb27
“Muzyria” <fila090580@gmail.com>
2023-02-21 16:05:36 +0200

    new

---
+++ Stepik/OOP_na_Python/2_9_7.py
@@ -0,0 +1,28 @@
+class TemperatureConverter:
+
+    @staticmethod
+    def celsius_to_fahrenheit(value):
+        return value*(9/5) + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(value):
+        return (value-32) * (5/9)
+
+# Напишите определение класса TemperatureConverter
+
+
+# Ниже код для проверки методов класса TemperatureConverter
+assert TemperatureConverter.celsius_to_fahrenheit(0) == 32.0
+assert TemperatureConverter.celsius_to_fahrenheit(10) == 50.0
+assert TemperatureConverter.celsius_to_fahrenheit(15) == 59.0
+assert TemperatureConverter.celsius_to_fahrenheit(20) == 68.0
+assert TemperatureConverter.celsius_to_fahrenheit(25) == 77.0
+assert TemperatureConverter.celsius_to_fahrenheit(30) == 86.0
+
+assert TemperatureConverter.fahrenheit_to_celsius(86) == 30.0
+assert TemperatureConverter.fahrenheit_to_celsius(77) == 25.0
+assert TemperatureConverter.fahrenheit_to_celsius(68) == 20.0
+assert TemperatureConverter.fahrenheit_to_celsius(59) == 15.0
+assert TemperatureConverter.fahrenheit_to_celsius(50) == 10.0
+assert TemperatureConverter.fahrenheit_to_celsius(32) == 0
+print('Good')
