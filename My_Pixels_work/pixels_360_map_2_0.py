e95efea57b42a01c677dff42b31b2ea454e2731b
Muzyria <fila090580@gmail.com>
2023-02-22 22:02:13 +0200

    new

--- My_Pixels_work/pixels_360_map_2_0.py
+++ My_Pixels_work/pixels_360_map_2_0.py
@@ -16,63 +16,71 @@ class NoTest1:
 
     def driwing_map(self):
 
-        base_url = 'https://accounts.syncwise360.com/#login'
+        base_url = 'https://sandbox.syncwise360.com/login'
         self.driver.get(base_url)
         self.driver.maximize_window()
         time.sleep(2)
 
         print('Start test')
 
-        login_standard_user = "nessayurassic"
-        password_all = "10583021"
+        login_standard_user = "igorperetssuperior"
+        password_all = "1234"
 
-        #  LOGON PAGE
-        user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="login-form-username"]')))
+        #  LOGIN PAGE
+        user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="username"]')))
         user_name.send_keys(login_standard_user)
         print('Input Login')
 
-        password = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="login-form-password"]')))
+        password = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="password"]')))
         password.send_keys(password_all)
         print('Input Password')
 
-        button_login = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="login-form-submit"]')))
+        button_login = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="helo"]/app-auth/app-login/form/div/div/div[2]/div/div[2]/div[3]/button')))
         button_login.click()
         print('Click Login Button')
-        # time.sleep(2)
+        time.sleep(20)
 
         #  ASSERT TRACKER
-        button_asset_tracker = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//li[@id="nav-yamaha"]')))
-        button_asset_tracker.click()
-        print('Click Asset Tracker Button')
+        button_close_slidebar = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="closeSidebarbtn"]')))
+        button_close_slidebar.click()
+        print('Click Close SlideBar Button')
+        time.sleep(3)
 
         #  Geofence Button
-        button_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button7 bsize9 vertical-box-center btn-geofence"]')))
+        button_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="geofenceZones"]')))
         button_geofence.click()
         print('Click Geofence Button')
-        # time.sleep(5)
+        time.sleep(3)
 
     def add_geofence(self, center_x, center_y, radius, ind):
         #  ADD Geofence
-        button_add_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button4 rectangle btn-add"]')))
+        button_add_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="closeModal p-0 ng-star-inserted"]')))
         button_add_geofence.click()
         print('Click ADD Geofence Button')
         time.sleep(1)
 
         #  INPUT NAME Geofence
-        input_name_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@name="name"]')))
-        input_name_geofence.send_keys(f'1_DELETE_My_Test_geofence_{ind}')
+        input_name_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//input[@class="ng-untouched ng-pristine ng-invalid"]')))
+        input_name_geofence.send_keys(f'Please_DELETE_My_Test_geofence_{ind}')
         print(f'Input NAME GEOFENCE {ind}')
-        # time.sleep(1)
+        time.sleep(1)
 
-        select_command_list = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//select[@name="id_geofenceActionType"]')))
+        # SELECT ListCommand Geofence
+        select_command_list = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//select[@class="ng-untouched ng-pristine ng-invalid"]')))
         select_command_list.click()
         print('Click select_command_list')
-        # time.sleep(1)
-
-        select_immediate_command = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//option[text()="Immediate shutdown"]')))
+        time.sleep(1)
+        # Select command
+        select_immediate_command = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//option[text()="2 mph with beeping "]')))
         select_immediate_command.click()
-        print('Click Immediate shutdown')
-        # time.sleep(1)
+        print('Click 2 mph with beeping ')
+        time.sleep(1)
+
+        # # Input Custom Message
+        # user_name = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//textarea[@class="ng-untouched ng-pristine ng-invalid error-valid"]')))
+        # user_name.send_keys("This geofence was created automatically")
+        # print('Input Custom Message')
+
 
         #  Рисуем геофенс -------------------------------------------------------------------------->
         # Устанавливаем центр окружности
@@ -94,13 +102,13 @@ class NoTest1:
             pyautogui.moveTo(x, y)
             pyautogui.click(button='left')
 
-        pyautogui.press('enter')
+        # pyautogui.click(button='left')
 
-        button_add_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button save stretched-box"]')))
+        # Press Button SAVE
+        button_add_geofence = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="btn"]')))
         button_add_geofence.click()
         print('Click SAVE Button')
-
-        time.sleep(2)
+        time.sleep(5)
 
     @staticmethod
     def action_draw():
@@ -119,7 +127,9 @@ test = NoTest1()
 test.driwing_map()
 
 
-test.add_geofence(200, 400, 50, 1)
+test.add_geofence(300, 450, 50, 1)
+
+
 test.driver.close()
 
 
