8144588642c70914c896df963c48a9ab1887af59
“Muzyria” <fila090580@gmail.com>
2023-02-24 12:19:56 +0200

    new

--- My_Pixels_work/Fake_devices/debug.py
+++ My_Pixels_work/Fake_devices/debug.py
@@ -1,11 +1,11 @@
-class MyClass:
+class Adder:
     def __init__(self, x):
         self.x = x
 
-    def __eq__(self, other):
-        return isinstance(other, MyClass) and self.x == other.x
+    def __call__(self, y):
+        return self.x + y
 
-a = MyClass(5)
-b = "hello"
-
-print(a == b)
+a = Adder(10)
+b = Adder(20)
+c = a(5) + b(5)
+print(c)
