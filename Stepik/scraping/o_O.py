88809a4ca07844cb606d93ba4bce9e4fc786cafa
Muzyria <fila090580@gmail.com>
2023-02-25 21:21:51 +0200

    new

--- Stepik/scraping/o_O.py
+++ Stepik/scraping/o_O.py
@@ -1,3 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://scrapingclub.com/exercise/list_basic/?page=1"
+
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "lxml")  # html.parser
