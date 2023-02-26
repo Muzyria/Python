262ef8734d235fc1c2197050570fbb96850f7318
Muzyria <fila090580@gmail.com>
2023-02-26 21:46:35 +0200

    new

--- Stepik/scraping/o_O_autorisatin_POST.py
+++ Stepik/scraping/o_O_autorisatin_POST.py
@@ -1,8 +1,20 @@
-import requests
+from requests import Session
 from bs4 import BeautifulSoup
 from time import sleep
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/110.0.0.0 Safari/537.36"}
 
+work = Session()
+
+work.get("https://quotes.toscrape.com/", headers=headers)
+
+response = work.get("https://quotes.toscrape.com/login", headers=headers)
+
+soup = BeautifulSoup(response.text, "lxml")
+
+token = soup.find("form").find("input").get("value")
+print(token)
+
+data = {"csrf_token": token, "username": "noname", "password": "password"}
 
