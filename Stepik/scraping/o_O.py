00c474e4fd81293454bbf47412ee801e07a42235
Muzyria <fila090580@gmail.com>
2023-02-26 11:45:05 +0200

    new

--- Stepik/scraping/o_O.py
+++ Stepik/scraping/o_O.py
@@ -2,11 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+
+list_card_url = []
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/110.0.0.0 Safari/537.36"}
+
 for count in range(1, 8):
-    sleep(3)
+    # sleep(1)
     url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, "lxml")  # html.parser
 
@@ -14,10 +20,12 @@ for count in range(1, 8):
     # print(data)
 
     for i in data:
-        name = i.find("h4", class_="card-title").text.replace("\n", "")
-        # print(name)
-        price = i.find("h5").text
-        # print(price)
-        url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
-        # print(url_img)
-        print(name, price, url_img, sep="\n")
+        # name = i.find("h4", class_="card-title").text.replace("\n", "")
+        # # print(name)
+        # price = i.find("h5").text
+        # # print(price)
+        # url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
+        # # print(url_img)
+        # print(name, price, url_img, sep="\n")
+        card_url = "https://scrapingclub.com" + i.find("a").get("href")
+        list_card_url.append(card_url)
