c276950bff56b6a96d28c1048b7669d083b9ce5c
“Muzyria” <fila090580@gmail.com>
2023-03-07 17:56:43 +0200

    new

--- Stepik/Regular_Expressions_in_Python/debug.py
+++ Stepik/Regular_Expressions_in_Python/debug.py
@@ -1,2 +1,5 @@
 regex = r'\b[a-z]+[A-Z][a-z]+\b'
 
+s = "suDhAsa010 Thematdev theminidev a_e_i_o_u_hacker143 aldyhk bio_chain_2_bot miiiiiyt bederke barry021 aikaravinu romangraef awidok mechanarutosucks"
+s2 = s.replace(" ", "|")
+print(s2)
