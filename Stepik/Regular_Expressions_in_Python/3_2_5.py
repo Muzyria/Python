71344362d16d0e9520b2738b0d676f79341aa543
“Muzyria” <fila090580@gmail.com>
2023-03-09 11:44:34 +0200

    new

---
+++ Stepik/Regular_Expressions_in_Python/3_2_5.py
@@ -0,0 +1,13 @@
+import re
+
+# match = re.match(input(), input())
+match = re.match("I", "I love regex!")
+# Код писать сюда \(❤‿❤)/
+print(match.group())
+print(match.start())
+print(match.end())
+print(match.pos)
+print(match.endpos)
+print(match.re)
+print(match.string)
+
