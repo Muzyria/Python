f6e1ffc4a0fafe7511277c0e4264377a3bb4afdf
“Muzyria” <fila090580@gmail.com>
2023-03-15 14:12:02 +0200

    new

--- My_Pixels_work/document_change_time/change_time_in_doc.py
+++ My_Pixels_work/document_change_time/change_time_in_doc.py
@@ -6,17 +6,20 @@ from random import randrange
 def change_time(value):
     time_str = value
     time_obj = datetime.strptime(time_str, '%H:%M')
-    new_time_obj = time_obj + timedelta(minutes=randrange(2, 5))
+    new_time_obj = time_obj + timedelta(minutes=randrange(3, 5))
     new_time_str = new_time_obj.strftime('%H:%M')
     # print(f'{value} -> {new_time_str}')
     return new_time_str
 
 
 with open('text.txt', 'r',encoding='utf-8') as file:
-    for i in file:
-        res = re.findall(r'\d\d:\d\d', i.strip())
-        if res:
-            res_2 = re.sub(r'\d\d:\d\d', change_time(res[0]), i.strip())
-            print(res_2)
-        else:
-            print(i)
+    with open('new_text.txt', 'w', encoding='utf8') as file_write:
+        for i in file:
+            res = re.findall(r'\d\d:\d\d', i)
+            if res:
+                res_2 = re.sub(r'\d\d:\d\d', change_time(res[0]), i)
+                print(res_2.strip())
+                file_write.write(res_2)
+            else:
+                print(i.strip())
+                file_write.write(i)
