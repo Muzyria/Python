17baf6a3f0188753d4a7113ce4eb66af30e6c578
“Muzyria” <fila090580@gmail.com>
2023-03-21 10:23:33 +0200

    new

--- My_Pixels_work/Fake_devices/debug.py
+++ My_Pixels_work/Fake_devices/debug.py
@@ -1,7 +1,3 @@
-from datetime import datetime, timedelta
+from string import ascii_lowercase as ascii
 
-time_str = '17:58'
-time_obj = datetime.strptime(time_str, '%H:%M')
-new_time_obj = time_obj + timedelta(minutes=5)
-new_time_str = new_time_obj.strftime('%H:%M')
-print(new_time_str)  # '17:13'
+print(alphabet := {k: v for v, k in enumerate(ascii, 1)})
