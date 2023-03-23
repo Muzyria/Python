11c336e2fe383bc92836edd23bfb5fed5dde2b50
“Muzyria” <fila090580@gmail.com>
2023-03-23 18:42:46 +0200

    new

--- Stepik/Indi_kurs_programirovaniya_na_Puthon_ot_egoroff_channel/7_4_12.py
+++ Stepik/Indi_kurs_programirovaniya_na_Puthon_ot_egoroff_channel/7_4_12.py
@@ -1,9 +1,12 @@
 def first_repeated_word(val: str) -> str:
     """Находит первый дубль в строке"""
-    li = val.split()
-    for i in range(len(li)-1):
-        if li[i] in li[i+1::]:
-            return li[i]
+    my_li = []
+    for word in val.split():
+        if word in my_li:
+            return word
+        else:
+            my_li.append(word)
+
 
 
 print(first_repeated_word("ab ca bc ab"))
