60e5f7dc6491498f7f49764c4a943cdfa23cfc85
“Muzyria” <fila090580@gmail.com>
2023-03-31 13:35:10 +0300

    new

--- My_Pixels_work/Fake_devices/debug.py
+++ My_Pixels_work/Fake_devices/debug.py
@@ -1,6 +1,1 @@
-my_dict = {'Дили': set(), 'Вили': set(), 'Били': set()}
-while (n := input()) != 'конец':
-    my_dict[n.split(': ')[0]].add((n.split(': ')[1]))
-
-for k, v in sorted(my_dict.items(), key=lambda x: -len(x[1])):
-    print(f"Количество уникальных комментаторов у {k} - {len(v)}")
+# print(__import__('requests').request("GET", "https://send-request.me/api/companies/").text)
--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,7 +1,4 @@
-words = ['feel', 'graduate', 'movie', 'fashionable', 'bacon',
-         'drop', 'produce', 'acquisition', 'cheap', 'strength',
-         'master', 'perception', 'noise', 'strange', 'am']
+import requests
 
 
-print(words_with_position := [(k, v) for v, k in enumerate(words, 1)])
-
+s = requests.get()
