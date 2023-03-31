7f6857d36aadce5fdf09bdd5390d961d5370db9f
“Muzyria” <fila090580@gmail.com>
2023-03-31 15:03:21 +0300

    new

--- Stepik/scraping/debug.py
+++ Stepik/scraping/debug.py
@@ -1,20 +1,2 @@
-
-
-
-def get_domain_name(url):
-    return __import__('re').sub(r'(?:(https?://)(www\.)?|www\.)', '', url, 1).split('.')[0]
-
-
-
-assert get_domain_name("http://google.com") == "google"
-assert get_domain_name("http://google.co.jp") == "google"
-assert get_domain_name("www.xakep.ru") == "xakep"
-assert get_domain_name("https://youtube.com") == "youtube"
-
-assert get_domain_name("http://github.com/carbonfive/raygun") =='github'
-assert get_domain_name("http://www.zombie-bites.com") == 'zombie-bites'
-assert get_domain_name("https://www.siemens.com") == 'siemens'
-assert get_domain_name("https://www.whatsapp.com") == 'whatsapp'
-assert get_domain_name("https://www.mywww.com") == 'mywww'
-print(get_domain_name("https://www.mywww.com"))
-print('Проверки пройдены')
+a: int = 5
+print(a)
