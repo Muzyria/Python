89940f15ec31bdb09a87aca7053ccd7cfbb7d00f
“Muzyria” <fila090580@gmail.com>
2023-04-05 12:28:27 +0300

    new

---
+++ My_Pixels_work/Send_message_device_to_site/send_message.py
@@ -0,0 +1,51 @@
+import os
+import random
+import time
+from datetime import datetime, timedelta
+
+def press_button_menu():
+    os.system(f'adb shell input tap 50 50')    # touch button "MENU"
+    print('PRESS BUTTON MENU')
+
+
+def press_button_send_mail():
+    os.system(f'adb shell input tap 1000 200')  # touch button "SEND MAIL"
+    print('PRESS BUTTON SEND MAIL')
+
+
+def press_button_settings():
+    os.system(f'adb shell input tap 800 600')   # touch button "SETTING"
+    print('PRESS BUTTON SETTINGS')
+
+
+def enter_password_settings():
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+    os.system(f'adb shell input tap 600 300')   # touch button "5"
+
+
+def press_select_message():
+    os.system(f'adb shell input tap 50 {random.randint(1, 8)}50')
+    print('SELECT MAIL')
+
+
+def press_yes_button():
+    os.system(f'adb shell input tap 500 650')  # touch button "SEND MAIL"
+    print('PRESS YES BUTTON')
+
+
+press_button_menu()
+
+start = datetime.now()  # получаем текущее время
+finish = start + timedelta(hours=5)  # добавляем 2 hours
+
+while datetime.now() < finish:
+    press_button_send_mail()
+    press_select_message()
+    press_yes_button()
+    time.sleep(60)
+
+    print(f'{start} -> {datetime.now()} -> {finish}')
