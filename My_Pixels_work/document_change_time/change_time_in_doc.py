5e5f0dafa8b4f5c1e65e7e9987f73ecfd441a54c
“Muzyria” <fila090580@gmail.com>
2023-04-13 12:25:27 +0300

    new

--- My_Pixels_work/document_change_time/change_time_in_doc.py
+++ My_Pixels_work/document_change_time/change_time_in_doc.py
@@ -9,7 +9,7 @@ time_list = []
 def change_time(value):
     time_str = value
     time_obj = datetime.strptime(time_str, '%H:%M')
-    new_time_obj = time_obj + timedelta(hours=0.2, minutes=randrange(3, 6))
+    new_time_obj = time_obj + timedelta(hours=-3, minutes=randrange(3, 6))
     new_time_str = new_time_obj.strftime('%H:%M')
     # print(f'{value} -> {new_time_str}')
     time_list.append(new_time_str)
@@ -27,5 +27,23 @@ with open('text.txt', 'r', encoding='utf-8') as file:
             else:
                 print(i.strip())
                 file_write.write(i)
+
+
+def take_hour(start, finish):
+    time1 = start
+    time2 = finish
+    # преобразуем временные метки в минуты
+    hours1, minutes1 = map(int, time1.split(":"))
+    hours2, minutes2 = map(int, time2.split(":"))
+    total_minutes1 = hours1 * 60 + minutes1
+    total_minutes2 = hours2 * 60 + minutes2
+    # вычисляем разницу
+    difference_hours = (total_minutes2 - total_minutes1) // 60
+    difference_minutes = (total_minutes2 - total_minutes1) % 60
+    # выводим результат
+    print("Разница между временными метками: {} часов {} минут".format(difference_hours, difference_minutes))
+
+
 print()
 print(f'{min(time_list)} ---> {max(time_list)}')
+take_hour(min(time_list), max(time_list))
