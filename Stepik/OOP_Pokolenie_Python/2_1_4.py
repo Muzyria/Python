ac6b028be3a72f9dc69189bbd36a1f3c63e08c1b
Muzyria <fila090580@gmail.com>
2023-04-30 22:08:43 +0300

    new

--- Stepik/OOP_Pokolenie_Python/2_1_4.py
+++ Stepik/OOP_Pokolenie_Python/2_1_4.py
@@ -1,4 +1,18 @@
 n = int(input())
 matrix = [[1]*n for _ in range(n)]
+step = n // 2
+
+marker = 1
+start = 0
+finish = n
+
+while step != 0:
+    marker += 1
+    start += 1
+    finish -= 1
+    for i in range(start, finish):
+        for j in range(start, finish):
+            matrix[i][j] = marker
+    step -= 1
 
 [print(*row) for row in matrix]
