9b11495445559d58da20de2f38650815f391074e
“Muzyria” <fila090580@gmail.com>
2023-05-11 11:45:02 +0300

    new

---
+++ Stepik/OOP_Pokolenie_Python/4_8_19.py
@@ -0,0 +1,36 @@
+from functools import singledispatchmethod
+from datetime import date
+
+
+class BirthInfo:
+    @singledispatchmethod
+    def __init__(self, birth_date):
+        raise TypeError('Аргумент переданного типа не поддерживается')
+
+    @__init__.register(date)
+    def _(self, birth_date):
+        self.birth_date = birth_date
+
+    @__init__.register(str)
+    def _(self, birth_date):
+        self.birth_date = date.fromisoformat(birth_date)
+
+    @__init__.register(list)
+    @__init__.register(tuple)
+    def _(self, birth_date):
+        self.birth_date = date(*birth_date)
+
+    @property
+    def age(self):
+        age = date.today().year - self.birth_date.year - 1
+        age += (date.today().month, date.today().day) >= (self.birth_date.month, self.birth_date.day)
+        return age
+
+
+birthinfo1 = BirthInfo('2020-09-18')
+birthinfo2 = BirthInfo(date(2010, 10, 10))
+birthinfo3 = BirthInfo([2016, 1, 1])
+
+print(birthinfo1.birth_date)
+print(birthinfo2.birth_date)
+print(birthinfo3.birth_date)
