c9af9a39934da99e0535d5a310380e15619375b7
Muzyria <fila090580@gmail.com>
2023-05-19 23:48:49 +0300

    new

--- Stepik/OOP_Pokolenie_Python/5_5_20.py
+++ Stepik/OOP_Pokolenie_Python/5_5_20.py
@@ -3,3 +3,39 @@ class Time:
         self.hours = hours
         self.minutes = minutes
 
+    @property
+    def _convert_time(self):
+        return (self.hours + self.minutes // 60) % 24, self.minutes % 60
+
+    def __str__(self):
+        converted_time = self._convert_time
+        return f"{converted_time[0]:02d}:{converted_time[1]:02d}"
+
+    def __add__(self, other):
+        if isinstance(other, Time):
+            return Time(self.hours + other.hours, self.minutes + other.minutes)
+        return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other, Time):
+            self.hours += other.hours
+            self.minutes += other.minutes
+            return self
+        return NotImplemented
+
+
+t = Time(22, 0)
+t += Time(3, 0)
+print(t)
+
+time1 = Time(2, 30)
+time2 = Time(3, 10)
+
+time1 += time2
+
+print(time1)
+print(time2)
+
+t = Time(40, 80)
+print(t.__add__([]))
+print(t.__iadd__('bee'))
