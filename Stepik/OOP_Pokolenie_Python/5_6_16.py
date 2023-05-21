f2e3e8dc0d395d3383f8f31ab907c79e311e4854
Muzyria <fila090580@gmail.com>
2023-05-21 13:34:15 +0300

    new

--- Stepik/OOP_Pokolenie_Python/5_6_16.py
+++ Stepik/OOP_Pokolenie_Python/5_6_16.py
@@ -6,3 +6,13 @@ class CountCalls:
     def __call__(self, *args, **kwargs):
         self.calls += 1
         return self.func(*args, **kwargs)
+
+
+@CountCalls
+def add(a, b):
+    return a + b
+
+
+print(add(1, 2))
+print(add(2, 3))
+print(add.calls)
