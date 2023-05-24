34b224ae7f1345295334551b055e0f5ada897365
“Muzyria” <fila090580@gmail.com>
2023-05-24 17:31:46 +0300

    new

--- Stepik/OOP_Pokolenie_Python/5_8_15.py
+++ Stepik/OOP_Pokolenie_Python/5_8_15.py
@@ -2,9 +2,24 @@ class ProtectedObject:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-    def __setattr__(self, key, value):
-        pass
+    def __getattribute__(self, item: str):
+        # if item.startswith('_'):
+        #     raise AttributeError('')
+        object.__getattribute__(self, item)
 
-    def __delattr__(self, item):
-        raise AttributeError('Доступ к защищенному атрибуту невозможен')
+
+    # def __setattr__(self, key, value):
+    #     pass
+
+    # def __delattr__(self, item):
+    #     raise AttributeError('Доступ к защищенному атрибуту невозможен')
+
+
+user = ProtectedObject(login='PG_kamiya', _password='alreadybanned')
+
+try:
+    print(user.login)
+    # print(user._password)
+except AttributeError as e:
+    print(e)
 
