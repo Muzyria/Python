ef23a52196445c40d9d7471d71daf89f94906b54
“Muzyria” <fila090580@gmail.com>
2023-05-25 11:13:25 +0300

    new

--- Stepik/OOP_Pokolenie_Python/5_8_15.py
+++ Stepik/OOP_Pokolenie_Python/5_8_15.py
@@ -1,10 +1,19 @@
 class ProtectedObject:
     def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+        [object.__setattr__(self, k, v) for k, v in kwargs.items()]
 
     def __setattr__(self, key, value):
-        pass
+        if key.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        object.__setattr__(self, key, value)
 
-    def __delattr__(self, item):
-        raise AttributeError('Доступ к защищенному атрибуту невозможен')
+    def __getattribute__(self, key):
+        if key.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        return object.__getattribute__(self, key)
+
+    def __delattr__(self, key):
+        if key.startswith('_'):
+            raise AttributeError('Доступ к защищенному атрибуту невозможен')
+        object.__delattr__(self, key)
 
