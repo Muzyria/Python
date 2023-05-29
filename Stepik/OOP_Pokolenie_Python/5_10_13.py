ee4a17d599fe0d1fce8f0d0b85c663861889078e
Muzyria <fila090580@gmail.com>
2023-05-29 21:33:31 +0300

    new

--- Stepik/OOP_Pokolenie_Python/5_10_13.py
+++ Stepik/OOP_Pokolenie_Python/5_10_13.py
@@ -15,3 +15,37 @@ class ColoredPoint:
     @property
     def color(self):
         return self._color
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.x}, {self.y}, {self.color})'
+
+    def __eq__(self, other):
+        if isinstance(other, ColoredPoint):
+            return (self.x, self.y, self.color) == (other.x, other.y, other.color)
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.color))
+
+
+point1 = ColoredPoint(1, 2, 'white')
+point2 = ColoredPoint(1, 2, 'white')
+point3 = ColoredPoint(3, 4, 'black')
+
+print(point1 == point2)
+print(hash(point1) == hash(point2))
+print(point1 == point3)
+print(hash(point1) == hash(point3))
+
+points = {ColoredPoint(1, 2, 'white'): 10, ColoredPoint(1, 2, 'black'): 20}
+
+print(points)
+
+point = ColoredPoint(1, 2, 'white')
+
+try:
+    point.color = 'black'
+except AttributeError as e:
+    print('Error')
+
+
--- Stepik/OOP_Pokolenie_Python/debug.py
+++ Stepik/OOP_Pokolenie_Python/debug.py
@@ -1,4 +1,19 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-my_list = [1, 2, 3, 4, 5]
-new_list = [(lambda x: x * ()-1 if x % 2 else x * 1 )(num) for num in my_list]
-print(new_list)
+    # def __eq__(self, other):
+    #     if isinstance(other, Point):
+    #         return self.x == other.x and self.y == other.y
+    #     return NotImplemented
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+p1 = Point(1, 2)
+p2 = Point(1, 2)
+
+# print(p1 == p2)
+print(hash(p1) == hash(p2))
