858c80447937bcde89a4aeca206f80a0eddd10f3
“Muzyria” <fila090580@gmail.com>
2023-05-30 13:26:28 +0300

    new

---
+++ My_Pixels_work/Redmine_Syncwise/redmine_syncwise_me.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# URL для запроса
+url = 'https://redmine.syncwise.com/activity.atom?key=6bbaa888a50f02f885083ab2f56d584acfc1d0aa&user_id=359'
+
+# Отправка GET-запроса и получение XML-ответа
+response = requests.get(url)
+
+# Проверка статуса ответа
+if response.status_code == 200:
+    xml_data = response.content
+
+    # Создание объекта BeautifulSoup для парсинга XML
+    soup = BeautifulSoup(xml_data, 'xml')
+
+    # Извлечение информации за сегодня
+    today = datetime.now().date()
+    entries = soup.find_all('entry')
+    for entry in entries:
+        updated = entry.find('updated').text
+        updated_date = datetime.strptime(updated, "%Y-%m-%dT%H:%M:%SZ").date()
+        if updated_date == today:
+            title = entry.find('title').text
+            author = entry.find('author').text
+            print('Title:', title)
+            print('Updated:', updated)
+            print('Author:', author)
+            print('---')
+
+else:
+    print('Ошибка при получении данных:', response.status_code)
