b4cdbb6cd079d59739673b7dc478d4bb8a5f9f00
Muzyria <fila090580@gmail.com>
2023-06-02 00:03:40 +0300

    new

--- Stepik/OOP_Pokolenie_Python/debug.py
+++ Stepik/OOP_Pokolenie_Python/debug.py
@@ -1,14 +1,6 @@
-class SkipIterator:
-    def __init__(self, iterable, n):
-        lst = list(iterable)
-        self.iterable = (lst[i] for i in range(0, len(lst), n + 1))
+nums = [1, 2, 3, 4, 5]
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return next(self.iterable)
-
-
-skipiterator = SkipIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
-print(*skipiterator)  # 1 3 5 7 9
+print(nums[1:])
+print(nums[:3])
+print(nums[:3])
+print(nums[::2])
