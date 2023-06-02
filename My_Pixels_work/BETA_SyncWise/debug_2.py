0145c361bf44ebc37e88a449c8a522a1a92b9933
Muzyria <fila090580@gmail.com>
2023-06-02 22:52:59 +0300

    new canvas

---
+++ My_Pixels_work/BETA_SyncWise/debug_2.py
@@ -0,0 +1,12 @@
+
+action_code = "UserAccountLogin"
+app_api_key = "FVyzsVqr-BmP280"
+api_version = "1.0"
+signature_version = "2.0"
+signature_method = "HmacSHA256"
+signature = "pGPDLc-pJFXFOe7ztfRInVCTxm6lS3V_U1YIPZrpXUo"
+timestamp = "230602224627+0300"
+response_format = "JSON"
+
+base_url = "https://dev-api.syncwise360.com/rest/action/"
+
