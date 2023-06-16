a444bd4d509d2dd87a82f6be3ab3581bb909b69f
“Muzyria” <fila090580@gmail.com>
2023-06-16 17:19:52 +0300

    new

---
+++ Stepik/OOP_Pokolenie_Python/7_1_24.py
@@ -0,0 +1,29 @@
+class Shape:
+    pass
+
+class Polygon(Shape):
+    pass
+
+class Circle(Shape):
+    pass
+
+class Triangle(Polygon):
+    pass
+
+class Quadrilateral(Polygon):
+    pass
+
+class Parallelogram(Quadrilateral):
+    pass
+
+class Rectangle(Parallelogram):
+    pass
+
+class Square(Rectangle):
+    pass
+
+class IsoscelesTriangle(Triangle):
+    pass
+
+class EquilateralTriangle(Triangle):
+    pass
