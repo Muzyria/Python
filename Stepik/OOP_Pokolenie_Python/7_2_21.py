a0aa803bb80b763d13038f7187d62b70e88015d4
Muzyria <fila090580@gmail.com>
2023-06-18 01:56:03 +0300

    new

--- Stepik/OOP_Pokolenie_Python/7_2_21.py
+++ Stepik/OOP_Pokolenie_Python/7_2_21.py
@@ -1,18 +1,26 @@
 class FieldTracker:
     def __init__(self):
-        self.changed_values = {}
+        self._values = {
+            field: getattr(self, field)
+            for field in self.fields
+        }
 
-    def base(self):
-        return
+    def base(self, field):
+        return self._values[field]
 
-    def has_changed(self):
-        return
+    def has_changed(self, field):
+        return self._values[field] != getattr(self, field)
 
     def changed(self):
-        return
+        return {
+            field: self.base(field)
+            for field in self.fields
+            if self.has_changed(field)
+        }
 
     def save(self):
-        return
+        for field in self.fields:
+            self._values[field] = getattr(self, field)
 
 
 class Point(FieldTracker):
