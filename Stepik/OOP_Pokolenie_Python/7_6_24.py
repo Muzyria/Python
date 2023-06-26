7e52dfe8ae8d13bc6cfb3c4fcbe6f4f42d073086
“Muzyria” <fila090580@gmail.com>
2023-06-26 17:22:37 +0300

    new

---
+++ Stepik/OOP_Pokolenie_Python/7_6_24.py
@@ -0,0 +1,37 @@
+class Father:
+    def __init__(self, mood='neutral'):
+        self.mood = mood
+
+    def greet(self):
+        return 'Hello!'
+
+    def be_strict(self):
+        self.mood = 'strict'
+
+class Mother():
+    def __init__(self, mood='neutral'):
+        self.mood = mood
+
+    def greet(self):
+        return 'Hi, honey!'
+
+    def be_kind(self):
+        self.mood = 'kind'
+
+class Daughter(Mother, Father):
+    def __init__(self, mood='neutral'):
+        super().__init__(mood)
+
+
+class Son(Father, Mother):
+    def __init__(self, mood='neutral'):
+        super().__init__(mood)
+
+
+father = Father()
+mother = Mother()
+
+print(father.mood)
+print(mother.mood)
+print(father.greet())
+print(mother.greet())
