ab9dbf35e7657a1ba3a61ea4b1a563c5f546144d
“Muzyria” <fila090580@gmail.com>
2023-07-10 16:43:48 +0300

    new

--- Stepik/OOP_Pokolenie_Python/8_2_25.py
+++ Stepik/OOP_Pokolenie_Python/8_2_25.py
@@ -1,6 +1,5 @@
-from enum import Enum
 from datetime import timedelta
-
+from enum import Enum
 class Weekday(Enum):
     MONDAY = 0
     TUESDAY = 1
@@ -10,9 +9,6 @@ class Weekday(Enum):
     SATURDAY = 5
     SUNDAY = 6
 
-
-from datetime import timedelta
-
 class NextDate:
     def __init__(self, today, weekday, after_today=False):
         self.today = today
@@ -20,26 +16,16 @@ class NextDate:
         self.after_today = after_today
 
     def date(self):
-        days_ahead = (self.weekday.value - self.today.weekday() + 7) % 7
-        if self.after_today and days_ahead == 0:
-            days_ahead = 7
+        if self.after_today and self.weekday.value <= self.today.weekday():
+            days_ahead = (7 - self.today.weekday() + self.weekday.value)
+        else:
+            days_ahead = (self.weekday.value - self.today.weekday())
         next_date = self.today + timedelta(days=days_ahead)
         return next_date
 
     def days_until(self):
-        days_ahead = (self.weekday.value - self.today.weekday() + 7) % 7
-        if self.after_today:
-            if days_ahead == 0:
-                days_ahead = 7
+        if self.after_today and self.weekday.value <= self.today.weekday():
+            days_ahead = (7 - self.today.weekday() + self.weekday.value)
+        else:
+            days_ahead = (self.weekday.value - self.today.weekday())
         return days_ahead
-
-
-
-
-from datetime import date
-
-today = date(2023, 4, 17)                              # понедельник
-next_monday = NextDate(today, Weekday.MONDAY)          # следующий понедельник без учета текущего
-
-print(next_monday.date())
-print(next_monday.days_until())
