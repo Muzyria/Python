4e2b60d78ee5393bdab5559f7b22eefacf5346f6
“Muzyria” <fila090580@gmail.com>
2023-08-04 17:28:03 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,4 +1,12 @@
-import requests
+d = [1, 2, [True, False], ["Москва", "Уфа", [100, 101], ['True', [-2, -1]]], 7.89]
+
+def get_line_list(d,a=[]):
+    for item in d:
+        if type(item) != list:
+            a.append(item)
+        else:
+            get_line_list(item)
+    return a
 
 
-s = requests.get()
+print(get_line_list(d))
