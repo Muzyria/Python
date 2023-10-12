5036cdd1e29af4695774e1699816be95eb06c3d0
“Muzyria” <fila090580@gmail.com>
2023-10-12 16:49:19 +0300

    new

--- My_Pixels_work/appium/first.py
+++ My_Pixels_work/appium/first.py
@@ -1,38 +1,30 @@
 import unittest
 from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.common.mobileby import MobileBy
 
-capabilities = dict(
-    platformName='android',
-    automationName='uiautomator2',
-    deviceName='dbe407da',
-    appPackage='com.l1inc.yamatrack3d',
-    # appActivity='.activities.MapActivity10Inch_',
-    # language='en',
-    # locale='US'
-)
+capabilities = {
+    "platformName": "android",
+    "automationName": "uiautomator2",
+    "deviceName": "dbe407da",
+    "appPackage": "com.l1inc.yamatrack3d",
+    # "language": "en",
+    # "locale": "US"
+}
 
-appium_server_url = 'http://localhost:4723'
+appium_server_url = 'http://localhost:4723/wd/hub'
 
 class TestAppium(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.driver = webdriver.Remote(appium_server_url, capabilities)
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         if self.driver:
             self.driver.quit()
 
-    # def test_find_battery(self) -> None:
-    #     el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
-    #     el.click()
-
-    def test_click_menu(self) -> None:
-         buton = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@id="com.l1inc.yamatrack3d:id/buttonMenu"]')
-         buton.click()
-
-
-
+    def test_click_menu(self):
+        button = self.driver.find_element(MobileBy.XPATH, '//*[@resource-id="com.l1inc.yamatrack3d:id/buttonMenu"]')
+        button.click()
 
 if __name__ == '__main__':
     unittest.main()
+
