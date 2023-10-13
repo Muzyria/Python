16541de34fa58b2a7e49fb2e1157d833c70bbba8
“Muzyria” <fila090580@gmail.com>
2023-10-13 12:43:07 +0300

    new

--- My_Pixels_work/appium/first.py
+++ My_Pixels_work/appium/first.py
@@ -1,32 +1,38 @@
 import unittest
 from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.common.appiumby import By
 
 capabilities = dict(
     platformName='android',
-    automationName='uiautomator2',
+    # automationName='uiautomator2',
     deviceName='dbe407da',
-    appPackage='com.l1inc.yamatrack3d',
+    # appPackage='com.l1inc.yamatrack3d',
     # language='en',
     # locale='US'
 )
 
-appium_server_url = 'http://localhost:4723'
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
 
-class TestAppium(unittest.TestCase):
-    def setUp(self) -> None:
-        self.driver = webdriver.Remote(appium_server_url, capabilities)
-
-    def tearDown(self) -> None:
-        if self.driver:
-            self.driver.quit()
+driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/buttonMenu').click()
+driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings').click()
 
 
-    def test_click_menu(self) -> None:
-         buton = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@resource-id="com.l1inc.yamatrack3d:id/buttonMenu"]')
-         buton.click()
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+# appium_server_url = 'http://localhost:4723'
+#
+# class TestAppium(unittest.TestCase):
+#     def setUp(self) -> None:
+#         self.driver = webdriver.Remote(appium_server_url, capabilities)
+#
+#     def tearDown(self) -> None:
+#         if self.driver:
+#             self.driver.quit()
+#
+#
+#     def test_click_menu(self) -> None:
+#          buton = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@resource-id="com.l1inc.yamatrack3d:id/buttonMenu"]')
+#          buton.click()
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
