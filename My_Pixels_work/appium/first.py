fdb8c0b97e840bc839ea39177802b5cb80319d12
“Muzyria” <fila090580@gmail.com>
2023-10-16 15:03:06 +0300

    new

--- My_Pixels_work/appium/first.py
+++ My_Pixels_work/appium/first.py
@@ -6,40 +6,37 @@ from appium.webdriver.common.appiumby import By
 capabilities = dict(
     platformName='android',
     automationName='uiautomator2',
-    # deviceName='dbe407da',
-    deviceName='192.168.3.220',
-    # uiautomator2ServerInstallTimeout=30000,  # Увеличьте таймаут
-    # appPackage='com.l1inc.yamatrack3d',
-    # language='en',
-    # locale='US'
+    deviceName='192.168.3.221'
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
+driver.implicitly_wait(10)
 
 driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/buttonMenu').click()
 print("CLICK BUTTON MENU")
-time.sleep(0.1)
-
-driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings').click()
-print("CLICK BUTTON SETTINGS")
-time.sleep(0.1)
-
-tex = driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')
-print(tex.text)
-# time.sleep()
 
 
-for i in '123999':
-    driver.find_element(By.ID, f'com.l1inc.yamatrack3d:id/button{i}').click()
-    print(f"PRESS BUTTON {i}")
-    # time.sleep()
+# driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings').click()
+# print("CLICK BUTTON SETTINGS")
+# time.sleep(0.1)
+#
+# tex = driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')
+# print(tex.text)
+# # time.sleep()
+#
+#
+# for i in '123999':
+#     driver.find_element(By.ID, f'com.l1inc.yamatrack3d:id/button{i}').click()
+#     print(f"PRESS BUTTON {i}")
+#     # time.sleep()
+#
+# driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit').click()
+# print("PRESS SUBMIT")
+# time.sleep(0.1)
+#
+# driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/listView/android.widget.LinearLayout[5]').click()
+# # print(data)
 
-driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit').click()
-print("PRESS SUBMIT")
-time.sleep(0.1)
 
-driver.find_element(By.ID, 'com.l1inc.yamatrack3d:id/listView/android.widget.LinearLayout[5]').click()
-# print(data)
-
-
+driver.quit()
 
--- My_Pixels_work/appium/first_2.py
+++ My_Pixels_work/appium/first_2.py
@@ -7,36 +7,43 @@ from selenium.webdriver.support import expected_conditions as EC
 capabilities = dict(
     platformName='android',
     automationName='uiautomator2',
-    deviceName='192.168.3.220'
+    deviceName='192.168.3.221'
     )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
 
 wait = WebDriverWait(driver, 10)  # Максимальное время ожидания в секундах
 
-button_menu = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonMenu')
+# button_menu = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonMenu')
+button_menu = driver.find_element(MobileBy.XPATH, '//*[@_id="00000000-0000-0004-7fff-ffff00000007"]')
+print(button_menu.__dict__['_id'])
 button_menu.click()
 print("CLICK BUTTON MENU")
 
-wait.until(EC.presence_of_element_located((MobileBy.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings')))
-button_settings = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings')
-button_settings.click()
-print("CLICK BUTTON SETTINGS")
+# button_menu = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonMenu')
+# button_menu.click()
+# print("CLICK BUTTON MENU")
 
-wait.until(EC.presence_of_element_located((MobileBy.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')))
-text = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')
-print(text.text)
+# wait.until(EC.presence_of_element_located((MobileBy.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings')))
+# button_settings = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/linearLayoutSettings')
+# button_settings.click()
+# print("CLICK BUTTON SETTINGS")
+#
+# wait.until(EC.presence_of_element_located((MobileBy.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')))
+# text = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/textViewPasscode')
+# print(text.text)
+#
+# for i in '123999':
+#     button_id = f'com.l1inc.yamatrack3d:id/button{i}'
+#     driver.find_element(MobileBy.ID, button_id).click()
+#     print(f"PRESS BUTTON {i}")
+#
+# wait.until(EC.element_to_be_clickable((MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit')))
+# driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit').click()
+# print("PRESS SUBMIT")
+#
+# wait.until(EC.element_to_be_clickable((MobileBy.ID, 'com.l1inc.yamatrack3d:id/listView')))
+# data = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/listView')
 
-for i in '123999':
-    button_id = f'com.l1inc.yamatrack3d:id/button{i}'
-    driver.find_element(MobileBy.ID, button_id).click()
-    print(f"PRESS BUTTON {i}")
 
-wait.until(EC.element_to_be_clickable((MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit')))
-driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/buttonSubmit').click()
-print("PRESS SUBMIT")
-
-wait.until(EC.element_to_be_clickable((MobileBy.ID, 'com.l1inc.yamatrack3d:id/listView')))
-data = driver.find_element(MobileBy.ID, 'com.l1inc.yamatrack3d:id/listView')
-
-
+driver.quit()
