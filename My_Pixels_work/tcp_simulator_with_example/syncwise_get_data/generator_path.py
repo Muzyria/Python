6d706f0185a6631b0c50ae3e8e81508c6b3f2a2e
Muzyria <fila090580@gmail.com>
2023-10-21 22:58:01 +0300

    new

--- My_Pixels_work/tcp_simulator_with_example/syncwise_get_data/generator_path.py
+++ My_Pixels_work/tcp_simulator_with_example/syncwise_get_data/generator_path.py
@@ -18,7 +18,7 @@ class IntermediateCoordinatesGenerator:
     START_COORDINATES = [{'lat': 50.07807852323376, 'lng': 36.23065154766116}]
     OUT_OFF_COURSE_COORDINATES = [{'lat': 50.07810572861073, 'lng': 36.22751781974624}]
     PATH_LIST_HOLES_COORDINATES = []
-    LAST_COORDINATE = None
+    # LAST_COORDINATE = None
 
     def __init__(self):
         self.client_data = SyncwiseClient("https://api2.syncwise360.com")
@@ -26,7 +26,8 @@ class IntermediateCoordinatesGenerator:
         self.client_data.course_vector_details()
         self.last_coordinate = None
 
-    def get_start_coordinates(self, steps):
+    def get_start_coordinates(self, minutes):
+        steps = int(minutes * 60)
         print('RETURN AREA COORDINATE')
         for _ in range(steps):  # start coordinate for begin
             lat, lng = self.START_COORDINATES[0]['lat'], self.START_COORDINATES[0]['lng']
@@ -55,37 +56,49 @@ class IntermediateCoordinatesGenerator:
                 intermediate_coordinates.append({'lat': lat, 'lng': lng})
 
         intermediate_coordinates.append(path[-1])  # Добавляем последнюю координату
-        print(intermediate_coordinates)
+        print(f'get_intermediate_coordinates {intermediate_coordinates}')
         print(f'COUNT STEPS FOR HOLE --- {len(intermediate_coordinates)}')
         return intermediate_coordinates
 
-    def run_device_last_step_to_next_point(self, path, steps):
+    def run_device_last_step_to_next_point(self, path, minutes):
+        steps = int(minutes * 60)
         print(f'MOVED LAST COORDINATE TO NEXT POINT')
-        # temp_coord = [{'lat': 50.07807852323376, 'lng': 36.23065154766116}, {'lat': 50.079678437647, 'lng': 36.231405436993}]
-        for step_patch in self.get_intermediate_coordinates(path, steps):
+        for step_patch in (current_patch := self.get_intermediate_coordinates(path, steps)):
             lat, lng = step_patch['lat'], step_patch['lng']
-            print(f'step -> {lat}, {lng}')
+            print(f'step By run_device_last_step_to_next_point -> {lat}, {lng}')
+        self.last_coordinate = [current_patch[-1]]
 
     def run_device_by_time(self, minutes):  #  генераци нахождения на лунке по времени
         steps = int(minutes * 60)
         for i in range(1, self.client_data.COURSE_VECTOR_DETAILS_HOLECOUNT + 1):
+            self.run_device_last_step_to_next_point([self.last_coordinate[0], self.client_data.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[i][0]], 0.1)
             for step_patch in (current_patch := self.get_intermediate_coordinates(self.client_data.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[i], steps)):
                 lat, lng = step_patch['lat'], step_patch['lng']
-                print(f'step -> {lat}, {lng}')
+                print(f'step By run_device_by_time -> ON HOLE ({i}) -> {lat}, {lng}')
+            self.last_coordinate = [current_patch[-1]]
 
 
+    def run_device_from_start_coordinate_to_first_hole(self, minutes):
+        """ Расчет маршрута от стартовых координат к координатам HOLE 1"""
+        steps = int(minutes * 60)
+        self.run_device_last_step_to_next_point([self.last_coordinate[0], self.client_data.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[1][0]], steps)
+
+    def run_device_from_last_hole_to_start_coordinate(self, minutes):
+        """ Расчет маршрута от финишных координат к координатам RETURN AREA"""
+        print(f'MOVED TO RETURN AREA COORDINATE')
+        self.run_device_last_step_to_next_point([self.last_coordinate[0], self.START_COORDINATES[0]], minutes)
 
 
 generator = IntermediateCoordinatesGenerator()
 
-# generator.get_start_coordinates(10)
+generator.get_start_coordinates(0.1)
 
-print(generator.PATH_LIST_HOLES_COORDINATES)
+# print(generator.PATH_LIST_HOLES_COORDINATES)
 
-generator.run_device_by_time(1)
+generator.run_device_by_time(0.1)
 
-# generator.run_device_last_step_to_next_point([generator.last_coordinate[0], generator.START_COORDINATES[0]], 40)
-# generator.get_start_coordinates(10)
+generator.run_device_from_last_hole_to_start_coordinate(0.1)
+# generator.get_start_coordinates(0.1)
 
 
 print("Everything has gone well.".upper())
