264a3986871574c782533b3bbd34352588219cd5
Muzyria <fila090580@gmail.com>
2023-10-28 23:06:41 +0300

    new

--- My_Pixels_work/SANBOX_SyncWise/script_copy_geofences_from_disney_to_superior/run.py
+++ My_Pixels_work/SANBOX_SyncWise/script_copy_geofences_from_disney_to_superior/run.py
@@ -1,22 +1,30 @@
 from sincwise_clients_method import SyncwiseClient
 
-dev_url = 'https://dev-api.syncwise360.com'
-live_url = 'https://api2.syncwise360.com'
-
-
-id_course = {'superior': 'xqrRgFzOAmmP',
-             'disney': 'f1wKXtcAgZ1n'}
-
-payloads = {'superior': {
+payloads_live = {'superior': {
+                    "host": "https://api2.syncwise360.com",
                     "username": "igorperetssuperior",
                     "password": "Qwerty01!",
-                    "id_company": 2973
+                    "id_company": "4442",
+                    "id_course": "KoyhA-zWt6os"
                         },
-            'disney': {
+                'disney': {
+                    "host": "https://api2.syncwise360.com",
                     "username": "SyncwiseDisney",
                     "password": "92108340",
-                    "id_company": 2973
+                    "id_company": "4820",
+                    "id_course": "f1wKXtcAgZ1n"
                         }
-            }
+                }
+
+
+payloads_dev = {'superior': {
+                    "host": "https://dev-api.syncwise360.com",
+                    "username": "igorperetssuperior",
+                    "password": "Qwerty01!",
+                    "id_company": "2973",
+                    "id_course": "xqrRgFzOAmmP"
+                        },
+                }
+
 
 
