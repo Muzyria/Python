876a71f668533ce0d38b0581fa97992e96ee327c
FILA <fila090580@gmail.com>
2024-01-19 17:35:30 +0200

    new

--- Stepik/debug.py
+++ Stepik/debug.py
@@ -1,48 +1,30 @@
+class BankAccount:
+    def __init__(self, balance=0):
+        self._balance = balance
+
+    def get_balance(self):
+        """метод, возвращающий актуальный баланс счета"""
+        return self._balance
+
+    def deposit(self, amount) -> None:
+        """метод, увеличивающий баланс счета"""
+        if isinstance(amount, (int, float)):
+            self._balance += amount
+        else:
+            raise ValueError("Неверный формат данных.")
+
+    def withdraw(self, amount):
+        """метод, уменьшающий баланс счета"""
+        if isinstance(amount, (int, float)):
+            self._balance += amount
+        else:
+            raise ValueError("На счете недостаточно средств")
 
 
-class Wordplay:
-    def __init__(self, words=None):
-        if not words:
-            words = []
-        self.words = words[:]
+account = BankAccount()
 
-    def add_word(self, word):
-        if word not in self.words:
-            self.words.append(word)
-
-    def words_with_length(self, n):
-        return [el for el in self.words if len(el) == n]
-
-    def only(self, *args):
-        return [el for el in self.words if all([char in args for char in set(el)])]
-
-    def avoid(self, *args):
-        return [el for el in self.words if all([char not in args for char in set(el)])]
-
-
-wordplay = Wordplay()
-
-print(wordplay.words_with_length(1))
-print(wordplay.only('a', 'b', 'c'))
-print(wordplay.avoid('a', 'b', 'c'))
-
-wordplay = Wordplay()
-
-print(wordplay.words)
-wordplay.add_word('bee')
-wordplay.add_word('geek')
-print(wordplay.words)
-
-wordplay = Wordplay(['bee', 'geek', 'cool', 'stepik'])
-
-wordplay.add_word('python')
-print(wordplay.words_with_length(4))
-
-wordplay = Wordplay(['o', 'to', 'otto', 'top', 't'])
-
-print(wordplay.only('o', 't'))
-
-
-wordplay = Wordplay(['a', 'arthur', 'timur', 'bee', 'geek', 'python', 'stepik'])
-
-print(wordplay.avoid('a', 'b', 'c'))  # ['timur', 'geek', 'python', 'stepik']
+print(account.get_balance())
+account.deposit(100)
+print(account.get_balance())
+account.withdraw(50)
+print(account.get_balance())
