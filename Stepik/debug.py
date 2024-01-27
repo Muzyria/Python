df32b328df8254d3a53a069ad51624ffa043fe04
FILA <fila090580@gmail.com>
2024-01-28 01:46:19 +0200

    new

--- Stepik/debug.py
+++ Stepik/debug.py
@@ -1,27 +1,55 @@
+from functools import total_ordering
 
 
-class AnyClass:
-    def __init__(self, **kwargs):
-        self.__dict__ = kwargs
+@total_ordering
+class Version:
+    def __init__(self, version):
+        self.version = version
+
+    @property
+    def version(self) -> list:
+        return self._version
+
+    @version.setter
+    def version(self, version: str) -> None:
+        numbers_list = list(map(int, version.split('.')))
+        numbers_list.extend([0] * (3 - len(numbers_list)))
+        self._version = numbers_list
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({", ".join(map(lambda x:  f"{x[0]}={repr(x[1])}", self.__dict__.items()))})'
+        return f"{self.__class__.__name__}('{'.'.join(map(str, self.version))}')"
 
     def __str__(self):
-        return f'AnyClass: {", ".join(map(lambda x:  f"{x[0]}={repr(x[1])}", self.__dict__.items()))}'
+        return ".".join(map(str, self.version))
+
+    def __eq__(self, other):
+        if isinstance(other, Version):
+            return self.version == other.version
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Version):
+            return self.version > other.version
+        return NotImplemented
 
 
-any = AnyClass()
+a = Version('3')
+print(a)
+print(repr(a))
 
-print(str(any))
-print(repr(any))
 
-cowboy = AnyClass(name='John', surname='Marston')
+print(Version('3.0.3') == Version('1.11.28'))
+print(Version('3.0.3') < Version('1.11.28'))
+print(Version('3.0.3') > Version('1.11.28'))
+print(Version('3.0.3') <= Version('1.11.28'))
+print(Version('3.0.3') >= Version('1.11.28'))
+print()
+print(Version('3') == Version('3.0'))
+print(Version('3') == Version('3.0.0'))
+print(Version('3.0') == Version('3.0.0'))
+print()
+versions = [Version('2'), Version('2.1'), Version('1.9.1')]
 
-print(str(cowboy))
-print(repr(cowboy))
-
-obj = AnyClass(attr1=10, attr2='beegeek', attr3=True, attr4=[1, 2, 3], attr5=('one', 'two'), attr6=None)
-
-print(str(obj))
-print(repr(obj))
+print(sorted(versions))
+print(min(versions))
+print(max(versions))
