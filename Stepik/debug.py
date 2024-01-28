43d7e505c980f89070ce64398073687f3b63c338
FILA <fila090580@gmail.com>
2024-01-28 18:44:05 +0200

    new

--- Stepik/debug.py
+++ Stepik/debug.py
@@ -1,9 +1,23 @@
 
 
 class Matrix:
-    def __init__(self, rows, cols, value=0):
+    def __init__(self, rows: int, cols: int, value: int = 0):
         self.rows = rows
         self.cols = cols
         self.value = value
+        self.matrix = [[value] * cols for _ in range(rows)]
+
+    def get_value(self, row: int, col: int):
+        pass
+
+    def set_value(self, row: int, col: int, value: int):
+        pass
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.rows}, {self.cols})"
+
+    def __str__(self):
+        return '\n'.join(' '.join(map(str, row)) for row in self.matrix)
 
 
+print(Matrix(2, 3))
---
+++ Stepik/debug_2.py
@@ -0,0 +1,3 @@
+matrix = [0, 1, 2, 3]
+
+print(' '.join(map(str, matrix)))
