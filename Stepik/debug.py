4512a8a7538c2f393531e2fb54a4a3bbf6204c19
“Muzyria” <fila090580@gmail.com>
2024-01-29 14:27:29 +0200

    new

--- Stepik/debug.py
+++ Stepik/debug.py
@@ -1,49 +1,45 @@
 
 
-class Gun:
-    def shoot(self):
-        print('pif')
+class Queue:
+    def __init__(self, *args):
+        self.queue = list(args)
+
+    def add(self, *args):
+        self.queue += list(args)
+
+    def pop(self):
+        if self.queue:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def __str__(self):
+        return " -> ".join(map(str, self.queue))
+
+    def __eq__(self, other):
+        if isinstance(other, Queue):
+            return self.queue == other.queue
+        return NotImplemented
+
+    def __add__(self, other):
+        if isinstance(other, Queue):
+            return Queue(*(self.queue + other.queue))
+        return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other, Queue):
+            self.queue += other.queue
+            return self
+        return NotImplemented
+
+    def __rshift__(self, other):
+        if isinstance(other, int):
+            if other >= len(self.queue):
+                return Queue()
+            return Queue(*self.queue[other:])
+        return NotImplemented
 
 
-gun = Gun()
+queue = Queue(1, 2, 3, 4, 5)
 
-gun.shoot()
-
-
-class Color:
-    def __init__(self, hexcode):
-        self.hexcode = hexcode
-
-    @property
-    def hexcode(self) -> str:
-        return self._hexcode
-
-    @property
-    def r(self) -> int:
-        return self._r
-
-    @property
-    def g(self) -> int:
-        return self._g
-
-    @property
-    def b(self) -> int:
-        return self._b
-
-    @hexcode.setter
-    def hexcode(self, value: str) -> None:
-        self._hexcode = value
-        self._r = int(value[0: 2], 16)
-        self._g = int(value[2: 4], 16)
-        self._b = int(value[4: 7], 16)
-
-
-color = Color('0000FF')
-print(color.__dict__)
-
-color.hexcode = 'A782E3'
-print(color.__dict__)
-print(color.hexcode)
-print(color.r)
-print(color.g)
-print(color.b)
+print(queue >> 3)
