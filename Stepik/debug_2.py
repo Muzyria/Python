46213e6941182dfc0d86699d4ddf0932a165787d
FILA <fila090580@gmail.com>
2024-02-12 00:19:51 +0200

    new

--- Stepik/debug_2.py
+++ Stepik/debug_2.py
@@ -2,9 +2,15 @@
 
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-with webdriver.Chrome(ChromeDriverManager().install()) as driver:
-    driver.get("https://google.com")
-    time.sleep(5)
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_extension(fr'C:\Users\FILA\Desktop\coordinates.crx')
+
+#with webdriver.Chrome(ChromeDriverManager().install()) as driver: # webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options_chrome) as driver:
+
+    driver.get('https://google.com')
+    time.sleep(30)
--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,17 +1,1 @@
-from copy import deepcopy
-
-class SequenceZip:
-    def __init__(self, *args):
-        self.sequences = [deepcopy(seq) for seq in args]
-
-    def __len__(self):
-        return min(len(seq) for seq in self.sequences)
-
-    def __getitem__(self, item):
-        return tuple(deepcopy(seq[item]) for seq in self.sequences)
-
-
-sequencezip = SequenceZip()
-print(len(sequencezip))
-print(list(sequencezip))
-
+print(eval("((12434107696 * 3) * 2) + 1"))
