151fd7f6d1d05777295433574940a88c2a28911f
FILA <fila090580@gmail.com>
2024-03-08 01:32:00 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,19 +1,38 @@
 
 
-class Triangle:
-    def __init__(self, a, b, c):
-        self.a, self.b, self.c = a, b, c
+class FieldTracker:
+    def __init__(self):
+        self._values = {item: getattr(self, item) for item in self.fields}
 
-    def perimeter(self):
-        return sum(self.__dict__.values())
+    def base(self, name) -> int:
+        return self._values[name]
 
-class EquilateralTriangle(Triangle):
-    def __init__(self, side):
-        super().__init__(side, side, side)
+    def has_changed(self, name) -> bool:
+        return getattr(self, name) != self._values[name]
+
+    def changed(self) -> dict:
+        return {k: v for k, v in self._values.items() if self.has_changed(k)}
+
+    def save(self) -> None:
+        for k in self.changed().keys():
+            self._values[k] = getattr(self, k)
 
 
-triangle1 = Triangle(3, 4, 5)
-triangle2 = EquilateralTriangle(3)
 
-print(triangle1.perimeter())
-print(triangle2.perimeter())
+class Point(FieldTracker):
+    fields = ('x', 'y', 'z')
+
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+        super().__init__()
+
+point = Point(1, 2, 3)
+point.x = 0
+point.z = 4
+point.save()
+
+print(point.base('x'))
+print(point.base('z'))
+print(point.has_changed('x'))
+print(point.has_changed('z'))
+print(point.changed())
