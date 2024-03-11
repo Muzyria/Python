515ced71dbc86924d7123da7f534c2db9ce76dda
FILA <fila090580@gmail.com>
2024-03-11 22:41:26 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,25 +1,24 @@
 
+class Xrange:
+    def __init__(self, start, end, step=1):
+        self.step = step
+        self.end = end
+        self.start = start - step
 
-class RandomNumbers:
-    def __init__(self, left, right, n):
-        self.iterator = iter([__import__("random").randint(left, right) for _ in range(n)])
-        self.len_iterator = n
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.len_iterator:
-            self.len_iterator -= 1
-            return next(self.iterator)
-        raise StopIteration
+        self.start += self.step
+        if self.start >= self.end and self.step > 0:
+            raise StopIteration
+        if self.start <= self.end and self.step < 0:
+            raise StopIteration
+        return self.start
 
 
-iterator = RandomNumbers(1, 10, 3)
 
-# print(next(iterator))
-# print(next(iterator))
-# print(next(iterator))
-# print(next(iterator))
-print(*iterator)
+xrange = Xrange(5, 10)
 
+print(*xrange)
