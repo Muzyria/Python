728cc9a043b729aa132a5a940b1e064d04514033
“Muzyria” <fila090580@gmail.com>
2024-03-11 16:51:40 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,29 +1,25 @@
 
 
-class CardDeck:
-    KEY = ("пик", "треф", "бубен", "червей")
-    VALUE = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "валет", "дама", "король", "туз")
-
-    def __init__(self):
-        self.coloda = iter([(k, v) for v in self.KEY for k in self.VALUE])
+class RandomNumbers:
+    def __init__(self, left, right, n):
+        self.iterator = iter([__import__("random").randint(left, right) for _ in range(n)])
+        self.len_iterator = n
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return "{} {}".format(*next(self.coloda))
+        if self.len_iterator:
+            self.len_iterator -= 1
+            return next(self.iterator)
+        raise StopIteration
 
 
-cards = CardDeck()
+iterator = RandomNumbers(1, 10, 3)
 
-print(next(cards))
-print(next(cards))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+print(*iterator)
 
-print()
-
-cards = list(CardDeck())
-
-print(cards[9])
-print(cards[23])
-print(cards[37])
-print(cards[51])
