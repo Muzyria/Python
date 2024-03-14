cc70a8f1f44f15a4f30dc38af637c9cf491c3513
FILA <fila090580@gmail.com>
2024-03-15 01:41:39 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,6 +1,10 @@
-from sys import getsizeof
 
-def cubes_of_odds(iterable):
-    return (i**3 for i in iterable if i % 2)
+def filter_names(names, ignor_char, max_names):
+    without_digit = (i for i in names if i.isalpha())
+    without_ignor_char = (i for i in without_digit if not i.lower().startswith(ignor_char.lower()))
+    return (v for k, v in enumerate(without_ignor_char) if k < max_names)
 
-print(*cubes_of_odds([1, 2, 3, 4, 5]))
+
+data = ['Dima', 'Timur', 'Arthur', 'Anri20', 'Arina', 'German', 'Ruslan']
+
+print(*filter_names(data, 'D', 3))
