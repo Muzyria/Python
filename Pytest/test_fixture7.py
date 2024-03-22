8a821f659e9fed04df4753c2d436855430afeb15
FILA <fila090580@gmail.com>
2024-03-22 22:33:00 +0200

    new

---
+++ Pytest/test_fixture7.py
@@ -0,0 +1,27 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("___USE SECOND FIXTURE__")
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    time.sleep(3)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(0)
+
