934751c76280d75f2277f659600be74b4802f618
FILA <fila090580@gmail.com>
2024-03-22 22:40:23 +0200

    new

--- Pytest/conftest.py
+++ Pytest/conftest.py
@@ -1,15 +1,15 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("___USE MAIN FIXTURE__")
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+#
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("___USE MAIN FIXTURE__")
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
