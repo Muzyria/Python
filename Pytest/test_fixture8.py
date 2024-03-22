4774cdd51fa4494157681341b8dde0554fa0b778
FILA <fila090580@gmail.com>
2024-03-22 18:50:47 +0200

    new

--- Pytest/test_fixture8.py
+++ Pytest/test_fixture8.py
@@ -4,24 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def browser():
-    print()
     print("\nstart browser for test..")
-    print()
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    return browser
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
 
-class TestMainPage1:
-    # вызываем фикстуру в тесте, передав ее как параметр
+class TestMainPage1():
+
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
