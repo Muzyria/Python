e467c7040ce07f5b309bd5246de7b47ca22540c1
FILA <fila090580@gmail.com>
2024-03-24 00:30:14 +0200

    new

---
+++ Pytest/test_rerun.py
@@ -0,0 +1,19 @@
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
