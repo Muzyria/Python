dae8ca36611c8f3dea258a7fb74718ba6541af9f
FILA <fila090580@gmail.com>
2024-07-16 00:31:26 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,30 +1,56 @@
-class GroceryItem:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+class Vector:
+    def __init__(self, *args):
+        self.values = args
+
+    @property
+    def values(self):
+        return self._values
+
+    @values.setter
+    def values(self, value):
+        if value:
+            self._values = sorted([item for item in value if type(item) == int])
+        else:
+            self._values = []
 
     def __str__(self):
-        return f"Name: {self.name}\nPrice: {self.price}\nQuantity: {self.quantity}"
+        if self.values:
+            return f"Вектор{tuple(self.values)}"
+        else:
+            return "Пустой вектор"
 
-    def __repr__(self):
-        return f"GroceryItem({self.name}, {self.price}, {self.quantity})"
+    def __add__(self, other):
+        if isinstance(other, int):
+            return Vector(*[i + other for i in self.values])
+        elif isinstance(other, Vector):
+            if len(self.values) == len(other.values):
+                return Vector(*[f + s for f, s in zip(self.values, other.values)])
+            else:
+                print("Сложение векторов разной длины недопустимо")
+        else:
+            print(f"Вектор нельзя сложить с {other}")
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Vector(*[i * other for i in self.values])
+        elif isinstance(other, Vector):
+            if len(self.values) == len(other.values):
+                return Vector(*[f * s for f, s in zip(self.values, other.values)])
+            else:
+                print("Умножение векторов разной длины недопустимо")
+        else:
+            print(f"Вектор нельзя умножать с {other}")
 
 
+v1 = Vector(1,2,3)
+print(v1) # печатает "Вектор(1, 2, 3)"
 
-# Ниже код для проверки методов класса GroceryItem
-banana = GroceryItem('Banana', 10, 5)
-assert banana.__str__() == """Name: Banana
-Price: 10
-Quantity: 5"""
-assert repr(banana) == 'GroceryItem(Banana, 10, 5)'
-print(banana)
-print(f"{banana!r}")
-
-dragon_fruit = GroceryItem('Dragon fruit', 5, 350)
-assert dragon_fruit.__str__() == """Name: Dragon fruit
-Price: 5
-Quantity: 350"""
-assert repr(dragon_fruit) == 'GroceryItem(Dragon fruit, 5, 350)'
-print(dragon_fruit)
-print(f"{dragon_fruit!r}")
+v2 = Vector(3,4,5)
+print(v2) # печатает "Вектор(3, 4, 5)"
+v3 = v1 + v2
+print(v3) # печатает "Вектор(4, 6, 8)"
+v4 = v3 + 5
+print(v4) # печатает "Вектор(9, 11, 13)"
+v5 = v1 * 2
+print(v5) # печатает "Вектор(2, 4, 6)"
+v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
