71f32796926dbb8d3ca7fa49ee666d750929e598
FILA <fila090580@gmail.com>
2024-07-16 15:41:55 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,56 +1,28 @@
-class Vector:
-    def __init__(self, *args):
-        self.values = args
+from functools import total_ordering
+
+
+@total_ordering
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     @property
-    def values(self):
-        return self._values
+    def area(self):
+        return self.width * self.height
 
-    @values.setter
-    def values(self, value):
-        if value:
-            self._values = sorted([item for item in value if type(item) == int])
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area == other.area
+        elif isinstance(other, int):
+            return self.area == other
         else:
-            self._values = []
+            raise NotImplemented
 
-    def __str__(self):
-        if self.values:
-            return f"Вектор{tuple(self.values)}"
+    def __gt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area > other.area
+        elif isinstance(other, int):
+            return self.area > other
         else:
-            return "Пустой вектор"
-
-    def __add__(self, other):
-        if isinstance(other, int):
-            return Vector(*[i + other for i in self.values])
-        elif isinstance(other, Vector):
-            if len(self.values) == len(other.values):
-                return Vector(*[f + s for f, s in zip(self.values, other.values)])
-            else:
-                print("Сложение векторов разной длины недопустимо")
-        else:
-            print(f"Вектор нельзя сложить с {other}")
-
-    def __mul__(self, other):
-        if isinstance(other, int):
-            return Vector(*[i * other for i in self.values])
-        elif isinstance(other, Vector):
-            if len(self.values) == len(other.values):
-                return Vector(*[f * s for f, s in zip(self.values, other.values)])
-            else:
-                print("Умножение векторов разной длины недопустимо")
-        else:
-            print(f"Вектор нельзя умножать с {other}")
-
-
-v1 = Vector(1,2,3)
-print(v1) # печатает "Вектор(1, 2, 3)"
-
-v2 = Vector(3,4,5)
-print(v2) # печатает "Вектор(3, 4, 5)"
-v3 = v1 + v2
-print(v3) # печатает "Вектор(4, 6, 8)"
-v4 = v3 + 5
-print(v4) # печатает "Вектор(9, 11, 13)"
-v5 = v1 * 2
-print(v5) # печатает "Вектор(2, 4, 6)"
-v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
+            raise NotImplemented
