690ca10d69524a4b48cf3b6b317ecd91a6f8db23
FILA <fila090580@gmail.com>
2024-07-26 12:53:11 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,14 +1,36 @@
-class MaxLengthAttribute:
+class RangeValidator:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __set_name__(self, owner, name):
+        self.attribute_name = name
+
     def __get__(self, instance, owner):
-        try:
-            return sorted(instance.__dict__, key=len, reverse=True)[0]
-        except IndexError:
-            return
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.attribute_name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('Неправильный тип данных')
+        if not self.x <= value <= self.y:
+            raise ValueError(f"Значение должно быть между {self.x} и {self.y}")
+        instance.__dict__[self.attribute_name] = value
 
 
+class Temperature:
+    celsius = RangeValidator(-273.15, 1000)
 
-class MyClass:
-    max_length_attribute = MaxLengthAttribute()
 
-obj = MyClass()
-print(obj.max_length_attribute)
+temp = Temperature()
+try:
+    temp.celsius = 500
+except TypeError as ex:
+    print(ex)
+else:
+    print(temp.celsius)
+    print(temp.__dict__)
+
+
