50ea3006a1260231905795f4bd13b6ba02c56857
FILA <fila090580@gmail.com>
2024-08-06 00:40:23 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,33 +1,17 @@
-tables = {
-    1: {'name': 'Andrey', 'is_vip': True},
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-    6: None,
-    7: None,
-    8: None,
-    9: {'name': 'Misha', 'is_vip': False},
-}
+def print_goods(*args):
+    goods = list(filter(lambda x: type(x) is str and x, args))
+    if goods:
+        [print(f"{n}. {name}") for n, name in enumerate(goods, 1)]
+    else:
+        print("Нет товаров")
 
 
-def is_table_free(number):
-    return tables[number] is None
-
-
-def get_free_tables():
-    return list(filter(is_table_free, tables.keys()))
-
-
-def reserve_table(number, name, status=False):
-    if is_table_free(number):
-        tables[number] = {"name": name, "is_vip": status}
-
-
-def delete_reservation(number):
-    tables[number] = None
-
-print(tables)
-reserve_table(2, 'Niko', True)
-reserve_table(6, 'Chubaka') # не передается вип-статус
-print(tables)
+print_goods('apple', 'banana', 'orange')
+print()
+print_goods(1, True, 'Грушечка', '', 'Pineapple')
+print()
+print_goods(*list('qwerty'))
+print()
+print_goods([], {}, 1, 2)
+print()
+print_goods(*list('abc'), 1, 'hello')
