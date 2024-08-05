a2084f070063afeeb5eceec05709ee05b0a967cf
FILA <fila090580@gmail.com>
2024-08-05 19:07:19 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,5 +1,33 @@
-def create_tuples(lst_1: list, lst_2: list) -> list:
-    return list(zip(lst_1, lst_2))
+tables = {
+    1: {'name': 'Andrey', 'is_vip': True},
+    2: None,
+    3: None,
+    4: None,
+    5: None,
+    6: None,
+    7: None,
+    8: None,
+    9: {'name': 'Misha', 'is_vip': False},
+}
 
 
-print(create_tuples([1, 2, 3, 4], [5, 6, 7, 8]))
+def is_table_free(number):
+    return tables[number] is None
+
+
+def get_free_tables():
+    return list(filter(is_table_free, tables.keys()))
+
+
+def reserve_table(number, name, status=False):
+    if is_table_free(number):
+        tables[number] = {"name": name, "is_vip": status}
+
+
+def delete_reservation(number):
+    tables[number] = None
+
+print(tables)
+reserve_table(2, 'Niko', True)
+reserve_table(6, 'Chubaka') # не передается вип-статус
+print(tables)
