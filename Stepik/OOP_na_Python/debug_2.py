5c14182ac226be348611ab8a7426a43a99a422f7
FILA <fila090580@gmail.com>
2024-08-07 12:15:31 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,17 +1,12 @@
-def print_goods(*args):
-    goods = list(filter(lambda x: type(x) is str and x, args))
-    if goods:
-        [print(f"{n}. {name}") for n, name in enumerate(goods, 1)]
-    else:
-        print("Нет товаров")
+def make_strings_big(*args, reverse=False):
+    for i in args:
+        if reverse:
+            print(i.lower())
+        else:
+            print(i.upper())
 
 
-print_goods('apple', 'banana', 'orange')
-print()
-print_goods(1, True, 'Грушечка', '', 'Pineapple')
-print()
-print_goods(*list('qwerty'))
-print()
-print_goods([], {}, 1, 2)
-print()
-print_goods(*list('abc'), 1, 'hello')
+
+make_strings_big('У лукоморья дуб зелёный',
+                 'Златая цепь на дубе том:',
+                 'И днём и ночью кот учёный')
