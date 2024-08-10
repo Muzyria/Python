041304700d073be52c089d6d33d2fdf00325064e
FILA <fila090580@gmail.com>
2024-08-11 02:59:15 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,16 +1,16 @@
-import string
-alfa = string.ascii_lowercase
+
+def check_exist_attrs(obj: object, lst: list[str]) -> dict[str, bool]:
+    return {i: hasattr(obj, i) for i in lst}
 
 
-def rotate_letter(letter: str, shift: int) -> str:
-    """Функция сдвигает символ letter на shift позиций"""
-    index = (shift + alfa.index(letter)) % len(alfa)
-    return alfa[index]
-
-
-def caesar_cipher(phrase: str, key: int) -> str:
-    """caesar_cipher"""
-    return "".join([rotate_letter(i, key) if i in alfa else i for i in phrase])
+def create_attrs(obj: object, lst: list[tuple[str, int|bool|str]]) -> None:
+    [setattr(obj, k, v) for k, v in lst]
 
 
 
+def print_goods(lst):
+    pass
+
+create_attrs(print_goods, [('is_working', False), ('days', 10), ('status', 'Not ready')])
+
+print(check_exist_attrs(print_goods, ['sort', 'is_working', 'days', 'status', 'upper']))
