f08dbdf1c090ca693799d3ba84deba73bf68f668
FILA <fila090580@gmail.com>
2024-08-12 22:14:04 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,12 +1,15 @@
 
-def aggregation(func, sequence):
+def aggregation(func, sequence, initial=None):
+    if initial:
+        sequence.insert(0, initial)
     ANS = [sequence[0]]
     for i in sequence[1:]:
         ANS.append(func(ANS[-1], i))
-    return ANS[1:]
+    return ANS[1:][-1]
 
 
-def get_product(x, y):
-    return x * y
+def get_add(x, y):
+    return x + y
 
-print(aggregation(get_product, [2, 6, 5, 10, 5, 1, 2]))
+
+print(aggregation(get_add, [5, 2, 4, 3, 5], initial=10))
