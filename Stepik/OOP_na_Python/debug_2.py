f779ac8cfb7f505ed81b839ac982bb87046a2c2d
FILA <fila090580@gmail.com>
2024-08-14 00:28:24 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,26 +1,27 @@
-def filter_even(func):
-    def wrapper(*args, **kwargs):
-        args = filter(lambda x: (type(x) is bool and x is False) or
-                                (type(x) is int and not x % 2) or
-                                (hasattr(x, '__len__') and not len(x) % 2), args)
+def counting_calls(func: callable) -> callable:
+    def inner(*args, **kwargs):
+        inner.calls.append({"args": args, "kwargs": kwargs})
+        inner.call_count += 1
         return func(*args, **kwargs)
-    return wrapper
-
-def delete_short(func):
-    def wrapper(*args, **kwargs):
-        kwargs = {k: v for k, v in kwargs.items() if type(k) is str and len(k) > 4}
-        return func(*args, **kwargs)
-    return wrapper
+    inner.call_count = 0
+    inner.calls = []
+    inner.__name__ = func.__name__
+    inner.__doc__ = func.__doc__
+    return inner
 
 
-
-@filter_even
-@delete_short
-def concatenate(*args, **kwargs):
-    result = ""
-    for arg in args + tuple(kwargs.values()):
-        result += str(arg)
-    return result
+@counting_calls
+def add(a: int, b: int) -> int:
+    '''Возвращает сумму двух чисел'''
+    return a + b
 
 
-print(concatenate([1], [1, 2], {1:1, 2:2}, {1:1}, a="3a", qwer=10, ccccc="Месяцев"))
+print(add.__name__)
+print(add.__doc__)
+
+print(add(10, b=20))
+print('Количество вызовов =', add.call_count)
+print(add.calls)
+
+print(add(5, 6))
+print(add.calls[1])
