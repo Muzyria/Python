05d9e0d5aa9ebe9cd406378fa229c900ecedf24d
FILA <fila090580@gmail.com>
2024-08-13 17:16:43 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,14 +1,26 @@
-def decorator(func):
+def filter_even(func):
     def wrapper(*args, **kwargs):
-        print('---Start calculation---')
-        result = func(*args, **kwargs)
-        print(f'---Finish calculation. Result is {result}---')
-        return result
+        args = filter(lambda x: (type(x) is bool and x is False) or
+                                (type(x) is int and not x % 2) or
+                                (hasattr(x, '__len__') and not len(x) % 2), args)
+        return func(*args, **kwargs)
+    return wrapper
+
+def delete_short(func):
+    def wrapper(*args, **kwargs):
+        kwargs = {k: v for k, v in kwargs.items() if type(k) is str and len(k) > 4}
+        return func(*args, **kwargs)
     return wrapper
 
 
-@decorator
-def add(*values):
-    return sum(values)
 
-add(1, 4, 5, 6)
+@filter_even
+@delete_short
+def concatenate(*args, **kwargs):
+    result = ""
+    for arg in args + tuple(kwargs.values()):
+        result += str(arg)
+    return result
+
+
+print(concatenate([1], [1, 2], {1:1, 2:2}, {1:1}, a="3a", qwer=10, ccccc="Месяцев"))
