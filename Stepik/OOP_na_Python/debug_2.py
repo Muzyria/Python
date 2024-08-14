a6438dfa86b606f29d08b6c349776cc57920edb1
FILA <fila090580@gmail.com>
2024-08-14 14:03:33 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,27 +1,16 @@
-def counting_calls(func: callable) -> callable:
-    def inner(*args, **kwargs):
-        inner.calls.append({"args": args, "kwargs": kwargs})
-        inner.call_count += 1
-        return func(*args, **kwargs)
-    inner.call_count = 0
-    inner.calls = []
-    inner.__name__ = func.__name__
-    inner.__doc__ = func.__doc__
-    return inner
+def add_attrs(**kwarg) -> callable:
+    def decorator(func: callable) -> callable:
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+        inner.__dict__.update(kwarg)
+        return inner
+    return decorator
 
 
-@counting_calls
-def add(a: int, b: int) -> int:
-    '''Возвращает сумму двух чисел'''
+@add_attrs(test=True, ordered=True)
+def add(a, b):
     return a + b
 
-
-print(add.__name__)
-print(add.__doc__)
-
-print(add(10, b=20))
-print('Количество вызовов =', add.call_count)
-print(add.calls)
-
-print(add(5, 6))
-print(add.calls[1])
+print(add(10, 5))
+print(add.test)
+print(add.ordered)
