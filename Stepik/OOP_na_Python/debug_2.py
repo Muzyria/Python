c9b9095488a344219934b25495a13e6dd65f7f1f
FILA <fila090580@gmail.com>
2024-08-15 00:33:08 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,12 +1,10 @@
 
-def from_hex_to_rgb(color: str) -> tuple[int, int, int]:
-    return tuple(int(color[i:i+2], 16) for i in (1, 3, 5))
-
-
-def convert_to_rgb(values: list[str]) -> list[tuple[int, int, int]]:
-    return list(map(from_hex_to_rgb, values))
+def get_info_marks(students: list[str], *marks: list) -> dict[str, dict[str, int]]:
+    return dict(zip(students, [{'best': max(m), 'worst': min(m)} for m in zip(*marks)]))
 
 
 
-colors = ['#B22222', '#DC143C', '#FF0000', '#FF6347', '#FF7F50']
-print(convert_to_rgb(colors))
+math = [90, 76, 94]
+history = [78, 79, 90]
+students = ["Marie", "Michael", "Marge"]
+print(get_info_marks(students, math, history))
