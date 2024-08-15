a3b4cb5731da20bda8149b5c98e3407fce8d65f4
FILA <fila090580@gmail.com>
2024-08-15 22:28:11 +0300

    new

--- Stepik/OOP_na_Python/debug_2.py
+++ Stepik/OOP_na_Python/debug_2.py
@@ -1,23 +1,10 @@
-from typing import Generator
 
-def my_range_gen(start, stop=None, step=1):
-    if stop is None:
-        start, stop = 0, start
-    if step == 0:
-        return
-    if (step > 0 and start >= stop) or (step < 0 and start <= stop):
-        return
-    current = start
-    while (step > 0 and current < stop) or (step < 0 and current > stop):
-        yield current
-        current += step
+def ackermann(m, n):
+    if m == 0:
+        return n + 1
+    if m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    return ackermann(m - 1, ackermann(m, n - 1))
 
 
-for i in my_range_gen(5):
-    print(i)
-
-for i in my_range_gen(10, 20):
-    print(i)
-
-for i in my_range_gen(10, 30, 3):
-    print(i)
+print(ackermann(1, 5))
