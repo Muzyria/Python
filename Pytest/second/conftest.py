d68bbc4988e1dd3383ce2cfb8905992d938930f2
FILA <fila090580@gmail.com>
2024-08-17 00:03:39 +0300

    new

--- Pytest/second/conftest.py
+++ Pytest/second/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 
+@pytest.fixture
+def check_condition(request):
+    # Условие для пропуска теста
+    if request.node.name == "test_to_skip":
+        pytest.skip("Пропущено, потому что это test_to_skip")
+
+    return "Тест не пропущен"
 
 
 
 
-@pytest.fixture(autouse=True)
-def setup(request):
-    print("START FIXTURE")
-    request.cls.my = "MY"
-    yield
-    print("FINISH FIXTURE")
--- Pytest/second/test_class_demo.py
+++ Pytest/second/test_class_demo.py
@@ -1,31 +1,11 @@
 import pytest
 
 
-class TestClassDemoInstance:
-    value = 0
 
+def test_to_skip(check_condition):
+    # Этот тест будет пропущен
+    assert check_condition == "Тест не пропущен"
 
-    # @pytest.fixture(setup, request, autouse=True)
-    # @classmethod
-    # def set_value(cls):
-    #     cls.value = 5
-    #     print()
-    #     print(request.cls.nodeid)
-    #     print(f"----- {cls.value}")
-    #     print()
-
-    def test_one(self, setup, request):
-        self.value = 1
-        print()
-        print(f"----- {self.value}")
-        print(request.cls.my)
-        # print(dir(request.cls))
-        print(request.node.nodeid)
-        print()
-        assert self.value == 1
-
-    def test_two(self):
-        print()
-        print(f"----- {self.value}")
-        print()
-        assert self.value == 0
+def test_not_skipped(check_condition):
+    # Этот тест будет выполнен
+    assert check_condition == "Тест не пропущен"
