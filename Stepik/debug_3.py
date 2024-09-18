c6025d447b4aca76171d0c9d322b363d1b8f21a6
FILA <fila090580@gmail.com>
2024-09-18 15:20:24 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,3 +1,11 @@
+numbers = [100, 70, 34, 45, 30, 83, 12, 83, -28, 49, -8, -2, 6, 62, 64, -22, -19, 61, 13, 5, 80, -17, 7, 3, 21, 73, 88, -11, 16, -22]
 
+it_numbers = iter(numbers)
 
-print(__import__("string").ascii_letters)
+while True:
+    try:
+        res = next(it_numbers)
+
+    except StopIteration:
+        print(res)
+        break
