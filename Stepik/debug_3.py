3ab01594c3bec9b448d335fc4d98ca13c2378cb1
FILA <fila090580@gmail.com>
2024-09-22 22:53:27 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,16 +1,5 @@
 
-class CardDeck:
-    def __init__(self):
-        self.cards = iter(f"{name} {card}" for card in ("пик", "треф", "бубен", "червей") for name in list(range(2, 11)) + ["валет", "дама", "король", "туз"])
 
-    def __iter__(self):
-        return self
+def around(iterable):
+    ...
 
-    def __next__(self):
-        return next(self.cards)
-
-
-cards = CardDeck()
-
-print()
-print(*cards)
