f5531638c00e0fd48ed676a5b7a1f5c70ddb3116
FILA <fila090580@gmail.com>
2024-10-04 16:35:42 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,22 +1,10 @@
 
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self. length = length
+        self.width = width
 
-class Color:
-    def __init__(self, hexcode: str):
-        self.hexcode = hexcode
+    @classmethod
+    def square(cls, side: int) -> "Rectangle":
+        return cls(side, side)
 
-    @property
-    def hexcode(self) -> str:
-        return self._hexcode
-
-    @hexcode.setter
-    def hexcode(self, value: str) -> None:
-        self._hexcode = value
-        self.r, self.g, self.b = int(value[:2], 16), int(value[2:4], 16), int(value[4:], 16)
-
-color = Color('0000FF')
-
-color.hexcode = 'A782E3'
-print(color.hexcode)
-print(color.r)
-print(color.g)
-print(color.b)
