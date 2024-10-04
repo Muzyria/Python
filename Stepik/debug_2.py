90579431ba2d409b4941c782359453c889c52e0b
FILA <fila090580@gmail.com>
2024-10-04 21:57:31 +0300

    new

--- Stepik/debug_2.py
+++ Stepik/debug_2.py
@@ -1,9 +1,3 @@
-class Calculator:
-    def divide(self, x: int | float, y: int | float) -> int | float:
-        return x / y
+s = "qweRty"
 
-
-
-
-if __name__ == "__main__":
-    pass
+print(s.s)
--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,10 +1,1 @@
 
-class Rectangle:
-    def __init__(self, length: int, width: int):
-        self. length = length
-        self.width = width
-
-    @classmethod
-    def square(cls, side: int) -> "Rectangle":
-        return cls(side, side)
-
