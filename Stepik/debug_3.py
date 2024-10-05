7e9ef03338b6588d67a4fea44f547e77f1416a09
FILA <fila090580@gmail.com>
2024-10-06 01:07:14 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,1 +1,21 @@
 
+from functools import singledispatchmethod
+from datetime import date
+
+class BirthInfo:
+
+    @singledispatchmethod
+    def __init__(self, birth_date):
+        raise TypeError("Аргумент переданного типа не поддерживается")
+
+    @__init__.register
+    def _init_(self, birth_date: date):
+        self.birth_date = birth_date
+
+    @property
+    def age(self):
+        return self.birth_date
+
+birthinfo = BirthInfo(date(2023, 2, 26))
+
+print(birthinfo.age)
