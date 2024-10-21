dd56a6cdb487e21e3ec66690f8efcf6bf60493e8
FILA <fila090580@gmail.com>
2024-10-21 23:57:03 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,35 +1,28 @@
 
 
-class Row:
-    def __init__(self, **kwargs):
-        [object.__setattr__(self, k, v) for k, v in kwargs.items()]
+class Peekable:
+    def __init__(self, iterable):
+        self.iterable = iter(iterable)
 
-    def __setattr__(self, key, value):
-        if key not in self.__dict__:
-            raise AttributeError("Установка нового атрибута невозможна")
-        raise AttributeError("Изменение значения атрибута невозможно")
+    def peek(self, default=None):
+        if not self.iterable:
+            print("TRUE")
+        else:
+            print("FALSE")
 
-    def __delattr__(self, item):
-        raise AttributeError("Удаление атрибута невозможно")
+    def __iter__(self):
+        return self
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({",".join([f"{k}={repr(v)}" for k, v in self.__dict__.items()])})"
-
-    def __eq__(self, other):
-        if isinstance(other, Row):
-            return tuple(self.__dict__.items()) == tuple(other.__dict__.items())
-        return NotImplemented
-
-    def __hash__(self):
-        return hash(tuple(self.__dict__.items()))
+    def __next__(self):
+        return next(self.iterable)
 
 
+iterator = Peekable('beegeek')
 
-row1 = Row(a=1, b=2, c=3)
-row2 = Row(a=1, b=2, c=3)
-row3 = Row(b=2, c=3, a=1)
+print(next(iterator))
+print(next(iterator))
+iterator.peek()
+print(*iterator)
 
-print(row1 == row2)
-print(hash(row1) == hash(row2))
-print(row1 == row3)
-print(hash(row1) == hash(row3))
+iterator.peek()
+print(next(iterator))
