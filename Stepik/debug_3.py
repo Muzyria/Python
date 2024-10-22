2aea5009a4a85591807ee86ffa8f4d5785a79f33
FILA <fila090580@gmail.com>
2024-10-23 00:06:52 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,3 +1,28 @@
 
+class SparseArray:
+    def __init__(self, default):
+        self.default = default
+        self.data = []
 
-print("q" * 250)
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        if key >= len(self.data):
+            return self.default
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        if key > (len(self.data) - 1):
+            self.data.extend([self.default] * (key - len(self.data) + 1))
+        self.data[key] = value
+
+
+array = SparseArray(0)
+
+array[5] = 1000
+array[12] = 1001
+
+print(array[5])
+print(array[12])
+print(array[13])
