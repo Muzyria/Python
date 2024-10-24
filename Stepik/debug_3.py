02e9e063df0a03a87f7992b6f82f80b1520e49f4
FILA <fila090580@gmail.com>
2024-10-24 23:09:06 +0300

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,20 +1,6 @@
-def log_for(logfile: str, date_str: str):
-    with (
-        open(logfile, "r", encoding="utf=8") as read_file,
-        open(f"log_for_{date_str}.txt", "w", encoding="utf=8") as write_file
-    ):
-        for line in read_file.readlines():
-            if line[:10] == date_str:
-                write_file.write(line[10:].lstrip())
+def is_context_manager(obl) -> bool:
+    return hasattr(obl, '__enter__')
 
 
-with open('log.txt', 'w', encoding='utf-8') as file:
-    print('2022-01-01 INFO: User logged in', file=file)
-    print('2022-01-01 ERROR: Invalid input data', file=file)
-    print('2022-01-02 INFO: User logged out', file=file)
-    print('2022-01-03 INFO: User registered', file=file)
 
-log_for('log.txt', '2022-01-01')
-
-with open('log_for_2022-01-01.txt', encoding='utf-8') as file:
-    print(file.read())
+print(is_context_manager(open('output.txt', mode='w')))
