f2fb1b182f0d611638a5c048ca10aa3b0c8b98ee
FILA <fila090580@gmail.com>
2024-11-01 23:32:47 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,34 +1,29 @@
 
 
-class RandomNumber:
-    def __init__(self, start: int, end: int, cache: bool = False):
-        self.start = start
-        self.end = end
-        self.cache = cache
-        if self.cache:
-            self.first_value = __import__("random").randint(self.start, self.end)
+class WeatherWarning:
+    def rain(self, date=None):
+        if date:
+            print(date.strftime('%d.%m.%Y'))
+        print('Ожидаются сильные дожди и ливни с грозой')
 
-    def __set_name__(self, cls, attr):
-        self._attr = attr
+    def snow(self, date=None):
+        if date:
+            print(date.strftime('%d.%m.%Y'))
+        print('Ожидается снег и усиление ветра')
 
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        if hasattr(self, "first_value"):
-            return self.first_value
-        return __import__("random").randint(self.start, self.end)
+    def low_temperature(self, date=None):
+        if date:
+            print(date.strftime('%d.%m.%Y'))
+        print('Ожидается сильное понижение температуры')
 
-    def __set__(self, obj, value):
-        raise AttributeError("Изменение невозможно")
 
-class MagicPoint:
-    x = RandomNumber(0, 5)
-    y = RandomNumber(0, 5)
-    z = RandomNumber(0, 5)
+class WeatherWarningWithDate(WeatherWarning):
+    pass
 
-magicpoint = MagicPoint()
 
-try:
-    magicpoint.x = 10
-except AttributeError as e:
-    print(e)
+from datetime import date
+
+weatherwarning = WeatherWarningWithDate()
+dt = date(2022, 12, 12)
+
+weatherwarning.rain(dt)
