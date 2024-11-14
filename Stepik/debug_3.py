a9aa1380aa5afc79ed447b453a0fd5ca3985e184
FILA <fila090580@gmail.com>
2024-11-14 23:40:37 +0200

    new

--- Stepik/debug_3.py
+++ Stepik/debug_3.py
@@ -1,16 +1,10 @@
-def create_accumulator(total: int | float = 0) -> callable:
-    def inner(x: int | float) -> int | float:
-        nonlocal total
-        total += x
-        return total
+
+def create_dict() -> callable:
+
+    def inner(item) -> dict:
+        inner.count += 1
+        inner.cache[inner.count] = item
+        return inner.cache
+    inner.cache = {}
+    inner.count = 0
     return inner
-
-
-summator_1 = create_accumulator(200)
-print(summator_1(5))
-print(summator_1(7))
-print(summator_1(2))
-
-summator_2 = create_accumulator()
-print(summator_2(3))
-print(summator_2(6))
