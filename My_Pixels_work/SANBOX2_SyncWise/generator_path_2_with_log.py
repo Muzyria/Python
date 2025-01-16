c46809ae284873977ebe4598cb49efe62f5cfe6a
FILA <fila090580@gmail.com>
2025-01-16 17:10:02 +0200

    add

--- My_Pixels_work/SANBOX2_SyncWise/generator_path_2_with_log.py
+++ My_Pixels_work/SANBOX2_SyncWise/generator_path_2_with_log.py
@@ -20,7 +20,7 @@ def execution_time_decorator(func):
 
 class IntermediateCoordinatesGenerator:
     # DICT_IP_DEVICES = {'S10115002211180009': '192.168.2.30', 'L101140017180605A5': '192.168.3.174'}
-    DICT_IP_DEVICES = {'W_W_W_->>>': '192.168.0.106'}
+    DICT_IP_DEVICES = {'W_W_W_->>>': '192.168.0.101'}
     # START_COORDINATES = "50.07807852323376, 36.23065154766116" # superior
     # START_COORDINATES = "49.86316203910068, 24.029529539745567" # lviv demo
     # START_COORDINATES = "41.399138246290164, -75.71986696282578"  # Eighteen Hole-Pine
@@ -113,11 +113,12 @@ class IntermediateCoordinatesGenerator:
     def run_device_by_time(self, minutes=None):  #  генераци нахождения на лунке по времени
         # steps = int(minutes * 40) # ----------------------------
 
-        time_list = [0, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        time_list = [0, 4, 4, 2, 2, 6, 4, 4, 4, 4]
+        # time_list = [0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
         # time_list = [0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-        # time_list = [0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
 
-        for i in range(1, self.client_data.COURSE_VECTOR_DETAILS_HOLECOUNT + 1):
+        for i in range(1, self.client_data.COURSE_VECTOR_DETAILS_HOLECOUNT + 1):  # MAIN
+        # for i in [9, 1, 2, 3, 4, 5, 6, 7, 8]:
         # for i in range(1, 4):
             # ------------- ---------------- -------------- -----------------
             # minutes = random.randint(4, 5)
@@ -132,10 +133,10 @@ class IntermediateCoordinatesGenerator:
             print(message)
 
             # ---------------------------------------------------------
-            steps = int(minutes * 35) # -------------------------------------------------------------------------------
+            steps = int(minutes * 48) #  35 -------------------------------------------------------------------------------
             print(f"-------------------------- STEP TIME {steps} -----------------------------------------------")
 
-            for step_patch in (current_patch := self.get_intermediate_coordinates(self.client_data.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[i], steps)):
+            for step_patch in (current_patch := self.get_intermediate_coordinates(self.client_data.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[i], steps))[5:-5]:  # добавил срез для смещения к центру лунки
                 lat, lng = step_patch['lat'], step_patch['lng']
                 print(f'step -> {lat}, {lng}')
 
@@ -161,7 +162,7 @@ generator = IntermediateCoordinatesGenerator()
 print()
 print(f"START GAME - -----------------------------------------------------------------------------------------")
 generator.log_to_file(f'the game started at {datetime.now().strftime("%H:%M:%S")}')
-generator.get_start_coordinates(3)
+generator.get_start_coordinates(1)
 # generator.run_device(6)
 generator.run_device_by_time()
 generator.get_start_coordinates(3)
