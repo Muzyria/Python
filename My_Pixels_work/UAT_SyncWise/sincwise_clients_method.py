22a3d8c33a194f9665d4ed1a81b68da3a75a4ba4
FILA <fila090580@gmail.com>
2025-11-20 15:34:18 +0200

    add

--- My_Pixels_work/UAT_SyncWise/sincwise_clients_method.py
+++ My_Pixels_work/UAT_SyncWise/sincwise_clients_method.py
@@ -56,7 +56,7 @@ class SyncwiseClient(SyncwiseAPI):
         """
         Login user and get secret key
         """
-        url = f"{self.host}/rest/action/{self.create_url_test_with_public()}"
+        url = f"{self.host}/auth-service/action/{self.create_url_test_with_public()}"
 
         payload = json.dumps({
             "username": "igolfccrb",
@@ -78,53 +78,13 @@ class SyncwiseClient(SyncwiseAPI):
         self.SECRET_KEY = response_data['secretKey']
 
     # PRIVATE
-    def course_geofence_list(self):
-        """
-        Get course geofence list
-        """
-        action = "CourseGeofenceList"
-        url = f"{self.host}/rest/action/{self.create_url_test_with_private(action, self.SECRET_KEY)}"
-        payload = json.dumps({
-            "id_company": 4442,
-            "active": 1
-        })
-
-        headers = {
-            'authority': 'api2.syncwise360.com',
-            'accept': '*/*',
-            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
-            'content-type': 'application/json',
-            'origin': 'https://uat.syncwise360.com',
-            'referer': 'https://uat.syncwise360.com/',
-            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            'x-access-token': '_dHjFdUbm5db7AibFL6fqYVpig1Wb0CyKeryH2YeN7sx2mGg056vEu2MbhkB'
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-        data = response.json()
-        self.COURSE_GEOFENCE_LIST = data
-
-        # poligone_list = []
-        #
-        # for i in data['resultList']:
-        #     poligone_list.append(i['points'])
-        #
-        # print(poligone_list)
-
     def course_vector_details(self, id_course):
         """
         Get course vector details
         """
         action = "CourseVectorDetails"
-        url = f"{self.host}/rest/action/{self.create_url_test_with_private(action, self.SECRET_KEY)}"
+        url = f"{self.host}main-service/action/{self.create_url_test_with_private(action, self.SECRET_KEY)}"
         payload = json.dumps({
-            # "id_course": "KoyhA-zWt6os"
             "id_course": id_course
         })
         headers = {
@@ -146,17 +106,22 @@ class SyncwiseClient(SyncwiseAPI):
 
         response = requests.request("POST", url, headers=headers, data=payload)
         data = response.json()
+        # print(data)
         self.COURSE_VECTOR_DETAILS = data
-        self.COURSE_VECTOR_DETAILS_HOLECOUNT = data['geometry']['HoleCount']
-        self.COURSE_VECTOR_DETAILS_HOLES = data['geometry']['Holes']['Hole']
+
+
+        self.COURSE_VECTOR_DETAILS_HOLECOUNT = data['vectorGPSObject']['HoleCount']
+
+
+        self.COURSE_VECTOR_DETAILS_HOLES = data['vectorGPSObject']['Holes']['Hole']
 
         # Write data for hole shape clubhouse
-        point_str = data['geometry']['Clubhouse']['Shapes']['Shape'][0]['Points']
+        point_str = data['vectorGPSObject']['Clubhouse']['Shapes']['Shape'][0]['Points']
         self.COURSE_VECTOR_DETAILS_CLUBHOUSE[1] = [
             {"lat": float(i.split()[1]), "lng": float(i.split()[0])} for i in point_str.split(",")]
 
         # Write data for hole shape background
-        point_str = data['geometry']['Background']['Shapes']['Shape'][0]['Points']
+        point_str = data['vectorGPSObject']['Background']['Shapes']['Shape'][0]['Points']
         self.COURSE_VECTOR_DETAILS_BACKGROUND[1] = [
             {"lat": float(i.split()[1]), "lng": float(i.split()[0])} for i in point_str.split(",")]
 
@@ -184,50 +149,6 @@ class SyncwiseClient(SyncwiseAPI):
             self.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH[item + 1] = [
                 {"lat": float(i.split()[1]), "lng": float(i.split()[0])} for i in point_str.split(",")]
 
-    def course_geofence_create(self, name, coordinates: list[dict]):
-        """
-        Create course geofence
-        """
-        action = "CourseGeofenceCreate"
-        url = f"{self.host}/rest/action/{self.create_url_test_with_private(action, self.SECRET_KEY)}"
-        payload = json.dumps({
-            "active": 1,
-            "status": 1,
-            "visible": 1,
-            "id_company": 4442,
-            "id_geofenceType": 10,
-            "name": name,
-            "marshallBypass": 1,
-            "disabilityBypass": 1,
-            "controlLevel": 22,
-            "customShutdownTimeout": None,
-            "customRestoreTimeout": None,
-            "geo_fence_type": "cart_control",
-            "id_geofenceActionType": 66,
-            "points": coordinates,
-            "defaultMessage": 1
-        })
-        headers = {
-            'authority': 'api2.syncwise360.com',
-            'accept': '*/*',
-            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
-            'content-type': 'application/json',
-            'origin': 'https://uat.syncwise360.com',
-            'referer': 'https://uat.syncwise360.com/',
-            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            'x-access-token': '_dHjFdUbm5db7AibFL6fqYVpig1Wb0CyKeryH2YeN7sx2mGg056vEu2MbhkB'
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-
-        print(response.text)
-
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -235,46 +156,10 @@ if __name__ == '__main__':
 
     test_1 = SyncwiseClient("https://uat.syncwise360.com")
     test_1.user_account_login()
+
     print(test_1.SECRET_KEY)
 
-    #
-    # test_1.course_geofence_list()
-    # print(test_1.COURSE_GEOFENCE_LIST)
+    test_1.course_vector_details("7rOQfXBAtJ2C")
 
-    test_1.course_vector_details("fAwbKaonIp7Q")
-    print(test_1.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH)
-    # print(test_1.COURSE_VECTOR_DETAILS_HOLES_GREENCENTER)
+    print(test_1.COURSE_VECTOR_DETAILS)
 
-    # # Пример использования
-    # center = (50.081868557672756, 36.22947635581976)
-    # diameter = 50  # в метрах
-    # points = calculate_circle_points(center, diameter)
-
-    # print(points)
-
-    # for key, value in test_1.COURSE_VECTOR_DETAILS_HOLES_GREENCENTER.items():
-    #     center_coordinate = (value[0]["lat"], value[0]["lng"])
-    #     points = calculate_circle_points(center_coordinate, 100, num_points=30)
-    #     # test_1.course_geofence_create(f"{key}_teeeest_", points)
-    #     # time.sleep(5)
-    #     break
-
-
-
-
-
-
-# print(test_1.COURSE_VECTOR_DETAILS)
-# print(test_1.COURSE_VECTOR_DETAILS_HOLES_PERIMETR)
-
-# for k, v in test_1.COURSE_VECTOR_DETAILS_HOLES_PERIMETR.items():  # Create shape geofence
-#     test_1.course_geofence_create(f"a_shape_{k}", test_1.COURSE_VECTOR_DETAILS_HOLES_PERIMETR[k])
-#     print(f"CREATE GEOFENCE - {k}")
-#     time.sleep(10)
-
-# for k, v in test_1.COURSE_VECTOR_DETAILS_CLUBHOUSE.items():
-#     print(k, v, len(v))
-
-# print(test_1.COURSE_VECTOR_DETAILS_HOLES_CENTRALPATH)
-
-# test_1.course_geofence_create('Back_ground', test_1.COURSE_VECTOR_DETAILS_BACKGROUND[1])
